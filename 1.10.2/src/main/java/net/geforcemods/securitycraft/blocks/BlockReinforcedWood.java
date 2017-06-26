b116dca40e116952d68e5a3fa90f437ab29009ee
bl4ckscor3 <donalddani9612@gmx.de>
2017-06-26 20:50:34 +0200

    fixed reinforced planks not showing up correctly in waila

--- 1.10.2/src/main/java/net/geforcemods/securitycraft/blocks/BlockReinforcedWood.java
+++ 1.10.2/src/main/java/net/geforcemods/securitycraft/blocks/BlockReinforcedWood.java
@@ -1,17 +1,22 @@
 package net.geforcemods.securitycraft.blocks;
 
+import net.geforcemods.securitycraft.imc.waila.ICustomWailaDisplay;
+import net.geforcemods.securitycraft.main.mod_SecurityCraft;
 import net.geforcemods.securitycraft.tileentity.TileEntityOwnable;
+import net.geforcemods.securitycraft.util.BlockUtils;
 import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class BlockReinforcedWood extends BlockPlanks implements ITileEntityProvider {
+public class BlockReinforcedWood extends BlockPlanks implements ITileEntityProvider, ICustomWailaDisplay {
 	
     public BlockReinforcedWood(){
         super();
@@ -35,4 +40,15 @@ public class BlockReinforcedWood extends BlockPlanks implements ITileEntityProvi
 		return new TileEntityOwnable();
 	}
 
+	@Override
+	public ItemStack getDisplayStack(World world, IBlockState state, BlockPos pos)
+	{
+		return new ItemStack(Item.getItemFromBlock(mod_SecurityCraft.reinforcedWoodPlanks), 1, BlockUtils.getBlockMeta(world, pos));
+	}
+
+	@Override
+	public boolean shouldShowSCInfo(World world, IBlockState state, BlockPos pos)
+	{
+		return true;
+	}
 }
