0f814651e74acac55a2827d277253012d2b26505
bl4ckscor3 <donalddani9612@gmx.de>
2017-12-22 20:23:44 +0100

    fix mod not loading
    oopsie

--- 1.8.9/src/main/java/net/geforcemods/securitycraft/blocks/reinforced/IReinforcedBlock.java
+++ 1.8.9/src/main/java/net/geforcemods/securitycraft/blocks/reinforced/IReinforcedBlock.java
@@ -1,10 +1,10 @@
 package net.geforcemods.securitycraft.blocks.reinforced;
 
+import java.util.Arrays;
 import java.util.List;
 
 import net.geforcemods.securitycraft.main.mod_SecurityCraft;
 import net.minecraft.block.Block;
-import scala.actors.threadpool.Arrays;
 
 public interface IReinforcedBlock
 {
