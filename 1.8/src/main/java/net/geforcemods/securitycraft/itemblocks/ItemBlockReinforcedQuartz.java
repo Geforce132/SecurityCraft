9737ce7602337b1eef5ae9415a065d3b66c7a463
bl4ckscor3 <donalddani9612@gmx.de>
2018-08-24 18:31:51 +0200

    forgot this in 572a2bfe75e2df4965cd60478a0d9c6929101d12

--- 1.8/src/main/java/net/geforcemods/securitycraft/itemblocks/ItemBlockReinforcedQuartz.java
+++ 1.8/src/main/java/net/geforcemods/securitycraft/itemblocks/ItemBlockReinforcedQuartz.java
@@ -25,7 +25,6 @@ public class ItemBlockReinforcedQuartz extends ItemBlockTinted
 
 		switch(stack.getItemDamage())
 		{
-			case 0: return name + "_default";
 			case 1: return name + "_chiseled";
 			case 2: return name + "_pillar";
 			default: return name;
