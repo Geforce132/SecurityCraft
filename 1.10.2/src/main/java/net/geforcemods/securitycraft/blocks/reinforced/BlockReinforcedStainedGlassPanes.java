019b6e7fabd01947bdf11e479188b6074d674157
bl4ckscor3 <donalddani9612@gmx.de>
2018-09-23 14:41:26 +0200

    fix new panes not dropping anything when broken

--- 1.10.2/src/main/java/net/geforcemods/securitycraft/blocks/reinforced/BlockReinforcedStainedGlassPanes.java
+++ 1.10.2/src/main/java/net/geforcemods/securitycraft/blocks/reinforced/BlockReinforcedStainedGlassPanes.java
@@ -1,7 +1,9 @@
 package net.geforcemods.securitycraft.blocks.reinforced;
 
 import java.util.List;
+import java.util.Random;
 
+import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.tileentity.TileEntityOwnable;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockStainedGlass;
@@ -11,8 +13,10 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import scala.actors.threadpool.Arrays;
 
@@ -33,6 +37,18 @@ public class BlockReinforcedStainedGlassPanes extends BlockStainedGlassPane impl
 	public TileEntity createNewTileEntity(World world, int meta)
 	{
 		return new TileEntityOwnable();
+	}
+
+	@Override
+	public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
+	{
+		return Arrays.asList(new ItemStack[] {new ItemStack(SCContent.reinforcedStainedGlassPanes, 1, state.getValue(COLOR).getMetadata())});
+	}
+
+	@Override
+	public int quantityDropped(Random random)
+	{
+		return 1;
 	}
 
 	@Override
