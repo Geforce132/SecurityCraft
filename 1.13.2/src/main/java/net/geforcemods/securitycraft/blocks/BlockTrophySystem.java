920cd4ec6a0c78976e14e7a2dd106ec759d213f1
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-10-15 17:41:08 +0200

    improve shape of trophy system

--- 1.13.2/src/main/java/net/geforcemods/securitycraft/blocks/BlockTrophySystem.java
+++ 1.13.2/src/main/java/net/geforcemods/securitycraft/blocks/BlockTrophySystem.java
@@ -1,5 +1,7 @@
 package net.geforcemods.securitycraft.blocks;
 
+import java.util.stream.Stream;
+
 import net.geforcemods.securitycraft.tileentity.TileEntityTrophySystem;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
@@ -11,19 +13,31 @@ import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReaderBase;
 import net.minecraft.world.World;
 
 public class BlockTrophySystem extends BlockOwnable {
-	
-	private static final VoxelShape SHAPE = Block.makeCuboidShape(1, 0, 1, 15, 15.5D, 15);
+
+	private static final VoxelShape SHAPE = Stream.of(
+			Block.makeCuboidShape(6.5, 0, 12, 9.5, 1.5, 15),
+			Block.makeCuboidShape(5.5, 7, 5.5, 10.5, 11, 10.5),
+			Block.makeCuboidShape(7, 12, 7, 9, 13, 9),
+			Block.makeCuboidShape(6.5, 12.5, 6.5, 9.5, 15, 9.5),
+			Block.makeCuboidShape(7, 14.5, 7, 9, 15.5, 9),
+			Block.makeCuboidShape(7.25, 9, 7.25, 8.75, 12, 8.75),
+			Block.makeCuboidShape(1, 0, 6.5, 4, 1.5, 9.5),
+			Block.makeCuboidShape(12, 0, 6.5, 15, 1.5, 9.5),
+			Block.makeCuboidShape(6.5, 0, 1, 9.5, 1.5, 4)
+			).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).orElse(VoxelShapes.fullCube());
 
 	public BlockTrophySystem(Material material) {
 		super(SoundType.METAL, Block.Properties.create(material).hardnessAndResistance(-1.0F, 6000000.0F));
 	}
-	
+
 	@Override
 	public boolean isFullCube(IBlockState state) {
 		return false;
@@ -33,7 +47,8 @@ public class BlockTrophySystem extends BlockOwnable {
 	public boolean isNormalCube(IBlockState state) {
 		return false;
 	}
-	
+
+	@Override
 	public BlockRenderLayer getRenderLayer() {
 		return BlockRenderLayer.CUTOUT_MIPPED;
 	}
@@ -42,7 +57,7 @@ public class BlockTrophySystem extends BlockOwnable {
 	public EnumBlockRenderType getRenderType(IBlockState state) {
 		return EnumBlockRenderType.MODEL;
 	}
-	
+
 	@Override
 	public BlockFaceShape getBlockFaceShape(IBlockReader world, IBlockState state, BlockPos pos, EnumFacing face) {
 		return BlockFaceShape.UNDEFINED;
@@ -52,7 +67,7 @@ public class BlockTrophySystem extends BlockOwnable {
 	public boolean isValidPosition(IBlockState state, IWorldReaderBase world, BlockPos pos) {
 		return world.getBlockState(pos.down()).isTopSolid();
 	}
-	
+
 	@Override
 	public void neighborChanged(IBlockState state, World world, BlockPos pos, Block block, BlockPos fromPos) {
 		if(!isValidPosition(state, world, pos)) {
@@ -60,13 +75,13 @@ public class BlockTrophySystem extends BlockOwnable {
 			world.removeBlock(pos);
 		}
 	}
-	
+
 	@Override
 	public VoxelShape getShape(IBlockState state, IBlockReader source, BlockPos pos)
 	{
 		return SHAPE;
 	}
-	
+
 	@Override
 	public TileEntity createNewTileEntity(IBlockReader world) {
 		return new TileEntityTrophySystem();
--- 1.14.4/src/main/java/net/geforcemods/securitycraft/blocks/BlockTrophySystem.java
+++ 1.14.4/src/main/java/net/geforcemods/securitycraft/blocks/BlockTrophySystem.java
@@ -1,5 +1,7 @@
 package net.geforcemods.securitycraft.blocks;
 
+import java.util.stream.Stream;
+
 import net.geforcemods.securitycraft.tileentity.TileEntityTrophySystem;
 import net.geforcemods.securitycraft.util.BlockUtils;
 import net.minecraft.block.Block;
@@ -11,15 +13,27 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 
 public class BlockTrophySystem extends BlockOwnable {
-	
-	private static final VoxelShape SHAPE = Block.makeCuboidShape(1, 0, 1, 15, 15.5D, 15);
+
+	private static final VoxelShape SHAPE = Stream.of(
+			Block.makeCuboidShape(6.5, 0, 12, 9.5, 1.5, 15),
+			Block.makeCuboidShape(5.5, 7, 5.5, 10.5, 11, 10.5),
+			Block.makeCuboidShape(7, 12, 7, 9, 13, 9),
+			Block.makeCuboidShape(6.5, 12.5, 6.5, 9.5, 15, 9.5),
+			Block.makeCuboidShape(7, 14.5, 7, 9, 15.5, 9),
+			Block.makeCuboidShape(7.25, 9, 7.25, 8.75, 12, 8.75),
+			Block.makeCuboidShape(1, 0, 6.5, 4, 1.5, 9.5),
+			Block.makeCuboidShape(12, 0, 6.5, 15, 1.5, 9.5),
+			Block.makeCuboidShape(6.5, 0, 1, 9.5, 1.5, 4)
+			).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).orElse(VoxelShapes.fullCube());
 
 	public BlockTrophySystem(Material material) {
 		super(SoundType.METAL, Block.Properties.create(material).hardnessAndResistance(-1.0F, 6000000.0F));
@@ -29,7 +43,8 @@ public class BlockTrophySystem extends BlockOwnable {
 	public boolean isNormalCube(BlockState state, IBlockReader reader, BlockPos pos) {
 		return false;
 	}
-	
+
+	@Override
 	public BlockRenderLayer getRenderLayer() {
 		return BlockRenderLayer.CUTOUT_MIPPED;
 	}
@@ -43,20 +58,20 @@ public class BlockTrophySystem extends BlockOwnable {
 	public boolean isValidPosition(BlockState state, IWorldReader world, BlockPos pos){
 		return BlockUtils.isSideSolid(world, pos.down(), Direction.UP);
 	}
-	
+
 	@Override
 	public void neighborChanged(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean flag) {
 		if(!isValidPosition(state, world, pos)) {
 			world.destroyBlock(pos, true);
 		}
 	}
-	
+
 	@Override
 	public VoxelShape getShape(BlockState state, IBlockReader source, BlockPos pos, ISelectionContext context)
 	{
 		return SHAPE;
 	}
-	
+
 	@Override
 	public TileEntity createNewTileEntity(IBlockReader world) {
 		return new TileEntityTrophySystem();
