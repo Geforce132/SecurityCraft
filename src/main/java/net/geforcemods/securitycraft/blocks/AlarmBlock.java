7c9d9e9890489ecdd23d580c2751913d8e21d0b4
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-11-13 11:03:44 +0100

    add another alarm crash safeguard

--- src/main/java/net/geforcemods/securitycraft/blocks/AlarmBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/AlarmBlock.java
@@ -146,7 +146,7 @@ public class AlarmBlock extends OwnableBlock {
 	}
 
 	private void playSoundAndUpdate(World world, BlockPos pos){
-		if(!(world.getTileEntity(pos) instanceof AlarmTileEntity)) return;
+		if(world.getBlockState(pos).getBlock() != SCContent.alarm || !(world.getTileEntity(pos) instanceof AlarmTileEntity)) return;
 
 		if(world.getRedstonePowerFromNeighbors(pos) > 0){
 			boolean isPowered = ((AlarmTileEntity) world.getTileEntity(pos)).isPowered();
