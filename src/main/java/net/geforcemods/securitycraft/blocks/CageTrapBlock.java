b3cea6043e4bb1553c27714044af4a7c52ddd9ca
bl4ckscor3 <donalddani9612@gmx.de>
2020-01-03 11:32:43 +0100

    players now can't escape cage traps

--- src/main/java/net/geforcemods/securitycraft/blocks/CageTrapBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/CageTrapBlock.java
@@ -66,6 +66,7 @@ public class CageTrapBlock extends OwnableBlock implements IIntersectable {
 
 				BlockUtils.setBlockProperty(world, pos, DEACTIVATED, true);
 				BlockUtils.setBlock(world, pos.up(4), SCContent.reinforcedIronBars);
+				BlockUtils.setBlock(world, pos.up(3), SCContent.reinforcedIronBars);
 				BlockUtils.setBlock(world, pos.getX() + 1, pos.getY() + 4, pos.getZ(), SCContent.reinforcedIronBars);
 				BlockUtils.setBlock(world, pos.getX() - 1, pos.getY() + 4, pos.getZ(), SCContent.reinforcedIronBars);
 				BlockUtils.setBlock(world, pos.getX(), pos.getY() + 4, pos.getZ() + 1, SCContent.reinforcedIronBars);
@@ -109,6 +110,7 @@ public class CageTrapBlock extends OwnableBlock implements IIntersectable {
 		((IOwnable)world.getTileEntity(BlockUtils.toPos(x, y, z))).getOwner().set(uuid, name);
 
 		((IOwnable)world.getTileEntity(BlockUtils.toPos(x, y + 4, z))).getOwner().set(uuid, name);
+		((IOwnable)world.getTileEntity(BlockUtils.toPos(x, y + 3, z))).getOwner().set(uuid, name);
 		((IOwnable)world.getTileEntity(BlockUtils.toPos(x + 1, y + 4, z))).getOwner().set(uuid, name);
 		((IOwnable)world.getTileEntity(BlockUtils.toPos(x - 1, y + 4, z))).getOwner().set(uuid, name);
 		((IOwnable)world.getTileEntity(BlockUtils.toPos(x, y + 4, z + 1))).getOwner().set(uuid, name);
