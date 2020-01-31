6f14a97ee76f5b3c606117df6885da1ea1252926
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-01-31 15:21:51 +0100

    change bouncing betty renderer a bit

--- src/main/java/net/geforcemods/securitycraft/renderers/RenderBouncingBetty.java
+++ src/main/java/net/geforcemods/securitycraft/renderers/RenderBouncingBetty.java
@@ -28,11 +28,10 @@ public class RenderBouncingBetty extends Render<EntityBouncingBetty> {
 		BlockRendererDispatcher blockrendererdispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
 		GlStateManager.pushMatrix();
 		GlStateManager.translate((float)x, (float)y + 0.5F, (float)z);
-		float alpha;
 
 		if (entity.fuse - partialTicks + 1.0F < 10.0F)
 		{
-			alpha = 1.0F - (entity.fuse - partialTicks + 1.0F) / 10.0F;
+			float alpha = 1.0F - (entity.fuse - partialTicks + 1.0F) / 10.0F;
 			alpha = MathHelper.clamp(alpha, 0.0F, 1.0F);
 			alpha *= alpha;
 			alpha *= alpha;
@@ -40,30 +39,10 @@ public class RenderBouncingBetty extends Render<EntityBouncingBetty> {
 			GlStateManager.scale(scale, scale, scale);
 		}
 
-		alpha = (1.0F - (entity.fuse - partialTicks + 1.0F) / 100.0F) * 0.8F;
 		bindEntityTexture(entity);
 		GlStateManager.translate(-0.5F, -0.5F, 0.5F);
 		blockrendererdispatcher.renderBlockBrightness(SCContent.bouncingBetty.getDefaultState(), entity.getBrightness());
 		GlStateManager.translate(0.0F, 0.0F, 1.0F);
-
-		if (entity.fuse / 5 % 2 == 0)
-		{
-			GlStateManager.disableTexture2D();
-			GlStateManager.disableLighting();
-			GlStateManager.enableBlend();
-			GlStateManager.blendFunc(770, 772);
-			GlStateManager.color(1.0F, 1.0F, 1.0F, alpha);
-			GlStateManager.doPolygonOffset(-3.0F, -3.0F);
-			GlStateManager.enablePolygonOffset();
-			blockrendererdispatcher.renderBlockBrightness(SCContent.bouncingBetty.getDefaultState(), 1.0F);
-			GlStateManager.doPolygonOffset(0.0F, 0.0F);
-			GlStateManager.disablePolygonOffset();
-			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-			GlStateManager.disableBlend();
-			GlStateManager.enableLighting();
-			GlStateManager.enableTexture2D();
-		}
-
 		GlStateManager.popMatrix();
 		super.doRender(entity, x, y, z, entityYaw, partialTicks);
 	}
