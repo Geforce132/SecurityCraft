9eb8ab0e8abb1a0f7ca7e7af1f1e3e2d00c169c4
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-02-03 22:47:28 +0100

    add scrollable list to username logger

--- src/main/java/net/geforcemods/securitycraft/screen/UsernameLoggerScreen.java
+++ src/main/java/net/geforcemods/securitycraft/screen/UsernameLoggerScreen.java
@@ -8,15 +8,19 @@ import net.geforcemods.securitycraft.network.server.ClearLoggerServer;
 import net.geforcemods.securitycraft.screen.components.ClickButton;
 import net.geforcemods.securitycraft.tileentity.UsernameLoggerTileEntity;
 import net.geforcemods.securitycraft.util.ClientUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.client.gui.ScrollPanel;
 
 public class UsernameLoggerScreen extends ContainerScreen<GenericTEContainer>{
 
-	private UsernameLoggerTileEntity tileEntity;
 	private static final ResourceLocation TEXTURE = new ResourceLocation("securitycraft:textures/gui/container/blank.png");
+	private UsernameLoggerTileEntity tileEntity;
+	private PlayerList playerList;
 
 	public UsernameLoggerScreen(GenericTEContainer container, PlayerInventory inv, ITextComponent name) {
 		super(container, inv, name);
@@ -32,6 +36,7 @@ public class UsernameLoggerScreen extends ContainerScreen<GenericTEContainer>{
 			tileEntity.players = new String[100];
 			SecurityCraft.channel.sendToServer(new ClearLoggerServer(tileEntity.getPos()));
 		})).active = tileEntity.getOwner().isOwner(minecraft.player);
+		children.add(playerList = new PlayerList(minecraft, xSize - 24, ySize - 30, guiTop + 20, guiLeft + 12));
 	}
 
 	/**
@@ -44,12 +49,17 @@ public class UsernameLoggerScreen extends ContainerScreen<GenericTEContainer>{
 
 		font.drawString(localized, xSize / 2 - font.getStringWidth(localized) / 2, 6, 4210752);
 
-		for(int i = 0; i < tileEntity.players.length; i++)
-			if(tileEntity.players[i] != "")
-				font.drawString(tileEntity.players[i], xSize / 2 - font.getStringWidth(tileEntity.players[i]) / 2, 25 + (10 * i), 4210752);
-
 		if(mouseX >= guiLeft + 4 && mouseY >= guiTop + 4 && mouseX < guiLeft + 4 + 8 && mouseY < guiTop + 4 + 8)
 			renderTooltip(ClientUtils.localize("gui.securitycraft:editModule.clear"), mouseX - guiLeft, mouseY - guiTop);
+	}
+
+	@Override
+	public void render(int mouseX, int mouseY, float partialTicks)
+	{
+		super.render(mouseX, mouseY, partialTicks);
+
+		if(playerList != null)
+			playerList.render(mouseX, mouseY, partialTicks);
 	}
 
 	@Override
@@ -62,4 +72,66 @@ public class UsernameLoggerScreen extends ContainerScreen<GenericTEContainer>{
 		this.blit(startX, startY, 0, 0, xSize, ySize);
 	}
 
+	@Override
+	public boolean mouseScrolled(double mouseX, double mouseY, double scroll)
+	{
+		if(playerList != null)
+			playerList.mouseScrolled(mouseX, mouseY, scroll);
+
+		return super.mouseScrolled(mouseX, mouseY, scroll);
+	}
+
+	@Override
+	public boolean mouseClicked(double mouseX, double mouseY, int button)
+	{
+		if(playerList != null)
+			playerList.mouseClicked(mouseX, mouseY, button);
+
+		return super.mouseClicked(mouseX, mouseY, button);
+	}
+
+	@Override
+	public boolean mouseReleased(double mouseX, double mouseY, int button)
+	{
+		if(playerList != null)
+			playerList.mouseReleased(mouseX, mouseY, button);
+
+		return super.mouseReleased(mouseX, mouseY, button);
+	}
+
+	@Override
+	public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY)
+	{
+		if(playerList != null)
+			playerList.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
+
+		return super.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
+	}
+
+	class PlayerList extends ScrollPanel
+	{
+		public PlayerList(Minecraft client, int width, int height, int top, int left)
+		{
+			super(client, width, height, top, left);
+		}
+
+		@Override
+		protected int getContentHeight()
+		{
+			int height = 50 + (tileEntity.players.length * font.FONT_HEIGHT);
+
+			if(height < bottom - top - 8)
+				height = bottom - top - 8;
+
+			return height;
+		}
+
+		@Override
+		protected void drawPanel(int entryRight, int relativeY, Tessellator tess, int mouseX, int mouseY)
+		{
+			for(int i = 0; i < tileEntity.players.length; i++)
+				if(tileEntity.players[i] != "")
+					font.drawString(tileEntity.players[i], left + width / 2 - font.getStringWidth(tileEntity.players[i]) / 2, relativeY + (10 * i), 0xC6C6C6);
+		}
+	}
 }
