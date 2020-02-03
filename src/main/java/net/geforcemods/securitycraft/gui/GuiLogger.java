7ac4cebb4451a89ed25017806a1bd297ace11ea3
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-02-03 22:51:28 +0100

    add scrollable list to username logger

--- src/main/java/net/geforcemods/securitycraft/gui/GuiLogger.java
+++ src/main/java/net/geforcemods/securitycraft/gui/GuiLogger.java
@@ -1,20 +1,26 @@
 package net.geforcemods.securitycraft.gui;
 
+import java.io.IOException;
+
 import net.geforcemods.securitycraft.SecurityCraft;
 import net.geforcemods.securitycraft.containers.ContainerGeneric;
 import net.geforcemods.securitycraft.network.packets.PacketSClearLogger;
 import net.geforcemods.securitycraft.tileentity.TileEntityLogger;
 import net.geforcemods.securitycraft.util.ClientUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.client.GuiScrollingList;
 
 public class GuiLogger extends GuiContainer{
 
-	private TileEntityLogger tileEntity;
 	private static final ResourceLocation TEXTURE = new ResourceLocation("securitycraft:textures/gui/container/blank.png");
+	private TileEntityLogger tileEntity;
+	private PlayerList playerList;
 
 	public GuiLogger(InventoryPlayer inventory, TileEntityLogger te) {
 		super(new ContainerGeneric(inventory, te));
@@ -27,6 +33,7 @@ public class GuiLogger extends GuiContainer{
 		super.initGui();
 
 		addButton(new GuiButton(0, guiLeft + 4, guiTop + 4, 8, 8, "x")).enabled = tileEntity.getOwner().isOwner(mc.player);
+		playerList = new PlayerList(mc, xSize - 24, ySize - 30, guiTop + 20, guiLeft + 12, width, height);
 	}
 
 	@Override
@@ -37,6 +44,9 @@ public class GuiLogger extends GuiContainer{
 			tileEntity.players = new String[100];
 			SecurityCraft.network.sendToServer(new PacketSClearLogger(tileEntity.getPos()));
 		}
+
+		if(playerList != null)
+			playerList.actionPerformed(button);
 	}
 
 	/**
@@ -48,10 +58,6 @@ public class GuiLogger extends GuiContainer{
 		String localized = ClientUtils.localize("gui.securitycraft:logger.logged");
 
 		fontRenderer.drawString(localized, xSize / 2 - fontRenderer.getStringWidth(localized) / 2, 6, 4210752);
-
-		for(int i = 0; i < tileEntity.players.length; i++)
-			if(tileEntity.players[i] != "")
-				fontRenderer.drawString(tileEntity.players[i], xSize / 2 - fontRenderer.getStringWidth(tileEntity.players[i]) / 2, 25 + (10 * i), 4210752);
 
 		if(mouseX >= guiLeft + 4 && mouseY >= guiTop + 4 && mouseX < guiLeft + 4 + 8 && mouseY < guiTop + 4 + 8)
 			drawHoveringText(ClientUtils.localize("gui.securitycraft:editModule.clear"), mouseX - guiLeft, mouseY - guiTop);
@@ -65,6 +71,50 @@ public class GuiLogger extends GuiContainer{
 		int startX = (width - xSize) / 2;
 		int startY = (height - ySize) / 2;
 		this.drawTexturedModalRect(startX, startY, 0, 0, xSize, ySize);
+
+		if(playerList != null)
+			playerList.drawScreen(mouseX, mouseY, partialTicks);
 	}
 
+	@Override
+	protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
+	{
+		if(playerList != null)
+			playerList.handleMouseInput(mouseX, mouseY);
+
+		super.mouseClicked(mouseX, mouseY, mouseButton);
+	}
+
+	class PlayerList extends GuiScrollingList
+	{
+		public PlayerList(Minecraft client, int width, int height, int top, int left, int screenWidth, int screenHeight)
+		{
+			super(client, width, height, top, top + height, left, 10, screenWidth, screenHeight);
+		}
+
+		@Override
+		protected int getSize()
+		{
+			return tileEntity.players.length;
+		}
+
+		@Override
+		protected void elementClicked(int index, boolean doubleClick) {}
+
+		@Override
+		protected boolean isSelected(int index)
+		{
+			return index == selectedIndex;
+		}
+
+		@Override
+		protected void drawBackground() {}
+
+		@Override
+		protected void drawSlot(int slotIdx, int entryRight, int slotTop, int slotBuffer, Tessellator tess)
+		{
+			if(slotIdx >= 0 && slotIdx < tileEntity.players.length && tileEntity.players[slotIdx] != "")
+				fontRenderer.drawString(tileEntity.players[slotIdx], width / 2 - fontRenderer.getStringWidth(tileEntity.players[slotIdx]) / 2, slotTop, 0xC6C6C6);
+		}
+	}
 }
