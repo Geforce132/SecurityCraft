bdbb2f4411438934b8e29eb4961e92b5d10d1600
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-02-13 20:38:12 +0100

    fix some glass rendering issues

--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedGlassBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedGlassBlock.java
@@ -5,8 +5,11 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class ReinforcedGlassBlock extends BaseReinforcedBlock
 {
@@ -25,5 +28,18 @@ public class ReinforcedGlassBlock extends BaseReinforcedBlock
 	public BlockRenderLayer getRenderLayer()
 	{
 		return BlockRenderLayer.CUTOUT;
+	}
+
+	@Override
+	public float getAmbientOcclusionLightValue(BlockState state, IBlockReader world, BlockPos pos)
+	{
+		return 1.0F;
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public boolean isSideInvisible(BlockState state, BlockState adjacentBlockState, Direction side)
+	{
+		return adjacentBlockState.getBlock() == this ? true : super.isSideInvisible(state, adjacentBlockState, side);
 	}
 }
