01db065c45cc5558d55d314038923311f7b07a4b
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-02-13 21:06:51 +0100

    fix some glass rendering issues

--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/BlockReinforcedGlass.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/BlockReinforcedGlass.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.api.TileEntityOwnable;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockGlass;
@@ -13,8 +14,12 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockReinforcedGlass extends BlockGlass implements ITileEntityProvider, IReinforcedBlock {
 
@@ -52,5 +57,24 @@ public class BlockReinforcedGlass extends BlockGlass implements ITileEntityProvi
 	public int getAmount()
 	{
 		return 1;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
+	{
+		IBlockState offsetState = blockAccess.getBlockState(pos.offset(side));
+		Block offsetBlock = offsetState.getBlock();
+
+		if(this == SCContent.reinforcedGlass)
+		{
+			if(blockState != offsetState)
+				return true;
+
+			if(offsetBlock == this)
+				return false;
+		}
+
+		return true;
 	}
 }
--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/BlockReinforcedStainedGlass.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/BlockReinforcedStainedGlass.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.api.TileEntityOwnable;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockStainedGlass;
@@ -13,8 +14,12 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockReinforcedStainedGlass extends BlockStainedGlass implements ITileEntityProvider, IReinforcedBlock {
 
@@ -62,5 +67,24 @@ public class BlockReinforcedStainedGlass extends BlockStainedGlass implements IT
 	public int getAmount()
 	{
 		return 16;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
+	{
+		IBlockState offsetState = blockAccess.getBlockState(pos.offset(side));
+		Block offsetBlock = offsetState.getBlock();
+
+		if(this == SCContent.reinforcedStainedGlass)
+		{
+			if(blockState != offsetState)
+				return true;
+
+			if(offsetBlock == this)
+				return false;
+		}
+
+		return true;
 	}
 }
