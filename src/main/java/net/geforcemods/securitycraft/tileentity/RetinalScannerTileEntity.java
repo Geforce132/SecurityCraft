e280d019571eb3f515cd4093d3631cec04a08e3d
Lorena GdL <LorenaGdL@users.noreply.github.com>
2020-02-15 11:23:57 +0100

    [Feature] Retinal scanner renders owner face (#103)
    * [ENH] Retinal scanner now renders owner skin
    
    * PR requested changes

--- src/main/java/net/geforcemods/securitycraft/tileentity/RetinalScannerTileEntity.java
+++ src/main/java/net/geforcemods/securitycraft/tileentity/RetinalScannerTileEntity.java
@@ -1,5 +1,14 @@
 package net.geforcemods.securitycraft.tileentity;
 
+import java.util.UUID;
+
+import javax.annotation.Nullable;
+
+import com.google.common.collect.Iterables;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
+import com.mojang.authlib.properties.Property;
+
 import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.api.Option;
 import net.geforcemods.securitycraft.api.Option.BooleanOption;
@@ -11,14 +20,22 @@ import net.geforcemods.securitycraft.util.ModuleUtils;
 import net.geforcemods.securitycraft.util.PlayerUtils;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.NBTUtil;
+import net.minecraft.server.management.PlayerProfileCache;
+import net.minecraft.util.StringUtils;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
-
+import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 public class RetinalScannerTileEntity extends DisguisableTileEntity {
 
 	private BooleanOption activatedByEntities = new BooleanOption("activatedByEntities", false);
 	private BooleanOption sendMessage = new BooleanOption("sendMessage", true);
+	private GameProfile ownerProfile;
+	private static PlayerProfileCache profileCache;
+	private static MinecraftSessionService sessionService;
 
 	public RetinalScannerTileEntity()
 	{
@@ -67,4 +84,70 @@ public class RetinalScannerTileEntity extends DisguisableTileEntity {
 		return new Option[]{ activatedByEntities, sendMessage };
 	}
 
+	public static void setProfileCache(PlayerProfileCache profileCacheIn) {
+		profileCache = profileCacheIn;
+	}
+
+	public static void setSessionService(MinecraftSessionService sessionServiceIn) {
+		sessionService = sessionServiceIn;
+	}
+
+	@Override
+	public CompoundNBT write(CompoundNBT tag) {
+		super.write(tag);
+		if(!StringUtils.isNullOrEmpty(getOwner().getName()) && !(getOwner().getName().equals("owner")))
+		{
+			if(ownerProfile == null || !getOwner().getName().equals(ownerProfile.getName()))
+				setPlayerProfile(new GameProfile((UUID)null, getOwner().getName()));
+
+			updatePlayerProfile();
+			CompoundNBT ownerProfileTag = new CompoundNBT();
+			NBTUtil.writeGameProfile(ownerProfileTag, ownerProfile);
+			tag.put("ownerProfile", ownerProfileTag);
+			return tag;
+		}
+		return tag;
+	}
+
+	@Override
+	public void read(CompoundNBT tag) {
+		super.read(tag);
+		if(tag.contains("ownerProfile", Constants.NBT.TAG_COMPOUND))
+			ownerProfile = NBTUtil.readGameProfile(tag.getCompound("ownerProfile"));
+	}
+
+	@Nullable
+	public GameProfile getPlayerProfile() {
+		return ownerProfile;
+	}
+
+	public void setPlayerProfile(@Nullable GameProfile profile) {
+		ownerProfile = profile;
+	}
+
+	public void updatePlayerProfile() {
+		ownerProfile = updateGameProfile(ownerProfile);
+	}
+
+	public static GameProfile updateGameProfile(GameProfile input) {
+		if (profileCache == null)
+			setProfileCache(ServerLifecycleHooks.getCurrentServer().getPlayerProfileCache());
+		if(sessionService == null)
+			setSessionService(ServerLifecycleHooks.getCurrentServer().getMinecraftSessionService());
+
+		if (input != null && !StringUtils.isNullOrEmpty(input.getName())) {
+			if (profileCache != null && sessionService != null) {
+				GameProfile gameprofile = profileCache.getGameProfileForUsername(input.getName());
+				if (gameprofile == null)
+					return input;
+				else {
+					Property property = Iterables.getFirst(gameprofile.getProperties().get("textures"), (Property)null);
+					if (property == null) {
+						gameprofile = sessionService.fillProfileProperties(gameprofile, true);
+					}
+					return gameprofile;
+				}
+			} else return input;
+		} else return input;
+	}
 }
