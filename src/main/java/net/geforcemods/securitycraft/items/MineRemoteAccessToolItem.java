ea6c17da13fd83dbaaa36a7dea1e4626e7a4468c
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-02-23 20:09:19 +0100

    fix mrat rightclicking

--- src/main/java/net/geforcemods/securitycraft/items/MineRemoteAccessToolItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/MineRemoteAccessToolItem.java
@@ -46,12 +46,12 @@ public class MineRemoteAccessToolItem extends Item {
 	}
 
 	@Override
-	public ActionResultType onItemUse(ItemUseContext ctx)
+	public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext ctx)
 	{
-		return onItemUse(ctx.getPlayer(), ctx.getWorld(), ctx.getPos(), ctx.getItem(), ctx.getFace(), ctx.getHitVec().x, ctx.getHitVec().y, ctx.getHitVec().z);
+		return onItemUseFirst(ctx.getPlayer(), ctx.getWorld(), ctx.getPos(), stack, ctx.getFace(), ctx.getHitVec().x, ctx.getHitVec().y, ctx.getHitVec().z);
 	}
 
-	public ActionResultType onItemUse(PlayerEntity player, World world, BlockPos pos, ItemStack stack, Direction facing, double hitX, double hitY, double hitZ){
+	public ActionResultType onItemUseFirst(PlayerEntity player, World world, BlockPos pos, ItemStack stack, Direction facing, double hitX, double hitY, double hitZ){
 
 		if(!world.isRemote)
 		{
