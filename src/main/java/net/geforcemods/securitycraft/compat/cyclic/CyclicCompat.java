18c8b3a7ad3b2078dd02feb032766bf26a9e69f8
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-02-26 23:28:05 +0100

    syntax fix

--- src/main/java/net/geforcemods/securitycraft/compat/cyclic/CyclicCompat.java
+++ src/main/java/net/geforcemods/securitycraft/compat/cyclic/CyclicCompat.java
@@ -13,12 +13,12 @@ public class CyclicCompat
 	//blocks sack of holding from picking up blocks of other owners
 	public static void onRightClickBlock(RightClickBlock event)
 	{
-		if(PlayerUtils.isHoldingItem(event.getPlayer(), ItemRegistry.tile_transporterempty)))
-{
-	TileEntity te = event.getWorld().getTileEntity(event.getPos());
+		if(PlayerUtils.isHoldingItem(event.getPlayer(), ItemRegistry.tile_transporterempty))
+		{
+			TileEntity te = event.getWorld().getTileEntity(event.getPos());
 
-	if(te instanceof IOwnable && !((IOwnable)te).getOwner().isOwner(event.getPlayer()))
-		event.setUseItem(Result.DENY);
-}
+			if(te instanceof IOwnable && !((IOwnable)te).getOwner().isOwner(event.getPlayer()))
+				event.setUseItem(Result.DENY);
+		}
 	}
 }
