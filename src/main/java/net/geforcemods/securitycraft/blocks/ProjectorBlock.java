554d9420e318db99840e01b5e9a619f4e2d7652d
Geforce <Geforce132@gmail.com>
2020-06-12 23:58:39 -0500

    Improved the projector's bounding box to better match its model

--- src/main/java/net/geforcemods/securitycraft/blocks/ProjectorBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/ProjectorBlock.java
@@ -1,6 +1,7 @@
 package net.geforcemods.securitycraft.blocks;
 
 import java.util.Random;
+import java.util.stream.Stream;
 
 import net.geforcemods.securitycraft.tileentity.ProjectorTileEntity;
 import net.geforcemods.securitycraft.util.WorldUtils;
@@ -21,8 +22,10 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
@@ -31,23 +34,31 @@ import net.minecraftforge.fml.network.NetworkHooks;
 public class ProjectorBlock extends OwnableBlock {
 
 	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-	private static final VoxelShape SHAPE = Block.makeCuboidShape(0, 0, 0, 16, 10, 16);
+	private static final VoxelShape NORTH = Stream.of(Block.makeCuboidShape(3, 5, 0.9, 6, 8, 1.9), Block.makeCuboidShape(0, 3, 1, 16, 10, 16), Block.makeCuboidShape(2, 8, 0.5, 7, 9, 1), Block.makeCuboidShape(2, 4, 0.5, 7, 5, 1), Block.makeCuboidShape(6, 5, 0.5, 7, 8, 1), Block.makeCuboidShape(2, 5, 0.5, 3, 8, 1), Block.makeCuboidShape(0, 0, 1, 2, 3, 3), Block.makeCuboidShape(14, 0, 1, 16, 3, 3), Block.makeCuboidShape(14, 0, 14, 16, 3, 16), Block.makeCuboidShape(0, 0, 14, 2, 3, 16)).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).orElse(VoxelShapes.fullCube());
+	private static final VoxelShape SOUTH = Stream.of(Block.makeCuboidShape(0, 3, 0, 16, 10, 15), Block.makeCuboidShape(10, 5, 14.1, 13, 8, 15.100000000000001), Block.makeCuboidShape(9, 8, 15, 14, 9, 15.5), Block.makeCuboidShape(9, 4, 15, 14, 5, 15.5), Block.makeCuboidShape(9, 5, 15, 10, 8, 15.5), Block.makeCuboidShape(13, 5, 15, 14, 8, 15.5), Block.makeCuboidShape(14, 0, 13, 16, 3, 15), Block.makeCuboidShape(0, 0, 13, 2, 3, 15), Block.makeCuboidShape(0, 0, 0, 2, 3, 2), Block.makeCuboidShape(14, 0, 0, 16, 3, 2)).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).orElse(VoxelShapes.fullCube());
+	private static final VoxelShape WEST = Stream.of(Block.makeCuboidShape(0.5, 5, 13, 1, 8, 14), Block.makeCuboidShape(0.5, 5, 9, 1, 8, 10), Block.makeCuboidShape(0.5, 4, 9, 1, 5, 14), Block.makeCuboidShape(0.5, 8, 9, 1, 9, 14), Block.makeCuboidShape(0.75, 5, 10, 1.75, 8, 13), Block.makeCuboidShape(1, 0, 14, 3, 3, 16), Block.makeCuboidShape(14, 0, 14, 16, 3, 16), Block.makeCuboidShape(14, 0, 0, 16, 3, 2), Block.makeCuboidShape(1, 0, 0, 3, 3, 2), Block.makeCuboidShape(1, 3, 0, 16, 10, 16)).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).orElse(VoxelShapes.fullCube());
+	private static final VoxelShape EAST = Stream.of(Block.makeCuboidShape(15, 5, 2, 15.5, 8, 3), Block.makeCuboidShape(15, 5, 6, 15.5, 8, 7), Block.makeCuboidShape(15, 4, 2, 15.5, 5, 7), Block.makeCuboidShape(15, 8, 2, 15.5, 9, 7), Block.makeCuboidShape(14.25, 5, 3, 15.25, 8, 6), Block.makeCuboidShape(13, 0, 0, 15, 3, 2), Block.makeCuboidShape(0, 0, 0, 2, 3, 2), Block.makeCuboidShape(0, 0, 14, 2, 3, 16), Block.makeCuboidShape(13, 0, 14, 15, 3, 16), Block.makeCuboidShape(0, 3, 0, 15, 10, 16)).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).orElse(VoxelShapes.fullCube());
 
 	public ProjectorBlock(Properties properties) {
-		super(SoundType.METAL, properties.notSolid());
+		super(SoundType.METAL, properties);
 		setDefaultState(stateContainer.getBaseState().with(FACING, Direction.NORTH));
 	}
 
 	@Override
-	public boolean isNormalCube(BlockState state, IBlockReader reader, BlockPos pos) 
+	public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext ctx)
 	{
-		return false;
-	}
-
-	@Override
-	public VoxelShape getShape(BlockState state, IBlockReader source, BlockPos pos, ISelectionContext ctx)
-	{
-		return SHAPE;
+		switch(state.get(FACING))
+		{
+			case NORTH:
+				return SOUTH;
+			case EAST:
+				return WEST;
+			case SOUTH:
+				return NORTH;
+			case WEST:
+				return EAST;
+			default: return VoxelShapes.fullCube();
+		}
 	}
 
 	@Override
