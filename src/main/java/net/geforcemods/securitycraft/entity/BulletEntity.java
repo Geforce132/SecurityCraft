1164a7105bb9a654b110be0e41e0855afeebb0f6
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-10-04 16:58:35 +0200

    fix sentry bullets not disappearing when hitting blocks

--- src/main/java/net/geforcemods/securitycraft/entity/BulletEntity.java
+++ src/main/java/net/geforcemods/securitycraft/entity/BulletEntity.java
@@ -7,12 +7,10 @@ import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.IPacket;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceResult.Type;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
@@ -37,12 +35,7 @@ public class BulletEntity extends AbstractArrowEntity
 			remove();
 		}
 		else if(raytraceResult.getType() == Type.BLOCK)
-		{
-			BlockRayTraceResult blockraytraceresult = (BlockRayTraceResult)raytraceResult;
-			Vec3d vec3d = blockraytraceresult.getHitVec().subtract(posX, posY, posZ);
-			this.inGround = true;
-			this.setMotion(vec3d);
-		}
+			remove();
 	}
 
 	@Override
