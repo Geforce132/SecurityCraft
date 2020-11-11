7e82af9197c331b20fdec4403b5317e69dc4ce52
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-11-11 16:17:02 +0100

    fix #215

--- Changelog v1.8.x.md
+++ Changelog v1.8.x.md
@@ -1,3 +1,7 @@
+--------------------------Changelog for v1.8.21 of SecurityCraft--------------------------
+
+- Fix: Potential crash while starting the game
+
 --------------------------Changelog for v1.8.20.1 of SecurityCraft--------------------------
 
 - Fix: Crash when reinforcing blocks in the world
--- src/main/java/net/geforcemods/securitycraft/models/BlockMineModel.java
+++ src/main/java/net/geforcemods/securitycraft/models/BlockMineModel.java
@@ -1,5 +1,6 @@
 package net.geforcemods.securitycraft.models;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,6 +13,7 @@ import net.minecraft.client.renderer.model.BakedQuad;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.model.ItemOverrideList;
+import net.minecraft.client.renderer.texture.MissingTextureSprite;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.util.Direction;
 import net.minecraftforge.client.ForgeHooksClient;
@@ -45,36 +47,36 @@ public class BlockMineModel implements IBakedModel
 	@Override
 	public List<BakedQuad> getQuads(BlockState state, Direction side, Random rand)
 	{
-		return defaultModel.getQuads(state, side, rand);
+		return defaultModel == null ? new ArrayList<>() : defaultModel.getQuads(state, side, rand);
 	}
 
 	@Override
 	public boolean isAmbientOcclusion()
 	{
-		return defaultModel.isAmbientOcclusion();
+		return defaultModel == null ? true : defaultModel.isAmbientOcclusion();
 	}
 
 	@Override
 	public boolean isGui3d()
 	{
-		return defaultModel.isGui3d();
+		return defaultModel == null ? false : defaultModel.isGui3d();
 	}
 
 	@Override
 	public boolean isBuiltInRenderer()
 	{
-		return defaultModel.isBuiltInRenderer();
+		return defaultModel == null ? false : defaultModel.isBuiltInRenderer();
 	}
 
 	@Override
 	public TextureAtlasSprite getParticleTexture()
 	{
-		return defaultModel.getParticleTexture();
+		return defaultModel == null ? MissingTextureSprite.func_217790_a() : defaultModel.getParticleTexture();
 	}
 
 	@Override
 	public ItemOverrideList getOverrides()
 	{
-		return defaultModel.getOverrides();
+		return defaultModel == null ? ItemOverrideList.EMPTY : defaultModel.getOverrides();
 	}
 }
