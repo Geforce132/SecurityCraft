7b3945dd5d4f864418d8a4c6899319aa577df812
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-01-08 15:22:12 +0100

    add api hook for checking if sentries should attack other mods' entities

---
+++ src/main/java/net/geforcemods/securitycraft/api/IAttackTargetCheck.java
@@ -0,0 +1,20 @@
+package net.geforcemods.securitycraft.api;
+
+import net.minecraft.entity.Entity;
+
+/**
+ * Defines a callback that the Sentry checks before trying to attack an entity.
+ * Call <pre>InterModComms.sendTo("securitycraft", "registerSentryAttackTargetCheck", ClassThatImplementsIAttackTargetCheck::new);</pre>
+ * during InterModEnqueueEvent to register this with SecurityCraft.
+ *
+ * @author bl4ckscor3
+ */
+public interface IAttackTargetCheck
+{
+	/**
+	 * Checks if the Sentry is allowed to attack the given entity
+	 * @param potentialTarget The entity that the Sentry wants to attack
+	 * @return true if the Sentry is allowed to attack this entity, false otherwise
+	 */
+	public boolean canAttack(Entity potentialTarget);
+}
