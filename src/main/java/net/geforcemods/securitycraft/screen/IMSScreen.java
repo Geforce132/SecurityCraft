655cf5c78cc86e7edef837cd897fe2a9670f6af7
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2021-02-16 18:35:28 +0100

    Deduplicated translation keys

--- src/main/java/net/geforcemods/securitycraft/screen/IMSScreen.java
+++ src/main/java/net/geforcemods/securitycraft/screen/IMSScreen.java
@@ -73,11 +73,11 @@ public class IMSScreen extends ContainerScreen<GenericTEContainer>{
 
 	private void updateButtonText() {
 		if(targetMode == IMSTargetingMode.PLAYERS)
-			targetButton.setMessage(ClientUtils.localize("gui.securitycraft:ims.players"));
+			targetButton.setMessage(ClientUtils.localize("gui.securitycraft:srat.targets3"));
 		else if(targetMode == IMSTargetingMode.PLAYERS_AND_MOBS)
-			targetButton.setMessage(ClientUtils.localize("gui.securitycraft:ims.hostileAndPlayers"));
+			targetButton.setMessage(ClientUtils.localize("gui.securitycraft:srat.targets1"));
 		else if(targetMode == IMSTargetingMode.MOBS)
-			targetButton.setMessage(ClientUtils.localize("gui.securitycraft:ims.hostile"));
+			targetButton.setMessage(ClientUtils.localize("gui.securitycraft:srat.targets2"));
 	}
 
 }
