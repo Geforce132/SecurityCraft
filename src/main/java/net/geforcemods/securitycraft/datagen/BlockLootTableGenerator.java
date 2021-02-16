f11a6ace6230f8bcf35a040e79f7cc6f44e4d920
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-02-16 18:05:17 +0100

    strip down loot table generation code a bit more

--- src/main/java/net/geforcemods/securitycraft/datagen/BlockLootTableGenerator.java
+++ src/main/java/net/geforcemods/securitycraft/datagen/BlockLootTableGenerator.java
@@ -10,10 +10,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.api.IExplosive;
 import net.geforcemods.securitycraft.blocks.mines.IMSBlock;
 import net.geforcemods.securitycraft.blocks.reinforced.ReinforcedDoorBlock;
 import net.geforcemods.securitycraft.blocks.reinforced.ReinforcedSlabBlock;
 import net.geforcemods.securitycraft.misc.conditions.TileEntityNBTCondition;
+import net.geforcemods.securitycraft.util.RegisterItemBlock;
 import net.geforcemods.securitycraft.util.Reinforced;
 import net.minecraft.advancements.criterion.StatePropertiesPredicate;
 import net.minecraft.block.Block;
@@ -67,6 +69,15 @@ public class BlockLootTableGenerator implements IDataProvider
 					else
 						putStandardBlockLootTable(obj);
 				}
+				else if(field.isAnnotationPresent(RegisterItemBlock.class) && !field.getAnnotation(RegisterItemBlock.class).ignoreDataGen())
+				{
+					RegistryObject<Block> obj = ((RegistryObject<Block>)field.get(null));
+
+					if(obj.get() instanceof IExplosive)
+						putMineLootTable(obj);
+					else
+						putStandardBlockLootTable(obj);
+				}
 			}
 			catch(IllegalArgumentException | IllegalAccessException e)
 			{
@@ -74,26 +85,8 @@ public class BlockLootTableGenerator implements IDataProvider
 			}
 		}
 
-		putStandardBlockLootTable(SCContent.ALARM);
 		putMineLootTable(SCContent.ANCIENT_DEBRIS_MINE);
-		putStandardBlockLootTable(SCContent.BLOCK_POCKET_MANAGER);
-		putStandardBlockLootTable(SCContent.BLOCK_POCKET_WALL);
-		putMineLootTable(SCContent.BOUNCING_BETTY);
-		putStandardBlockLootTable(SCContent.CAGE_TRAP);
-		putStandardBlockLootTable(SCContent.CHISELED_CRYSTAL_QUARTZ);
-		putMineLootTable(SCContent.CLAYMORE);
-		putMineLootTable(SCContent.COAL_ORE_MINE);
-		putMineLootTable(SCContent.COBBLESTONE_MINE);
-		putStandardBlockLootTable(SCContent.CRYSTAL_QUARTZ);
-		putStandardBlockLootTable(SCContent.CRYSTAL_QUARTZ_PILLAR);
 		putSlabLootTable(SCContent.CRYSTAL_QUARTZ_SLAB);
-		putMineLootTable(SCContent.DIAMOND_ORE_MINE);
-		putMineLootTable(SCContent.DIRT_MINE);
-		putMineLootTable(SCContent.EMERALD_ORE_MINE);
-		putMineLootTable(SCContent.FURNACE_MINE);
-		putMineLootTable(SCContent.GOLD_ORE_MINE);
-		putMineLootTable(SCContent.GILDED_BLACKSTONE_MINE);
-		putMineLootTable(SCContent.GRAVEL_MINE);
 
 		StandaloneLootEntry.Builder<?> imsLootEntryBuilder = ItemLootEntry.builder(SCContent.BOUNCING_BETTY.get());
 
@@ -112,28 +105,9 @@ public class BlockLootTableGenerator implements IDataProvider
 				.addLootPool(LootPool.builder()
 						.rolls(ConstantRange.of(1))
 						.addEntry(imsLootEntryBuilder)));
-		putStandardBlockLootTable(SCContent.INVENTORY_SCANNER);
-		putStandardBlockLootTable(SCContent.IRON_FENCE);
-		putMineLootTable(SCContent.IRON_ORE_MINE);
-		putStandardBlockLootTable(SCContent.KEYCARD_READER);
 		putStandardBlockLootTable(SCContent.KEYPAD_CHEST);
 		putDoorLootTable(SCContent.KEYPAD_DOOR, SCContent.KEYPAD_DOOR_ITEM);
-		putStandardBlockLootTable(SCContent.FRAME);
-		putStandardBlockLootTable(SCContent.KEYPAD_FURNACE);
-		putStandardBlockLootTable(SCContent.KEYPAD);
-		putMineLootTable(SCContent.LAPIS_ORE_MINE);
-		putStandardBlockLootTable(SCContent.LASER_BLOCK);
-		putMineLootTable(SCContent.MINE);
-		putStandardBlockLootTable(SCContent.MOTION_ACTIVATED_LIGHT);
-		putMineLootTable(SCContent.NETHER_GOLD_ORE_MINE);
-		putStandardBlockLootTable(SCContent.PANIC_BUTTON);
-		putStandardBlockLootTable(SCContent.PORTABLE_RADAR);
-		putStandardBlockLootTable(SCContent.PROJECTOR);
-		putStandardBlockLootTable(SCContent.PROTECTO);
-		putMineLootTable(SCContent.QUARTZ_ORE_MINE);
-		putMineLootTable(SCContent.REDSTONE_ORE_MINE);
 		putDoorLootTable(SCContent.REINFORCED_DOOR, SCContent.REINFORCED_DOOR_ITEM);
-		putStandardBlockLootTable(SCContent.REINFORCED_FENCEGATE);
 		lootTables.put(SCContent.REINFORCED_IRON_BARS,
 				LootTable.builder()
 				.addLootPool(LootPool.builder()
@@ -141,10 +115,6 @@ public class BlockLootTableGenerator implements IDataProvider
 						.addEntry(ItemLootEntry.builder(SCContent.REINFORCED_IRON_BARS.get())
 								.acceptCondition(TileEntityNBTCondition.builder().equals("canDrop", true)))
 						.acceptCondition(SurvivesExplosion.builder())));
-		putStandardBlockLootTable(SCContent.REINFORCED_IRON_TRAPDOOR);
-		putStandardBlockLootTable(SCContent.REINFORCED_HOPPER);
-		putStandardBlockLootTable(SCContent.RETINAL_SCANNER);
-		putMineLootTable(SCContent.SAND_MINE);
 		putDoorLootTable(SCContent.SCANNER_DOOR, SCContent.SCANNER_DOOR_ITEM);
 		putStandardBlockLootTable(SCContent.SECRET_ACACIA_SIGN);
 		putStandardBlockLootTable(SCContent.SECRET_ACACIA_WALL_SIGN);
@@ -164,10 +134,6 @@ public class BlockLootTableGenerator implements IDataProvider
 		putStandardBlockLootTable(SCContent.SECRET_WARPED_WALL_SIGN);
 		putStandardBlockLootTable(SCContent.SECURITY_CAMERA);
 		putStandardBlockLootTable(SCContent.STAIRS_CRYSTAL_QUARTZ);
-		putMineLootTable(SCContent.STONE_MINE);
-		putMineLootTable(SCContent.TRACK_MINE);
-		putStandardBlockLootTable(SCContent.TROPHY_SYSTEM);
-		putStandardBlockLootTable(SCContent.USERNAME_LOGGER);
 	}
 
 	protected final LootTable.Builder createStandardBlockLootTable(Supplier<Block> block)
