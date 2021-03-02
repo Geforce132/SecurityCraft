5883655a3286c492b24273855c36067e4c27b5d3
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-03-02 16:41:09 +0100

    improve reinforced_block_tint config option

--- src/main/java/net/geforcemods/securitycraft/network/ClientProxy.java
+++ src/main/java/net/geforcemods/securitycraft/network/ClientProxy.java
@@ -309,7 +309,7 @@ public class ClientProxy implements IProxy
 			if(world == null || pos == null)
 				return tint;
 
-			boolean tintBlocks = ConfigHandler.SERVER.reinforcedBlockTint.get();
+			boolean tintBlocks = ConfigHandler.SERVER.forceReinforcedBlockTint.get() ? ConfigHandler.SERVER.reinforcedBlockTint.get() : ConfigHandler.CLIENT.reinforcedBlockTint.get();
 
 			if(block == SCContent.REINFORCED_GRASS_BLOCK.get() && !state.get(ReinforcedSnowyDirtBlock.SNOWY))
 			{
@@ -328,7 +328,7 @@ public class ClientProxy implements IProxy
 				return noTint;
 		}, block));
 		toTint.forEach((item, tint) -> Minecraft.getInstance().getItemColors().register((stack, tintIndex) -> {
-			boolean tintBlocks = ConfigHandler.SERVER.reinforcedBlockTint.get();
+			boolean tintBlocks = ConfigHandler.SERVER.forceReinforcedBlockTint.get() ? ConfigHandler.SERVER.reinforcedBlockTint.get() : ConfigHandler.CLIENT.reinforcedBlockTint.get();
 
 			if(item == SCContent.REINFORCED_GRASS_BLOCK.get())
 			{
