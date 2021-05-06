079521af2b28b2216d1bd32b6fd287877d473b6a
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-05-06 17:47:27 +0200

    fix fourth ims bomb not getting shot properly
    also fix ims bomb entity spawn offset to look better

--- src/main/java/net/geforcemods/securitycraft/tileentity/IMSTileEntity.java
+++ src/main/java/net/geforcemods/securitycraft/tileentity/IMSTileEntity.java
@@ -83,8 +83,8 @@ public class IMSTileEntity extends CustomizableTileEntity implements INamedConta
 			}
 
 			if (target != null) {
-				double addToX = bombsRemaining == 4 || bombsRemaining == 3 ? 1.2D : 0.55D;
-				double addToZ = bombsRemaining == 4 || bombsRemaining == 2 ? 1.2D : 0.6D;
+				double addToX = bombsRemaining == 4 || bombsRemaining == 3 ? 0.84375D : 0.0D; //0.84375 is the offset towards the bomb's position in the model
+				double addToZ = bombsRemaining == 4 || bombsRemaining == 2 ? 0.84375D : 0.0D;
 				int launchHeight = getLaunchHeight();
 				double accelerationX = target.getPosX() - pos.getX();
 				double accelerationY = target.getBoundingBox().minY + target.getHeight() / 2.0F - pos.getY() - launchHeight;
