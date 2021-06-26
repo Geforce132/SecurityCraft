411d70b4931501c9ed5611147f2bfcf2838464b8
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2021-06-26 13:50:37 +0200

    Added message to Frame notifying players that they cannot view a monitor

--- src/main/java/net/geforcemods/securitycraft/blocks/BlockFrame.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/BlockFrame.java
@@ -1,6 +1,8 @@
 package net.geforcemods.securitycraft.blocks;
 
 import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.util.PlayerUtils;
+import net.geforcemods.securitycraft.util.Utils;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
@@ -9,11 +11,13 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
@@ -40,7 +44,14 @@ public class BlockFrame extends BlockOwnable {
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
-		return player.getHeldItem(hand).getItem() == SCContent.keyPanel;
+		ItemStack stack = player.getHeldItem(hand);
+
+		if (stack.getItem() == SCContent.cameraMonitor) {
+			PlayerUtils.sendMessageToPlayer(player, Utils.localize("tile.securitycraft:keypadFrame.name"), Utils.localize("messages.securitycraft:frame.rightclick"), TextFormatting.RED);
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override
