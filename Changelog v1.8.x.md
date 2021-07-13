d58d9561e7a03306c49a01d00afdbe24346a5944
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2021-07-13 11:57:31 +0200

    Made Sentry bullets able to carry the Sentry's effects to their target

--- Changelog v1.8.x.md
+++ Changelog v1.8.x.md
@@ -2,6 +2,7 @@
 
 - New: The Frame block will now send a message when being rightclicked with a Camera Monitor, notifying the player that viewing a Camera Monitor in a Frame is currently not possible
 - New: The Trophy System now supports the Allowlist Module. Projectiles by listed players (as well as their Sentries' and I.M.S.' projectiles) will be ignored
+- New: Sentry bullets will now apply the Sentry's potion effects to their target
 - Fix: Blocks with a tint (Grass, leaves, reinforced blocks, ...) projected by the Projector are not tinted correctly
 - Fix: The Protecto does not attack players
 - Fix: The Frame's model does not match up with the vanilla iron block (this fix may break resource packs slightly)
--- src/main/java/net/geforcemods/securitycraft/entity/EntityBullet.java
+++ src/main/java/net/geforcemods/securitycraft/entity/EntityBullet.java
@@ -1,11 +1,17 @@
 package net.geforcemods.securitycraft.entity;
 
+import java.util.Collection;
+
+import com.google.common.collect.Sets;
+
 import net.geforcemods.securitycraft.api.Owner;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
@@ -14,6 +20,7 @@ import net.minecraft.world.World;
 public class EntityBullet extends EntityArrow
 {
 	private static final DataParameter<Owner> OWNER = EntityDataManager.<Owner>createKey(EntityBullet.class, Owner.getSerializer());
+	private Collection<PotionEffect> potionEffects = Sets.newHashSet();
 
 	public EntityBullet(World world)
 	{
@@ -27,6 +34,7 @@ public class EntityBullet extends EntityArrow
 
 		Owner owner =  shooter.getOwner();
 
+		this.potionEffects = shooter.getActivePotionEffects();
 		dataManager.set(OWNER, new Owner(owner.getName(), owner.getUUID()));
 		setSize(0.15F, 0.1F);
 	}
@@ -49,8 +57,17 @@ public class EntityBullet extends EntityArrow
 	@Override
 	protected void onHit(RayTraceResult raytraceResult)
 	{
-		if(raytraceResult.entityHit != null)
-			raytraceResult.entityHit.attackEntityFrom(DamageSource.causeArrowDamage(this, shootingEntity == null ? this : shootingEntity), MathHelper.ceil(MathHelper.sqrt(motionX * motionX + motionY * motionY + motionZ * motionZ) * 2.0D));
+		Entity target = raytraceResult.entityHit;
+
+		if(target != null && !(target instanceof EntitySentry)) {
+			target.attackEntityFrom(DamageSource.causeArrowDamage(this, shootingEntity == null ? this : shootingEntity), MathHelper.ceil(MathHelper.sqrt(motionX * motionX + motionY * motionY + motionZ * motionZ) * 2.0D));
+
+			if (target instanceof EntityLivingBase && !potionEffects.isEmpty()) {
+				for (PotionEffect effect : potionEffects) {
+					((EntityLivingBase)target).addPotionEffect(effect);
+				}
+			}
+		}
 
 		setDead();
 	}
