1cd1e95b8fff81ea3a6b1dfbbe0b5c1d6bcbf6aa
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2021-07-23 17:53:39 +0200

    Commented out JEI compat

--- src/main/java/net/geforcemods/securitycraft/compat/jei/SecurityCraftToVanillaCategory.java
+++ src/main/java/net/geforcemods/securitycraft/compat/jei/SecurityCraftToVanillaCategory.java
@@ -1,57 +1,45 @@
 package net.geforcemods.securitycraft.compat.jei;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-
-import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.gui.IRecipeLayout;
-import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
-import mezz.jei.api.helpers.IGuiHelper;
-import mezz.jei.api.ingredients.IIngredients;
-import net.geforcemods.securitycraft.util.Utils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
-
 public class SecurityCraftToVanillaCategory extends BaseCategory
 {
-	public SecurityCraftToVanillaCategory(IGuiHelper helper)
-	{
-		super(helper);
-	}
-
-	@Override
-	public void draw(ReinforcerRecipe recipe, PoseStack matrix, double mouseX, double mouseY)
-	{
-		Minecraft.getInstance().font.draw(matrix, OUTPUT_TEXT, 24, 30, 4210752);
-	}
-
-	@Override
-	public String getTitle()
-	{
-		return Utils.localize("jei.securitycraft.category.unreinforcing").getString();
-	}
-
-	@Override
-	public ResourceLocation getUid()
-	{
-		return SCJEIPlugin.STV_ID;
-	}
-
-	@Override
-	public void setIngredients(ReinforcerRecipe recipe, IIngredients ingredients)
-	{
-		ingredients.setInput(VanillaTypes.ITEM, new ItemStack(recipe.getSecurityCraftBlock()));
-		ingredients.setOutput(VanillaTypes.ITEM, new ItemStack(recipe.getVanillaBlock()));
-	}
-
-	@Override
-	public void setRecipe(IRecipeLayout layout, ReinforcerRecipe recipe, IIngredients ingredients)
-	{
-		IGuiItemStackGroup group = layout.getItemStacks();
-
-		group.init(0, true, 0, 25);
-		group.init(1, false, 90, 25);
-		group.set(0, new ItemStack(recipe.getSecurityCraftBlock()));
-		group.set(1, new ItemStack(recipe.getVanillaBlock()));
-	}
+//	public SecurityCraftToVanillaCategory(IGuiHelper helper)
+//	{
+//		super(helper);
+//	}
+//
+//	@Override
+//	public void draw(ReinforcerRecipe recipe, PoseStack matrix, double mouseX, double mouseY)
+//	{
+//		Minecraft.getInstance().font.draw(matrix, OUTPUT_TEXT, 24, 30, 4210752);
+//	}
+//
+//	@Override
+//	public String getTitle()
+//	{
+//		return Utils.localize("jei.securitycraft.category.unreinforcing").getString();
+//	}
+//
+//	@Override
+//	public ResourceLocation getUid()
+//	{
+//		return SCJEIPlugin.STV_ID;
+//	}
+//
+//	@Override
+//	public void setIngredients(ReinforcerRecipe recipe, IIngredients ingredients)
+//	{
+//		ingredients.setInput(VanillaTypes.ITEM, new ItemStack(recipe.getSecurityCraftBlock()));
+//		ingredients.setOutput(VanillaTypes.ITEM, new ItemStack(recipe.getVanillaBlock()));
+//	}
+//
+//	@Override
+//	public void setRecipe(IRecipeLayout layout, ReinforcerRecipe recipe, IIngredients ingredients)
+//	{
+//		IGuiItemStackGroup group = layout.getItemStacks();
+//
+//		group.init(0, true, 0, 25);
+//		group.init(1, false, 90, 25);
+//		group.set(0, new ItemStack(recipe.getSecurityCraftBlock()));
+//		group.set(1, new ItemStack(recipe.getVanillaBlock()));
+//	}
 }
--- src/main/java/net/geforcemods/securitycraft/compat/jei/VanillaToSecurityCraftCategory.java
+++ src/main/java/net/geforcemods/securitycraft/compat/jei/VanillaToSecurityCraftCategory.java
@@ -1,57 +1,45 @@
 package net.geforcemods.securitycraft.compat.jei;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-
-import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.gui.IRecipeLayout;
-import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
-import mezz.jei.api.helpers.IGuiHelper;
-import mezz.jei.api.ingredients.IIngredients;
-import net.geforcemods.securitycraft.util.Utils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
-
 public class VanillaToSecurityCraftCategory extends BaseCategory
 {
-	public VanillaToSecurityCraftCategory(IGuiHelper helper)
-	{
-		super(helper);
-	}
-
-	@Override
-	public void draw(ReinforcerRecipe recipe, PoseStack matrix, double mouseX, double mouseY)
-	{
-		Minecraft.getInstance().font.draw(matrix, OUTPUT_TEXT, 24, 5, 4210752);
-	}
-
-	@Override
-	public String getTitle()
-	{
-		return Utils.localize("jei.securitycraft.category.reinforcing").getString();
-	}
-
-	@Override
-	public ResourceLocation getUid()
-	{
-		return SCJEIPlugin.VTS_ID;
-	}
-
-	@Override
-	public void setIngredients(ReinforcerRecipe recipe, IIngredients ingredients)
-	{
-		ingredients.setInput(VanillaTypes.ITEM, new ItemStack(recipe.getVanillaBlock()));
-		ingredients.setOutput(VanillaTypes.ITEM, new ItemStack(recipe.getSecurityCraftBlock()));
-	}
-
-	@Override
-	public void setRecipe(IRecipeLayout layout, ReinforcerRecipe recipe, IIngredients ingredients)
-	{
-		IGuiItemStackGroup group = layout.getItemStacks();
-
-		group.init(0, true, 0, 0);
-		group.init(1, false, 90, 0);
-		group.set(0, new ItemStack(recipe.getVanillaBlock()));
-		group.set(1, new ItemStack(recipe.getSecurityCraftBlock()));
-	}
+//	public VanillaToSecurityCraftCategory(IGuiHelper helper)
+//	{
+//		super(helper);
+//	}
+//
+//	@Override
+//	public void draw(ReinforcerRecipe recipe, PoseStack matrix, double mouseX, double mouseY)
+//	{
+//		Minecraft.getInstance().font.draw(matrix, OUTPUT_TEXT, 24, 5, 4210752);
+//	}
+//
+//	@Override
+//	public String getTitle()
+//	{
+//		return Utils.localize("jei.securitycraft.category.reinforcing").getString();
+//	}
+//
+//	@Override
+//	public ResourceLocation getUid()
+//	{
+//		return SCJEIPlugin.VTS_ID;
+//	}
+//
+//	@Override
+//	public void setIngredients(ReinforcerRecipe recipe, IIngredients ingredients)
+//	{
+//		ingredients.setInput(VanillaTypes.ITEM, new ItemStack(recipe.getVanillaBlock()));
+//		ingredients.setOutput(VanillaTypes.ITEM, new ItemStack(recipe.getSecurityCraftBlock()));
+//	}
+//
+//	@Override
+//	public void setRecipe(IRecipeLayout layout, ReinforcerRecipe recipe, IIngredients ingredients)
+//	{
+//		IGuiItemStackGroup group = layout.getItemStacks();
+//
+//		group.init(0, true, 0, 0);
+//		group.init(1, false, 90, 0);
+//		group.set(0, new ItemStack(recipe.getVanillaBlock()));
+//		group.set(1, new ItemStack(recipe.getSecurityCraftBlock()));
+//	}
 }
