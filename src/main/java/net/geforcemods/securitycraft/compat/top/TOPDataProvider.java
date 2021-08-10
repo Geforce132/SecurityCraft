b9990850e9255a6ce15fbe6a77c5504cc37168c4
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-08-10 19:42:57 +0200

    fix compatibility with newer top releases

--- src/main/java/net/geforcemods/securitycraft/compat/top/TOPDataProvider.java
+++ src/main/java/net/geforcemods/securitycraft/compat/top/TOPDataProvider.java
@@ -28,6 +28,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -65,9 +66,9 @@ public class TOPDataProvider implements Function<ITheOneProbe, Void>
 		});
 		theOneProbe.registerProvider(new IProbeInfoProvider() {
 			@Override
-			public String getID()
+			public ResourceLocation getID()
 			{
-				return SecurityCraft.MODID + ":" + SecurityCraft.MODID;
+				return new ResourceLocation(SecurityCraft.MODID, SecurityCraft.MODID);
 			}
 
 			@Override
