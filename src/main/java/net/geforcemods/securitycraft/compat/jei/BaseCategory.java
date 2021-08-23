8da9b5374a36a844ab5cdf8cd9aa4a78eef48809
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-08-23 12:46:06 +0200

    readd jei compat

--- src/main/java/net/geforcemods/securitycraft/compat/jei/BaseCategory.java
+++ src/main/java/net/geforcemods/securitycraft/compat/jei/BaseCategory.java
@@ -1,32 +1,42 @@
 package net.geforcemods.securitycraft.compat.jei;
 
-public abstract class BaseCategory // implements IRecipeCategory<ReinforcerRecipe>
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.SecurityCraft;
+import net.geforcemods.securitycraft.util.Utils;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+public abstract class BaseCategory implements IRecipeCategory<ReinforcerRecipe>
 {
-//	protected static final TranslatableComponent OUTPUT_TEXT = Utils.localize("gui.securitycraft:blockReinforcer.output");
-//	private final IDrawable background;
-//	private final IDrawable icon;
-//
-//	public BaseCategory(IGuiHelper helper)
-//	{
-//		background = helper.createDrawable(new ResourceLocation(SecurityCraft.MODID, "textures/gui/container/universal_block_reinforcer.png"), 25, 19, 126, 43);
-//		icon = helper.createDrawableIngredient(new ItemStack(SCContent.UNIVERSAL_BLOCK_REINFORCER_LVL_3.get()));
-//	}
-//
-//	@Override
-//	public IDrawable getBackground()
-//	{
-//		return background;
-//	}
-//
-//	@Override
-//	public IDrawable getIcon()
-//	{
-//		return icon;
-//	}
-//
-//	@Override
-//	public Class<? extends ReinforcerRecipe> getRecipeClass()
-//	{
-//		return ReinforcerRecipe.class;
-//	}
+	protected static final TranslatableComponent OUTPUT_TEXT = Utils.localize("gui.securitycraft:blockReinforcer.output");
+	private final IDrawable background;
+	private final IDrawable icon;
+
+	public BaseCategory(IGuiHelper helper)
+	{
+		background = helper.createDrawable(new ResourceLocation(SecurityCraft.MODID, "textures/gui/container/universal_block_reinforcer.png"), 25, 19, 126, 43);
+		icon = helper.createDrawableIngredient(new ItemStack(SCContent.UNIVERSAL_BLOCK_REINFORCER_LVL_3.get()));
+	}
+
+	@Override
+	public IDrawable getBackground()
+	{
+		return background;
+	}
+
+	@Override
+	public IDrawable getIcon()
+	{
+		return icon;
+	}
+
+	@Override
+	public Class<? extends ReinforcerRecipe> getRecipeClass()
+	{
+		return ReinforcerRecipe.class;
+	}
 }
--- src/main/java/net/geforcemods/securitycraft/compat/jei/SlotMover.java
+++ src/main/java/net/geforcemods/securitycraft/compat/jei/SlotMover.java
@@ -1,10 +1,16 @@
 package net.geforcemods.securitycraft.compat.jei;
 
-public class SlotMover // implements IGuiContainerHandler<CustomizeBlockScreen>
+import java.util.List;
+
+import mezz.jei.api.gui.handlers.IGuiContainerHandler;
+import net.geforcemods.securitycraft.screen.CustomizeBlockScreen;
+import net.minecraft.client.renderer.Rect2i;
+
+public class SlotMover implements IGuiContainerHandler<CustomizeBlockScreen>
 {
-//	@Override
-//	public List<Rect2i> getGuiExtraAreas(CustomizeBlockScreen guiContainer)
-//	{
-//		return guiContainer.getGuiExtraAreas();
-//	}
+	@Override
+	public List<Rect2i> getGuiExtraAreas(CustomizeBlockScreen guiContainer)
+	{
+		return guiContainer.getGuiExtraAreas();
+	}
 }
