a00416d8ed536ecba31d514136523ad4d8db627f
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-08-26 02:01:01 +0200

    spoof fake fluids as well and improve code

--- src/main/java/net/geforcemods/securitycraft/mixin/DebugOverlayGuiMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/DebugOverlayGuiMixin.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.blocks.DisguisableBlock;
 import net.geforcemods.securitycraft.blocks.mines.BaseFullMineBlock;
 import net.geforcemods.securitycraft.blocks.mines.FurnaceMineBlock;
@@ -13,6 +14,10 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.overlay.DebugOverlayGui;
+import net.minecraft.fluid.FlowingFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
@@ -24,18 +29,34 @@ public class DebugOverlayGuiMixin
 	protected RayTraceResult rayTraceBlock;
 
 	@ModifyVariable(method="getDebugInfoRight", at=@At(value="INVOKE_ASSIGN", target="Lnet/minecraft/client/world/ClientWorld;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;"))
-	public BlockState spoofState(BlockState originalState)
+	public BlockState spoofBlockState(BlockState originalState)
 	{
 		Block originalBlock = originalState.getBlock();
-		BlockState disguisedState = null;
 
 		if(originalBlock instanceof DisguisableBlock)
-			disguisedState = ((DisguisableBlock)originalBlock).getDisguisedBlockState(Minecraft.getInstance().world, ((BlockRayTraceResult)rayTraceBlock).getPos());
+			return ((DisguisableBlock)originalBlock).getDisguisedBlockState(Minecraft.getInstance().world, ((BlockRayTraceResult)rayTraceBlock).getPos());
 		else if(originalBlock instanceof BaseFullMineBlock)
-			disguisedState = ((BaseFullMineBlock)originalBlock).getBlockDisguisedAs().getDefaultState();
+			return ((BaseFullMineBlock)originalBlock).getBlockDisguisedAs().getDefaultState();
 		else if(originalBlock instanceof FurnaceMineBlock)
-			disguisedState = Blocks.FURNACE.getDefaultState().with(BlockStateProperties.HORIZONTAL_FACING, originalState.get(BlockStateProperties.HORIZONTAL_FACING));
+			return Blocks.FURNACE.getDefaultState().with(BlockStateProperties.HORIZONTAL_FACING, originalState.get(BlockStateProperties.HORIZONTAL_FACING));
 
-		return disguisedState != null ? disguisedState : originalState;
+		return originalState;
+	}
+
+	@ModifyVariable(method="getDebugInfoRight", at=@At(value="INVOKE_ASSIGN", target="Lnet/minecraft/client/world/ClientWorld;getFluidState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/fluid/FluidState;"))
+	public FluidState spoofFluidState(FluidState originalState)
+	{
+		Fluid originalFluid = originalState.getFluid();
+
+		if(originalFluid == SCContent.FAKE_WATER.get())
+			return Fluids.WATER.getDefaultState().with(FlowingFluid.FALLING, originalState.get(FlowingFluid.FALLING));
+		else if(originalFluid == SCContent.FLOWING_FAKE_WATER.get())
+			return Fluids.FLOWING_WATER.getDefaultState().with(FlowingFluid.FALLING, originalState.get(FlowingFluid.FALLING)).with(FlowingFluid.LEVEL_1_8, originalState.get(FlowingFluid.LEVEL_1_8));
+		else if(originalFluid == SCContent.FAKE_LAVA.get())
+			return Fluids.LAVA.getDefaultState().with(FlowingFluid.FALLING, originalState.get(FlowingFluid.FALLING));
+		else if(originalFluid == SCContent.FLOWING_FAKE_LAVA.get())
+			return Fluids.FLOWING_LAVA.getDefaultState().with(FlowingFluid.FALLING, originalState.get(FlowingFluid.FALLING)).with(FlowingFluid.LEVEL_1_8, originalState.get(FlowingFluid.LEVEL_1_8));
+
+		return originalState;
 	}
 }
