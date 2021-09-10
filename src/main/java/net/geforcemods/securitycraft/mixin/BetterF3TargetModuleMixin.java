7a6ee5db00ad19fac38b3d6d9310a043bfc24db4
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-09-10 22:48:53 +0200

    fix mixins

--- src/main/java/net/geforcemods/securitycraft/mixin/BetterF3TargetModuleMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/BetterF3TargetModuleMixin.java
@@ -3,22 +3,25 @@ package net.geforcemods.securitycraft.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import me.cominixo.betterf3.modules.TargetModule;
 import net.geforcemods.securitycraft.misc.F3Spoofer;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.fluid.FluidState;
+import net.minecraft.util.math.BlockPos;
 
 @Mixin(TargetModule.class)
 public class BetterF3TargetModuleMixin
 {
-	//TODO: get the position somehow, without recomputing the ray trace
-	//	@ModifyVariable(method="update", at=@At(value="INVOKE_ASSIGN", target="Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;"))
-	//	public BlockState spoofBlockState(BlockState originalState)
-	//	{
-	//		return F3Spoofer.spoofBlockState(originalState, pos);
-	//	}
+	@Redirect(method="update", at=@At(value="INVOKE", target="Lnet/minecraft/client/world/ClientWorld;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;"))
+	public BlockState spoofBlockState(ClientWorld world, BlockPos pos)
+	{
+		return F3Spoofer.spoofBlockState(world.getBlockState(pos), pos);
+	}
 
-	@ModifyVariable(method="update", at=@At(value="INVOKE_ASSIGN", target="Lnet/minecraft/world/World;getFluidState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/fluid/FluidState;"))
+	@ModifyVariable(method="update", name="fluidState", at=@At(value="INVOKE_ASSIGN", target="Lnet/minecraft/client/world/ClientWorld;getFluidState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/fluid/FluidState;"))
 	public FluidState spoofFluidState(FluidState originalState)
 	{
 		return F3Spoofer.spoofFluidState(originalState);
