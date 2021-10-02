3cd79c05a8f4e37fee824b173097c1190ae65ae1
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-10-02 22:09:51 +0200

    add affect everyone setting to list modules

---
+++ src/main/java/net/geforcemods/securitycraft/screen/components/CallbackCheckbox.java
@@ -0,0 +1,59 @@
+package net.geforcemods.securitycraft.screen.components;
+
+import java.util.function.Consumer;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.widget.button.AbstractButton;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.text.ITextComponent;
+
+//copy from vanilla's Checkbox to be able to change the text color and remove the shadow
+public class CallbackCheckbox extends AbstractButton
+{
+	private static final ResourceLocation TEXTURE = new ResourceLocation("textures/gui/checkbox.png");
+	private boolean selected;
+	private final Consumer<Boolean> onChange;
+	private final int textColor;
+
+	public CallbackCheckbox(int x, int y, int width, int height, ITextComponent message, boolean selected, Consumer<Boolean> onChange, int textColor)
+	{
+		super(x, y, width, height, message);
+
+		this.selected = selected;
+		this.onChange = onChange;
+		this.textColor = textColor;
+	}
+
+	@Override
+	public void onPress()
+	{
+		selected = !selected;
+		onChange.accept(selected);
+	}
+
+	@Override
+	public void renderWidget(MatrixStack matrix, int mouseX, int mouseY, float partialTicks)
+	{
+		Minecraft minecraft = Minecraft.getInstance();
+
+		minecraft.getTextureManager().bindTexture(TEXTURE);
+		RenderSystem.enableDepthTest();
+		RenderSystem.color4f(1.0F, 1.0F, 1.0F, alpha);
+		RenderSystem.enableBlend();
+		RenderSystem.defaultBlendFunc();
+		RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+		blit(matrix, x, y, isFocused() ? 20.0F : 0.0F, selected ? 20.0F : 0.0F, 20, height, 64, 64);
+		renderBg(matrix, minecraft, mouseX, mouseY);
+		minecraft.fontRenderer.drawText(matrix, getMessage(), x + 24, y + (height - 8) / 2, textColor | MathHelper.ceil(alpha * 255.0F) << 24);
+	}
+
+	public boolean selected()
+	{
+		return selected;
+	}
+}
