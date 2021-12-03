77efc427de581e31952d40ef1e723590ce818ebf
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-03 23:43:25 +0100

    fix players not being able to see themselves when viewing a camera

--- src/main/java/net/geforcemods/securitycraft/mixin/camera/WorldRendererMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/camera/WorldRendererMixin.java
@@ -8,21 +8,34 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.geforcemods.securitycraft.util.PlayerUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.ViewFrustum;
 import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.entity.Entity;
 
-/**
- * This mixin fixes camera chunks disappearing when the player entity moves while viewing a camera (e.g. while being in a minecart or falling)
- */
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
 	@Shadow
 	@Final
 	private Minecraft mc;
 
+	/**
+	 * Fixes camera chunks disappearing when the player entity moves while viewing a camera (e.g. while being in a minecart or falling)
+	 */
 	@Redirect(method = "setupTerrain", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ViewFrustum;updateChunkPositions(DD)V"))
-	public void onRepositionCamera(ViewFrustum viewFrustum, double x, double z) {
+	private void onRepositionCamera(ViewFrustum viewFrustum, double x, double z) {
 		if (!PlayerUtils.isPlayerMountedOnCamera(mc.player))
 			viewFrustum.updateChunkPositions(x, z);
+	}
+
+	/*
+	 * Fixes players not being able to see themselves when viewing a camera
+	 */
+	@Redirect(method = "updateCameraAndRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ActiveRenderInfo;getRenderViewEntity()Lnet/minecraft/entity/Entity;", ordinal = 3))
+	private Entity makePlayerRenderable(ActiveRenderInfo activeRenderInfo) {
+		if (PlayerUtils.isPlayerMountedOnCamera(mc.player))
+			return mc.player;
+
+		return activeRenderInfo.getRenderViewEntity();
 	}
 }
