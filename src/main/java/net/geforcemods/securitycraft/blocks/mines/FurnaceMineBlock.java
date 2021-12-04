97538d929070af005dd293332622b51408f5e909
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-04 23:10:15 +0100

    let FurnaceMineBlock extends BaseFullMineBlock

--- src/main/java/net/geforcemods/securitycraft/blocks/mines/FurnaceMineBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/mines/FurnaceMineBlock.java
@@ -1,67 +1,47 @@
 package net.geforcemods.securitycraft.blocks.mines;
 
-import net.geforcemods.securitycraft.ConfigHandler;
 import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.api.OwnableBlockEntity;
-import net.geforcemods.securitycraft.compat.IOverlayDisplay;
-import net.geforcemods.securitycraft.util.BlockUtils;
 import net.geforcemods.securitycraft.util.EntityUtils;
-import net.geforcemods.securitycraft.util.IBlockMine;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class FurnaceMineBlock extends ExplosiveBlock implements IOverlayDisplay, IBlockMine {
+public class FurnaceMineBlock extends BaseFullMineBlock {
 
 	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
 
 	public FurnaceMineBlock(Block.Properties properties) {
-		super(properties);
+		super(properties, Blocks.FURNACE);
 		registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
 	}
 
 	@Override
-	public void wasExploded(Level world, BlockPos pos, Explosion explosion) {
-		if (!world.isClientSide)
-		{
-			if(pos.equals(new BlockPos(explosion.getPosition())))
-				return;
-
-			explode(world, pos);
-		}
+	public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext ctx) {
+		return Shapes.block();
 	}
 
 	@Override
-	public boolean removedByPlayer(BlockState state, Level world, BlockPos pos, Player player, boolean willHarvest, FluidState fluid){
-		if(!world.isClientSide)
-			if(player != null && player.isCreative() && !ConfigHandler.SERVER.mineExplodesWhenInCreative.get())
-				return super.removedByPlayer(state, world, pos, player, willHarvest, fluid);
-			else if(!EntityUtils.doesPlayerOwn(player, world, pos)){
-				explode(world, pos);
-				return super.removedByPlayer(state, world, pos, player, willHarvest, fluid);
-			}
-
-		return super.removedByPlayer(state, world, pos, player, willHarvest, fluid);
-	}
+	public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {}
 
 	@Override
 	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit)
@@ -86,67 +66,14 @@ public class FurnaceMineBlock extends ExplosiveBlock implements IOverlayDisplay,
 	}
 
 	@Override
-	public boolean activateMine(Level world, BlockPos pos)
-	{
-		return false;
-	}
-
-	@Override
-	public boolean defuseMine(Level world, BlockPos pos)
-	{
-		return false;
-	}
-
-	@Override
-	public void explode(Level world, BlockPos pos) {
-		if(world.isClientSide)
-			return;
-
-		world.destroyBlock(pos, false);
-		world.explode((Entity)null, pos.getX(), pos.getY(), pos.getZ(), ConfigHandler.SERVER.smallerMineExplosion.get() ? 2.5F : 5.0F, ConfigHandler.SERVER.shouldSpawnFire.get(), BlockUtils.getExplosionMode());
-
-	}
-
-	/**
-	 * Return whether this block can drop from an explosion.
-	 */
-	@Override
-	public boolean dropFromExplosion(Explosion explosion) {
-		return false;
-	}
-
-	@Override
 	protected void createBlockStateDefinition(Builder<Block, BlockState> builder)
 	{
 		builder.add(FACING);
 	}
 
 	@Override
-	public boolean isActive(Level world, BlockPos pos) {
+	public boolean explodesWhenInteractedWith() {
 		return true;
-	}
-
-	@Override
-	public boolean isDefusable() {
-		return false;
-	}
-
-	@Override
-	public ItemStack getDisplayStack(Level world, BlockState state, BlockPos pos) {
-		return new ItemStack(Blocks.FURNACE);
-	}
-
-	@Override
-	public boolean shouldShowSCInfo(Level world, BlockState state, BlockPos pos) {
-		return false;
-	}
-
-	@Override
-	public ItemStack getPickBlock(BlockState state, HitResult target, BlockGetter world, BlockPos pos, Player player) {
-		if (player.isCreative() || (world.getBlockEntity(pos) instanceof OwnableBlockEntity te && te.getOwner().isOwner(player)))
-			return super.getPickBlock(state, target, world, pos, player);
-
-		return new ItemStack(Blocks.FURNACE);
 	}
 
 	@Override
@@ -159,5 +86,10 @@ public class FurnaceMineBlock extends ExplosiveBlock implements IOverlayDisplay,
 	public BlockState mirror(BlockState state, Mirror mirror)
 	{
 		return state.rotate(mirror.getRotation(state.getValue(FACING)));
+	}
+
+	@Override
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return new OwnableBlockEntity(pos, state);
 	}
 }
