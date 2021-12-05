61620289e4eb4408151d2bc602239be413dc0b18
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-05 16:09:56 +0100

    fix furnace mine tile entity

--- src/main/java/net/geforcemods/securitycraft/blocks/mines/FurnaceMineBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/mines/FurnaceMineBlock.java
@@ -2,6 +2,7 @@ package net.geforcemods.securitycraft.blocks.mines;
 
 import net.geforcemods.securitycraft.ConfigHandler;
 import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.api.OwnableTileEntity;
 import net.geforcemods.securitycraft.util.BlockUtils;
 import net.geforcemods.securitycraft.util.EntityUtils;
 import net.minecraft.block.Block;
@@ -13,6 +14,7 @@ import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
@@ -96,5 +98,10 @@ public class FurnaceMineBlock extends BaseFullMineBlock {
 	public BlockState mirror(BlockState state, Mirror mirror)
 	{
 		return state.rotate(mirror.toRotation(state.get(FACING)));
+	}
+
+	@Override
+	public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+		return new OwnableTileEntity();
 	}
 }
