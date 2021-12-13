212451f76a4e0fc4c986f35a734581f6d19e9005
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-13 22:12:32 +0100

    open sss screen from the server

---
+++ src/main/java/net/geforcemods/securitycraft/network/client/OpenSSSScreen.java
@@ -0,0 +1,40 @@
+package net.geforcemods.securitycraft.network.client;
+
+import java.util.function.Supplier;
+
+import net.geforcemods.securitycraft.ClientHandler;
+import net.geforcemods.securitycraft.tileentity.SonicSecuritySystemTileEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+public class OpenSSSScreen {
+	private BlockPos pos;
+
+	public OpenSSSScreen() {}
+
+	public OpenSSSScreen(BlockPos pos) {
+		this.pos = pos;
+	}
+
+	public static void encode(OpenSSSScreen message, PacketBuffer buf) {
+		buf.writeBlockPos(message.pos);
+	}
+
+	public static OpenSSSScreen decode(PacketBuffer buf) {
+		return new OpenSSSScreen(buf.readBlockPos());
+	}
+
+	public static void onMessage(OpenSSSScreen message, Supplier<NetworkEvent.Context> ctx)
+	{
+		ctx.get().enqueueWork(() -> {
+			TileEntity te = Minecraft.getInstance().world.getTileEntity(message.pos);
+
+			if (te instanceof SonicSecuritySystemTileEntity)
+				ClientHandler.displaySonicSecuritySystemGui((SonicSecuritySystemTileEntity)te);
+		});
+		ctx.get().setPacketHandled(true);
+	}
+}
