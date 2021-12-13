e479419103781d08f5ecaeab158ae328b283348a
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-13 17:51:50 +0100

    fix doors not being linkable to sss

--- src/main/java/net/geforcemods/securitycraft/SCEventHandler.java
+++ src/main/java/net/geforcemods/securitycraft/SCEventHandler.java
@@ -169,7 +169,7 @@ public class SCEventHandler {
 		ItemStack stack = event.getItemStack();
 		Item item = stack.getItem();
 
-		if(!stack.isEmpty() && item != SCContent.UNIVERSAL_BLOCK_REMOVER.get() && item != SCContent.UNIVERSAL_BLOCK_MODIFIER.get() && item != SCContent.UNIVERSAL_OWNER_CHANGER.get())
+		if(!stack.isEmpty() && item != SCContent.UNIVERSAL_BLOCK_REMOVER.get() && item != SCContent.UNIVERSAL_BLOCK_MODIFIER.get() && item != SCContent.UNIVERSAL_OWNER_CHANGER.get() && item != SCContent.SONIC_SECURITY_SYSTEM_ITEM.get())
 		{
 			if(!(item instanceof BlockItem))
 			{
