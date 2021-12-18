7e0c46eda82b9697759ce987d726933699690bcf
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-18 17:28:51 +0100

    remove unused code from ReinforcedNyliumBlock

--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedNyliumBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedNyliumBlock.java
@@ -5,35 +5,18 @@ import java.util.Random;
 import net.geforcemods.securitycraft.SCContent;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.IGrowable;
-import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.Features;
 import net.minecraft.world.gen.feature.NetherVegetationFeature;
 import net.minecraft.world.gen.feature.TwistingVineFeature;
-import net.minecraft.world.lighting.LightEngine;
 import net.minecraft.world.server.ServerWorld;
 
 public class ReinforcedNyliumBlock extends BaseReinforcedBlock implements IGrowable {
 	public ReinforcedNyliumBlock(Block.Properties properties, Block vB) {
 		super(properties, vB);
-	}
-
-	@Override
-	public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-		if (!hasLightAbove(state, world, pos))
-			world.setBlockState(pos, Blocks.NETHERRACK.getDefaultState());
-	}
-
-	private static boolean hasLightAbove(BlockState state, IWorldReader world, BlockPos pos) {
-		BlockPos upperPos = pos.up();
-		BlockState upperState = world.getBlockState(upperPos);
-		int lightLevel = LightEngine.func_215613_a(world, state, pos, upperState, upperPos, Direction.UP, upperState.getOpacity(world, upperPos));
-		return lightLevel < world.getMaxLightLevel();
 	}
 
 	@Override
