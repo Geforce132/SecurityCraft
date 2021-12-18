984320c5ee68fa71aa9d1d9a48d2018c5d3666a5
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-18 11:35:46 +0100

    remove unused nbt data from sc manual

--- src/main/java/net/geforcemods/securitycraft/items/SCManualItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/SCManualItem.java
@@ -5,12 +5,9 @@ import java.util.List;
 
 import net.geforcemods.securitycraft.ClientHandler;
 import net.geforcemods.securitycraft.misc.SCManualPage;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.ListNBT;
-import net.minecraft.nbt.StringNBT;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
@@ -30,16 +27,4 @@ public class SCManualItem extends Item {
 
 		return ActionResult.resultConsume(player.getHeldItem(hand));
 	}
-
-	@Override
-	public void inventoryTick(ItemStack stack, World world, Entity entity, int slotIndex, boolean isSelected){
-		if(stack.getTag() == null){
-			ListNBT bookPages = new ListNBT();
-
-			stack.setTagInfo("pages", bookPages);
-			stack.setTagInfo("author", StringNBT.valueOf("Geforce"));
-			stack.setTagInfo("title", StringNBT.valueOf("SecurityCraft"));
-		}
-	}
-
 }
