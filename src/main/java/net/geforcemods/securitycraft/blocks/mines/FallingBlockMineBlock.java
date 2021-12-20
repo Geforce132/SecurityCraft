0c0f0ffa6ebb3508233d47421cbbab1590ed3b16
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-20 15:12:07 +0100

    fix falling blocks

--- src/main/java/net/geforcemods/securitycraft/blocks/mines/FallingBlockMineBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/mines/FallingBlockMineBlock.java
@@ -2,25 +2,23 @@ package net.geforcemods.securitycraft.blocks.mines;
 
 import java.util.Random;
 
-import net.geforcemods.securitycraft.api.IOwnable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Fallable;
+import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class FallingBlockMineBlock extends BaseFullMineBlock {
+public class FallingBlockMineBlock extends BaseFullMineBlock implements Fallable {
 	public FallingBlockMineBlock(Block.Properties properties, Block disguisedBlock) {
 		super(properties, disguisedBlock);
 	}
@@ -38,44 +36,20 @@ public class FallingBlockMineBlock extends BaseFullMineBlock {
 
 	@Override
 	public void tick(BlockState state, ServerLevel level, BlockPos pos, Random random) {
-		//TODO: 1.18 check this
-		if ((level.isEmptyBlock(pos.below()) || canFallThrough(level.getBlockState(pos.below()))) && pos.getY() >= 0) {
-			if (level.hasChunksAt(pos.offset(-32, -32, -32), pos.offset(32, 32, 32))) {
-				BlockEntity be = level.getBlockEntity(pos);
+		if (FallingBlock.isFree(level.getBlockState(pos.below())) && pos.getY() >= level.getMinBuildHeight()) {
+			BlockEntity be = level.getBlockEntity(pos);
 
-				if (!level.isClientSide && be instanceof IOwnable) {
-					FallingBlockEntity entity = new FallingBlockEntity(level, pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, level.getBlockState(pos));
-
-					entity.blockData = be.save(new CompoundTag());
-					level.addFreshEntity(entity);
-				}
-			}
-			else {
-				BlockPos landedPos;
-
-				level.destroyBlock(pos, false);
-
-				//TODO: 1.18 check this
-				for (landedPos = pos.below(); (level.isEmptyBlock(landedPos) || canFallThrough(level.getBlockState(landedPos))) && landedPos.getY() > 0; landedPos = landedPos.below()) {}
-
-				if (landedPos.getY() > 0)
-					level.setBlockAndUpdate(landedPos.above(), state); //Forge: Fix loss of state information during world gen.
-			}
+			FallingBlockEntity entity = new FallingBlockEntity(level, pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, level.getBlockState(pos));
+			entity.blockData = be.saveWithoutMetadata();
+			level.addFreshEntity(entity);
 		}
-	}
-
-	public static boolean canFallThrough(BlockState state) {
-		Block block = state.getBlock();
-		Material material = state.getMaterial();
-
-		return block == Blocks.FIRE || state.isAir() || material == Material.WATER || material == Material.LAVA;
 	}
 
 	@Override
 	@OnlyIn(Dist.CLIENT)
 	public void animateTick(BlockState state, Level level, BlockPos pos, Random rand) {
 		if (rand.nextInt(16) == 0) {
-			if (canFallThrough(level.getBlockState(pos.below()))) {
+			if (FallingBlock.isFree(level.getBlockState(pos.below()))) {
 				double particleX = pos.getX() + rand.nextFloat();
 				double particleY = pos.getY() - 0.05D;
 				double particleZ = pos.getZ() + rand.nextFloat();
--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedFallingBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedFallingBlock.java
@@ -2,21 +2,19 @@ package net.geforcemods.securitycraft.blocks.reinforced;
 
 import java.util.Random;
 
-import net.geforcemods.securitycraft.api.IOwnable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Fallable;
+import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.Material;
 
-public class ReinforcedFallingBlock extends BaseReinforcedBlock {
+public class ReinforcedFallingBlock extends BaseReinforcedBlock implements Fallable {
 	public ReinforcedFallingBlock(Block.Properties properties, Block vB) {
 		super(properties, vB);
 	}
@@ -34,46 +32,12 @@ public class ReinforcedFallingBlock extends BaseReinforcedBlock {
 
 	@Override
 	public void tick(BlockState state, ServerLevel level, BlockPos pos, Random random) {
-		if (!level.isClientSide)
-			checkFallable(level, pos);
-	}
+		if (FallingBlock.isFree(level.getBlockState(pos.below())) && pos.getY() >= level.getMinBuildHeight()) {
+			BlockEntity be = level.getBlockEntity(pos);
 
-	private void checkFallable(Level level, BlockPos pos) {
-		//TODO: check 1.18
-		if (canFallThrough(level.getBlockState(pos.below())) && pos.getY() >= 0) {
-			if (level.hasChunksAt(pos.offset(-32, -32, -32), pos.offset(32, 32, 32))) {
-				BlockEntity be = level.getBlockEntity(pos);
-
-				if (!level.isClientSide && be instanceof IOwnable) {
-					FallingBlockEntity entity = new FallingBlockEntity(level, pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, level.getBlockState(pos));
-
-					entity.blockData = be.save(new CompoundTag());
-					level.addFreshEntity(entity);
-				}
-			}
-			else {
-				BlockState state = defaultBlockState();
-
-				if (level.getBlockState(pos).getBlock() == this) {
-					state = level.getBlockState(pos);
-					level.destroyBlock(pos, false);
-				}
-
-				BlockPos landedPos;
-
-				//TODO: 1.18 check this
-				for (landedPos = pos.below(); canFallThrough(level.getBlockState(landedPos)) && landedPos.getY() > 0; landedPos = landedPos.below()) {}
-
-				if (landedPos.getY() > 0)
-					level.setBlockAndUpdate(landedPos.above(), state); //Forge: Fix loss of state information during world gen.
-			}
+			FallingBlockEntity entity = new FallingBlockEntity(level, pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, level.getBlockState(pos));
+			entity.blockData = be.saveWithoutMetadata();
+			level.addFreshEntity(entity);
 		}
-	}
-
-	public static boolean canFallThrough(BlockState state) {
-		Block block = state.getBlock();
-		Material material = state.getMaterial();
-
-		return state.isAir() || block == Blocks.FIRE || material.isLiquid() || material.isReplaceable();
 	}
 }
