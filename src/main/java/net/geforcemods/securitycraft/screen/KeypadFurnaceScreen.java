04acdb0ee138a02e8fb420855a3cb74133bfa44d
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-22 22:15:46 +0100

    fix default ppf name

--- src/main/java/net/geforcemods/securitycraft/screen/KeypadFurnaceScreen.java
+++ src/main/java/net/geforcemods/securitycraft/screen/KeypadFurnaceScreen.java
@@ -4,30 +4,24 @@ import java.util.Random;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
+import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.containers.KeypadFurnaceContainer;
-import net.geforcemods.securitycraft.tileentity.KeypadFurnaceTileEntity;
 import net.geforcemods.securitycraft.util.Utils;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.AbstractFurnaceContainer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class KeypadFurnaceScreen extends ContainerScreen<KeypadFurnaceContainer> {
 	private static final ResourceLocation FURNACE_GUI_TEXTURES = new ResourceLocation("textures/gui/container/furnace.png");
-	private KeypadFurnaceTileEntity tileFurnace;
-	private boolean gurnace = false;
 
 	public KeypadFurnaceScreen(KeypadFurnaceContainer container, PlayerInventory inv, ITextComponent name) {
-		super(container, inv, name);
-
-		tileFurnace = container.te;
-
-		if (new Random().nextInt(100) < 5)
-			gurnace = true;
+		super(container, inv, new Random().nextInt(100) < 5 ? new StringTextComponent("Keypad Gurnace") : (container.te.hasCustomName() ? container.te.getCustomName() : Utils.localize(SCContent.KEYPAD_FURNACE.get().getTranslationKey())));
 	}
 
 	@Override
@@ -38,7 +32,8 @@ public class KeypadFurnaceScreen extends ContainerScreen<KeypadFurnaceContainer>
 
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-		String s = gurnace ? "Keypad Gurnace" : (tileFurnace.hasCustomName() ? tileFurnace.getCustomName().getFormattedText() : Utils.localize("gui.securitycraft:protectedFurnace.name").getFormattedText());
+		String s = title.getFormattedText();
+
 		font.drawString(s, xSize / 2 - font.getStringWidth(s) / 2, 6.0F, 4210752);
 		font.drawString(playerInventory.getDisplayName().getFormattedText(), 8.0F, ySize - 96 + 2, 4210752);
 	}
