3abb65a55d648f17c0d53bf7f356901ff5ead6c5
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-22 22:44:18 +0100

    fix default ppf name

--- src/main/java/net/geforcemods/securitycraft/gui/GuiKeypadFurnaceInventory.java
+++ src/main/java/net/geforcemods/securitycraft/gui/GuiKeypadFurnaceInventory.java
@@ -2,6 +2,7 @@ package net.geforcemods.securitycraft.gui;
 
 import java.util.Random;
 
+import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.tileentity.TileEntityKeypadFurnace;
 import net.geforcemods.securitycraft.util.Utils;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -14,16 +15,15 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class GuiKeypadFurnaceInventory extends GuiContainer {
-	private static final ResourceLocation furnaceGuiTextures = new ResourceLocation("textures/gui/container/furnace.png");
+	private static final ResourceLocation TEXTURE = new ResourceLocation("textures/gui/container/furnace.png");
 	private TileEntityKeypadFurnace tileFurnace;
-	private boolean gurnace = false;
+	private final String s;
 
 	public GuiKeypadFurnaceInventory(InventoryPlayer inventory, TileEntityKeypadFurnace te) {
 		super(new ContainerFurnace(inventory, te));
 		tileFurnace = te;
 
-		if (new Random().nextInt(100) < 5)
-			gurnace = true;
+		s = new Random().nextInt(100) < 5 ? "Keypad Gurnace" : (te.hasCustomName() ? te.getName() : Utils.localize(SCContent.keypadFurnace).getFormattedText());
 	}
 
 	@Override
@@ -36,25 +36,26 @@ public class GuiKeypadFurnaceInventory extends GuiContainer {
 
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-		String s = gurnace ? "Keypad Gurnace" : (tileFurnace.hasCustomName() ? tileFurnace.getName() : Utils.localize("gui.securitycraft:protectedFurnace.name").getFormattedText());
 		fontRenderer.drawString(s, xSize / 2 - fontRenderer.getStringWidth(s) / 2, 6, 4210752);
 		fontRenderer.drawString(Utils.localize("container.inventory").getFormattedText(), 8, ySize - 96 + 2, 4210752);
 	}
 
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-		drawDefaultBackground();
-		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-		mc.getTextureManager().bindTexture(furnaceGuiTextures);
 		int startX = (width - xSize) / 2;
 		int startY = (height - ySize) / 2;
-		this.drawTexturedModalRect(startX, startY, 0, 0, xSize, ySize);
+
+		drawDefaultBackground();
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+		mc.getTextureManager().bindTexture(TEXTURE);
+		drawTexturedModalRect(startX, startY, 0, 0, xSize, ySize);
 
 		if (tileFurnace.isBurning()) {
 			int burnTime = tileFurnace.getBurnTimeRemainingScaled(13);
-			this.drawTexturedModalRect(startX + 56, startY + 36 + 12 - burnTime, 176, 12 - burnTime, 14, burnTime + 1);
+
+			drawTexturedModalRect(startX + 56, startY + 36 + 12 - burnTime, 176, 12 - burnTime, 14, burnTime + 1);
 			burnTime = tileFurnace.getCookProgressScaled(24);
-			this.drawTexturedModalRect(startX + 79, startY + 34, 176, 14, burnTime + 1, 16);
+			drawTexturedModalRect(startX + 79, startY + 34, 176, 14, burnTime + 1, 16);
 		}
 	}
 }
