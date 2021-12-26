ef4b7d8d67d710c48e45765e3139d63fff339eee
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-26 18:19:04 +0100

    fix keypad smoker not making any sound and update changelog

--- src/main/java/net/geforcemods/securitycraft/blocks/KeypadSmokerBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/KeypadSmokerBlock.java
@@ -1,9 +1,13 @@
 package net.geforcemods.securitycraft.blocks;
 
+import java.util.Random;
+
 import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.blockentities.AbstractKeypadFurnaceBlockEntity;
 import net.geforcemods.securitycraft.blockentities.KeypadSmokerBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -13,6 +17,12 @@ import net.minecraft.world.level.block.state.BlockState;
 public class KeypadSmokerBlock extends AbstractKeypadFurnaceBlock {
 	public KeypadSmokerBlock(Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	public void animateTick(BlockState state, Level level, BlockPos pos, Random random) {
+		if (getDisguisedStateOrDefault(state, level, pos).getBlock() == this && random.nextDouble() < 0.1D)
+			level.playLocalSound(pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, SoundEvents.SMOKER_SMOKE, SoundSource.BLOCKS, 1.0F, 1.0F, false);
 	}
 
 	@Override
