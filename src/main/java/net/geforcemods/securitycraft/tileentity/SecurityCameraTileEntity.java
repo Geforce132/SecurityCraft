261ca0dfe844241bd06af33a5dfbdbf91a41d029
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-01-18 21:07:08 +0100

    remove some unused code

--- src/main/java/net/geforcemods/securitycraft/tileentity/SecurityCameraTileEntity.java
+++ src/main/java/net/geforcemods/securitycraft/tileentity/SecurityCameraTileEntity.java
@@ -8,7 +8,6 @@ import net.geforcemods.securitycraft.api.Option.DoubleOption;
 import net.geforcemods.securitycraft.blocks.SecurityCameraBlock;
 import net.geforcemods.securitycraft.misc.ModuleType;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.util.Direction;
 
@@ -16,8 +15,6 @@ public class SecurityCameraTileEntity extends CustomizableTileEntity implements 
 	public double cameraRotation = 0.0D;
 	public boolean addToRotation = true;
 	public boolean down = false, downSet = false;
-	public float lastPitch = Float.MAX_VALUE;
-	public float lastYaw = Float.MAX_VALUE;
 	private int playersViewing = 0;
 	private DoubleOption rotationSpeedOption = new DoubleOption(this::getPos, "rotationSpeed", 0.018D, 0.01D, 0.025D, 0.001D, true);
 	private BooleanOption shouldRotateOption = new BooleanOption("shouldRotate", true);
@@ -48,20 +45,6 @@ public class SecurityCameraTileEntity extends CustomizableTileEntity implements 
 			cameraRotation -= rotationSpeedOption.get();
 		else
 			addToRotation = true;
-	}
-
-	@Override
-	public CompoundNBT write(CompoundNBT tag) {
-		tag.putFloat("LastPitch", lastPitch);
-		tag.putFloat("LastYaw", lastYaw);
-		return super.write(tag);
-	}
-
-	@Override
-	public void read(CompoundNBT tag) {
-		super.read(tag);
-		lastPitch = tag.getFloat("LastPitch");
-		lastYaw = tag.getFloat("LastYaw");
 	}
 
 	@Override
