4b082c3bfbc300c0fce913f7568b2aba92c56535
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-02-06 17:47:44 +0100

    Fixed patron list only showing 33 patrons

--- src/main/java/net/geforcemods/securitycraft/screen/SCManualScreen.java
+++ src/main/java/net/geforcemods/securitycraft/screen/SCManualScreen.java
@@ -597,7 +597,7 @@ public class SCManualScreen extends Screen {
 
 		@Override
 		protected int getContentHeight() {
-			int height = 50 + (patrons.size() * font.lineHeight);
+			int height = patrons.size() * (font.lineHeight + 3);
 
 			if (height < bottom - top - 8)
 				height = bottom - top - 8;
