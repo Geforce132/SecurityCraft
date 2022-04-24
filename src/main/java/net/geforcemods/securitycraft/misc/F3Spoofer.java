b012b61079cdb45988379808c6354d56bfeba266
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-04-24 17:57:20 +0200

    don't spoof f3 menu in dev environment

--- src/main/java/net/geforcemods/securitycraft/misc/F3Spoofer.java
+++ src/main/java/net/geforcemods/securitycraft/misc/F3Spoofer.java
@@ -14,17 +14,20 @@ import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 public class F3Spoofer {
 	public static BlockState spoofBlockState(BlockState originalState, BlockPos pos) {
 		Block originalBlock = originalState.getBlock();
 
-		if (originalBlock instanceof DisguisableBlock)
-			return ((DisguisableBlock) originalBlock).getDisguisedStateOrDefault(originalState, Minecraft.getInstance().level, pos);
-		else if (originalBlock instanceof BaseFullMineBlock)
-			return ((BaseFullMineBlock) originalBlock).getBlockDisguisedAs().defaultBlockState();
-		else if (originalBlock instanceof FurnaceMineBlock)
-			return Blocks.FURNACE.defaultBlockState().setValue(BlockStateProperties.HORIZONTAL_FACING, originalState.getValue(BlockStateProperties.HORIZONTAL_FACING));
+		if (FMLEnvironment.production) {
+			if (originalBlock instanceof DisguisableBlock)
+				return ((DisguisableBlock) originalBlock).getDisguisedStateOrDefault(originalState, Minecraft.getInstance().level, pos);
+			else if (originalBlock instanceof BaseFullMineBlock)
+				return ((BaseFullMineBlock) originalBlock).getBlockDisguisedAs().defaultBlockState();
+			else if (originalBlock instanceof FurnaceMineBlock)
+				return Blocks.FURNACE.defaultBlockState().setValue(BlockStateProperties.HORIZONTAL_FACING, originalState.getValue(BlockStateProperties.HORIZONTAL_FACING));
+		}
 
 		return originalState;
 	}
@@ -32,14 +35,16 @@ public class F3Spoofer {
 	public static FluidState spoofFluidState(FluidState originalState) {
 		Fluid originalFluid = originalState.getType();
 
-		if (originalFluid == SCContent.FAKE_WATER.get())
-			return Fluids.WATER.defaultFluidState().setValue(FlowingFluid.FALLING, originalState.getValue(FlowingFluid.FALLING));
-		else if (originalFluid == SCContent.FLOWING_FAKE_WATER.get())
-			return Fluids.FLOWING_WATER.defaultFluidState().setValue(FlowingFluid.FALLING, originalState.getValue(FlowingFluid.FALLING)).setValue(FlowingFluid.LEVEL, originalState.getValue(FlowingFluid.LEVEL));
-		else if (originalFluid == SCContent.FAKE_LAVA.get())
-			return Fluids.LAVA.defaultFluidState().setValue(FlowingFluid.FALLING, originalState.getValue(FlowingFluid.FALLING));
-		else if (originalFluid == SCContent.FLOWING_FAKE_LAVA.get())
-			return Fluids.FLOWING_LAVA.defaultFluidState().setValue(FlowingFluid.FALLING, originalState.getValue(FlowingFluid.FALLING)).setValue(FlowingFluid.LEVEL, originalState.getValue(FlowingFluid.LEVEL));
+		if (FMLEnvironment.production) {
+			if (originalFluid == SCContent.FAKE_WATER.get())
+				return Fluids.WATER.defaultFluidState().setValue(FlowingFluid.FALLING, originalState.getValue(FlowingFluid.FALLING));
+			else if (originalFluid == SCContent.FLOWING_FAKE_WATER.get())
+				return Fluids.FLOWING_WATER.defaultFluidState().setValue(FlowingFluid.FALLING, originalState.getValue(FlowingFluid.FALLING)).setValue(FlowingFluid.LEVEL, originalState.getValue(FlowingFluid.LEVEL));
+			else if (originalFluid == SCContent.FAKE_LAVA.get())
+				return Fluids.LAVA.defaultFluidState().setValue(FlowingFluid.FALLING, originalState.getValue(FlowingFluid.FALLING));
+			else if (originalFluid == SCContent.FLOWING_FAKE_LAVA.get())
+				return Fluids.FLOWING_LAVA.defaultFluidState().setValue(FlowingFluid.FALLING, originalState.getValue(FlowingFluid.FALLING)).setValue(FlowingFluid.LEVEL, originalState.getValue(FlowingFluid.LEVEL));
+		}
 
 		return originalState;
 	}
