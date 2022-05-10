Geforce132/SecurityCraft

commit f7e566ea697bbf33eb35a2bb3effdaa6cadbadea

fix several blocks not respecting team ownership

src/main/java/net/geforcemods/securitycraft/api/Owner.java
@@ -74,8 +74,8 @@ public class Owner {
 			String uuidToCheck = ownable.getOwner().getUUID();
 			String nameToCheck = ownable.getOwner().getName();
 
-			if (ConfigHandler.enableTeamOwnership && !PlayerUtils.areOnSameTeam(ownerName, nameToCheck))
-				return false;
+			if (ConfigHandler.enableTeamOwnership && PlayerUtils.areOnSameTeam(ownerName, nameToCheck))
+				continue;
 
 			// Check the player's UUID first.
 			if (uuidToCheck != null && !uuidToCheck.equals(ownerUUID))

src/main/java/net/geforcemods/securitycraft/tileentity/TileEntityRetinalScanner.java
@@ -72,7 +72,7 @@ public class TileEntityRetinalScanner extends TileEntityDisguisable implements I
 					if (ConfigHandler.trickScannersWithPlayerHeads && player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() == Items.SKULL)
 						name = PlayerUtils.getNameOfSkull(player);
 
-					if (name == null || (!getOwner().getName().equals(name) && !ModuleUtils.isAllowed(this, name))) {
+					if (!getOwner().isOwner(player) && !ModuleUtils.isAllowed(this, player)) {
 						PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.retinalScanner), Utils.localize("messages.securitycraft:retinalScanner.notOwner", PlayerUtils.getOwnerComponent(getOwner().getName())), TextFormatting.RED);
 						return true;
 					}

src/main/java/net/geforcemods/securitycraft/tileentity/TileEntityScannerDoor.java
@@ -47,7 +47,7 @@ public class TileEntityScannerDoor extends TileEntitySpecialDoor implements IVie
 				if (ConfigHandler.trickScannersWithPlayerHeads && player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() == Items.SKULL)
 					name = PlayerUtils.getNameOfSkull(player);
 
-				if (name == null || (!getOwner().getName().equals(name) && !ModuleUtils.isAllowed(this, name))) {
+				if (!getOwner().isOwner(player) && !ModuleUtils.isAllowed(this, player)) {
 					PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.retinalScanner), Utils.localize("messages.securitycraft:retinalScanner.notOwner", PlayerUtils.getOwnerComponent(getOwner().getName())), TextFormatting.RED);
 					return true;
 				}

src/main/java/net/geforcemods/securitycraft/tileentity/TileEntityPortableRadar.java
@@ -44,7 +44,7 @@ public class TileEntityPortableRadar extends CustomizableSCTE implements ITickab
 				if (isModuleEnabled(EnumModuleType.ALLOWLIST))
 					isNotAllowed = !ModuleUtils.isAllowed(this, e);
 
-				return e != owner && isNotAllowed && !e.isSpectator() && !EntityUtils.isInvisible(e);
+				return !getOwner().isOwner(e) && isNotAllowed && !e.isSpectator() && !EntityUtils.isInvisible(e);
 			});
 
 			if (isModuleEnabled(EnumModuleType.REDSTONE)) {

commit f790145ac1c939d16315449ca18e70ee5ced32a0

fix player head tricking being broken and not checking for allowlist
it broke in f7e566ea697bbf33eb35a2bb3effdaa6cadbadea

src/main/java/net/geforcemods/securitycraft/api/Owner.java
@@ -24,6 +24,11 @@ public class Owner {
 
 	public Owner() {}
 
+	public Owner(EntityPlayer player) {
+		ownerName = player.getName();
+		ownerUUID = player.getGameProfile().getId().toString();
+	}
+
 	public Owner(String playerName, String playerUUID) {
 		this.ownerName = playerName;
 		this.ownerUUID = playerUUID;
@@ -96,8 +101,15 @@ public class Owner {
 		if (player == null)
 			return false;
 
-		String uuidToCheck = player.getGameProfile().getId().toString();
-		String nameToCheck = player.getName();
+		return isOwner(new Owner(player));
+	}
+
+	/**
+	 * @return If this person is the same person as the given owner.
+	 */
+	public boolean isOwner(Owner owner) {
+		String uuidToCheck = owner.getUUID();
+		String nameToCheck = owner.getName();
 
 		if (ConfigHandler.enableTeamOwnership && PlayerUtils.areOnSameTeam(ownerName, nameToCheck))
 			return true;

src/main/java/net/geforcemods/securitycraft/tileentity/TileEntityRetinalScanner.java
@@ -19,6 +19,7 @@ import net.geforcemods.securitycraft.api.IViewActivated;
 import net.geforcemods.securitycraft.api.Option;
 import net.geforcemods.securitycraft.api.Option.OptionBoolean;
 import net.geforcemods.securitycraft.api.Option.OptionInt;
+import net.geforcemods.securitycraft.api.Owner;
 import net.geforcemods.securitycraft.blocks.BlockRetinalScanner;
 import net.geforcemods.securitycraft.misc.EnumModuleType;
 import net.geforcemods.securitycraft.util.BlockUtils;
@@ -64,18 +65,20 @@ public class TileEntityRetinalScanner extends TileEntityDisguisable implements I
 				return false;
 
 			if (!state.getValue(BlockRetinalScanner.POWERED) && !EntityUtils.isInvisible(entity)) {
-				String name = entity.getName();
-
 				if (entity instanceof EntityPlayer) {
 					EntityPlayer player = (EntityPlayer) entity;
+					Owner viewingPlayer = new Owner(player);
 
 					if (ConfigHandler.trickScannersWithPlayerHeads && player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() == Items.SKULL)
-						name = PlayerUtils.getNameOfSkull(player);
+						viewingPlayer = PlayerUtils.getSkullOwner(player);
 
-					if (!getOwner().isOwner(player) && !ModuleUtils.isAllowed(this, player)) {
+					if (!getOwner().isOwner(viewingPlayer) && !ModuleUtils.isAllowed(this, viewingPlayer.getName())) {
 						PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.retinalScanner), Utils.localize("messages.securitycraft:retinalScanner.notOwner", PlayerUtils.getOwnerComponent(getOwner().getName())), TextFormatting.RED);
 						return true;
 					}
+
+					if (sendMessage.get())
+						PlayerUtils.sendMessageToPlayer((EntityPlayer) entity, Utils.localize("tile.securitycraft:retinalScanner.name"), Utils.localize("messages.securitycraft:retinalScanner.hello", viewingPlayer.getName()), TextFormatting.GREEN);
 				}
 				else if (activatedOnlyByPlayer())
 					return false;
@@ -83,10 +86,6 @@ public class TileEntityRetinalScanner extends TileEntityDisguisable implements I
 				world.setBlockState(pos, state.withProperty(BlockRetinalScanner.POWERED, true));
 				BlockUtils.updateIndirectNeighbors(world, pos, SCContent.retinalScanner);
 				world.scheduleUpdate(pos, SCContent.retinalScanner, getSignalLength());
-
-				if (entity instanceof EntityPlayer && sendMessage.get())
-					PlayerUtils.sendMessageToPlayer((EntityPlayer) entity, Utils.localize("tile.securitycraft:retinalScanner.name"), Utils.localize("messages.securitycraft:retinalScanner.hello", name), TextFormatting.GREEN);
-
 				return true;
 			}
 		}

src/main/java/net/geforcemods/securitycraft/tileentity/TileEntityScannerDoor.java
@@ -4,6 +4,7 @@ import net.geforcemods.securitycraft.ConfigHandler;
 import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.api.ILockable;
 import net.geforcemods.securitycraft.api.IViewActivated;
+import net.geforcemods.securitycraft.api.Owner;
 import net.geforcemods.securitycraft.blocks.BlockScannerDoor;
 import net.geforcemods.securitycraft.util.EntityUtils;
 import net.geforcemods.securitycraft.util.ModuleUtils;
@@ -18,6 +19,7 @@ import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.common.util.Constants;
 
 public class TileEntityScannerDoor extends TileEntitySpecialDoor implements IViewActivated, ILockable {
 	private int viewCooldown = 0;
@@ -42,12 +44,12 @@ public class TileEntityScannerDoor extends TileEntitySpecialDoor implements IVie
 			EntityPlayer player = (EntityPlayer) entity;
 
 			if (!isLocked()) {
-				String name = entity.getName();
+				Owner viewingPlayer = new Owner(player);
 
 				if (ConfigHandler.trickScannersWithPlayerHeads && player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() == Items.SKULL)
-					name = PlayerUtils.getNameOfSkull(player);
+					viewingPlayer = PlayerUtils.getSkullOwner(player);
 
-				if (!getOwner().isOwner(player) && !ModuleUtils.isAllowed(this, player)) {
+				if (!getOwner().isOwner(viewingPlayer) && !ModuleUtils.isAllowed(this, viewingPlayer.getName())) {
 					PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.retinalScanner), Utils.localize("messages.securitycraft:retinalScanner.notOwner", PlayerUtils.getOwnerComponent(getOwner().getName())), TextFormatting.RED);
 					return true;
 				}
@@ -58,13 +60,13 @@ public class TileEntityScannerDoor extends TileEntitySpecialDoor implements IVie
 				world.setBlockState(pos, upperState.withProperty(BlockDoor.OPEN, open), 3);
 				world.setBlockState(pos.down(), lowerState.withProperty(BlockDoor.OPEN, open), 3);
 				world.markBlockRangeForRenderUpdate(pos.down(), pos);
-				world.playEvent(null, open ? 1005 : 1011, pos, 0);
+				world.playEvent(null, open ? Constants.WorldEvents.IRON_DOOR_OPEN_SOUND : Constants.WorldEvents.IRON_DOOR_CLOSE_SOUND, pos, 0);
 
 				if (open && length > 0)
 					world.scheduleUpdate(pos, SCContent.scannerDoor, length);
 
 				if (open && sendsMessages())
-					PlayerUtils.sendMessageToPlayer(player, Utils.localize("item.securitycraft:scannerDoorItem.name"), Utils.localize("messages.securitycraft:retinalScanner.hello", name), TextFormatting.GREEN);
+					PlayerUtils.sendMessageToPlayer(player, Utils.localize("item.securitycraft:scannerDoorItem.name"), Utils.localize("messages.securitycraft:retinalScanner.hello", viewingPlayer.getName()), TextFormatting.GREEN);
 
 				return true;
 			}

src/main/java/net/geforcemods/securitycraft/util/PlayerUtils.java
@@ -3,8 +3,11 @@ package net.geforcemods.securitycraft.util;
 import java.util.Iterator;
 import java.util.List;
 
+import com.mojang.authlib.GameProfile;
+
 import net.geforcemods.securitycraft.ConfigHandler;
 import net.geforcemods.securitycraft.SecurityCraft;
+import net.geforcemods.securitycraft.api.Owner;
 import net.geforcemods.securitycraft.entity.camera.EntitySecurityCamera;
 import net.geforcemods.securitycraft.network.ClientProxy;
 import net.minecraft.client.Minecraft;
@@ -18,6 +21,7 @@ import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTUtil;
 import net.minecraft.scoreboard.ScorePlayerTeam;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumHand;
@@ -233,19 +237,26 @@ public class PlayerUtils {
 	 * @param player The player to check
 	 * @return The name of the skull owner, null if the player is not wearing a player head or the skull owner is faulty
 	 */
-	public static String getNameOfSkull(EntityPlayer player) {
+	public static Owner getSkullOwner(EntityPlayer player) {
 		ItemStack stack = player.getItemStackFromSlot(EntityEquipmentSlot.HEAD);
 
 		if (stack.getItem() == Items.SKULL && stack.hasTagCompound()) {
 			NBTTagCompound stackTag = stack.getTagCompound();
 
 			if (stackTag.hasKey("SkullOwner", Constants.NBT.TAG_STRING))
-				return stackTag.getString("SkullOwner");
+				return new Owner(stackTag.getString("SkullOwner"), "ownerUUID");
 			else if (stackTag.hasKey("SkullOwner", Constants.NBT.TAG_COMPOUND)) {
-				NBTTagCompound skullOwnerTag = stackTag.getCompoundTag("SkullOwner");
+				GameProfile profile = NBTUtil.readGameProfileFromNBT(stackTag.getCompoundTag("SkullOwner"));
+				String name = "ownerName";
+				String uuid = "ownerUUID";
 
-				if (skullOwnerTag.hasKey("Name", Constants.NBT.TAG_STRING))
-					return skullOwnerTag.getString("Name");
+				if (profile.getName() != null)
+					name = profile.getName();
+
+				if (profile.getId() != null)
+					uuid = profile.getId().toString();
+
+				return new Owner(name, uuid);
 			}
 		}
 