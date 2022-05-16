4b261265a4825d49f0033da3f6458746792616fe
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-05-16 21:02:31 +0200

    Fixed Reinforced Cobweb being destroyable by hand

--- src/main/java/net/geforcemods/securitycraft/SCContent.java
+++ src/main/java/net/geforcemods/securitycraft/SCContent.java
@@ -1669,7 +1669,7 @@ public class SCContent {
 	@HasManualPage(PageGroup.REINFORCED)
 	@OwnableBE
 	@Reinforced
-	public static final RegistryObject<Block> REINFORCED_COBWEB = BLOCKS.register("reinforced_cobweb", () -> new ReinforcedCobwebBlock(Block.Properties.of(Material.CACTUS, MaterialColor.WOOL).noCollission(), Blocks.COBWEB));
+	public static final RegistryObject<Block> REINFORCED_COBWEB = BLOCKS.register("reinforced_cobweb", () -> new ReinforcedCobwebBlock(prop(Material.CACTUS, MaterialColor.WOOL).noCollission(), Blocks.COBWEB));
 	@HasManualPage(PageGroup.REINFORCED)
 	@OwnableBE
 	@Reinforced
