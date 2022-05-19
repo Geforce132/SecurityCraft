32bd99d136f71e73085e75d1c48fa8c1d0576d7e
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-05-19 19:47:22 +0200

    Removed redundant method parameters and added missing method

--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedGlazedTerracottaBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedGlazedTerracottaBlock.java
@@ -41,4 +41,9 @@ public class ReinforcedGlazedTerracottaBlock extends BaseReinforcedBlock {
 	public BlockState mirror(BlockState state, Mirror mirror) {
 		return state.rotate(mirror.getRotation(state.getValue(FACING)));
 	}
+
+	@Override
+	public BlockState getConvertedState(BlockState vanillaState) {
+		return defaultBlockState().setValue(FACING, vanillaState.getValue(FACING));
+	}
 }
