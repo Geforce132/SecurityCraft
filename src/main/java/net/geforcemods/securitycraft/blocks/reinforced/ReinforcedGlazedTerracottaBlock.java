0d4c09e4570af68771a44dc8fc96b615a9974f99
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-05-19 19:35:56 +0200

    Added Reinforced Glazed Terracotta

---
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedGlazedTerracottaBlock.java
@@ -0,0 +1,49 @@
+package net.geforcemods.securitycraft.blocks.reinforced;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.material.PushReaction;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+
+public class ReinforcedGlazedTerracottaBlock extends BaseReinforcedBlock {
+	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+	public ReinforcedGlazedTerracottaBlock(Properties properties, Block vB) {
+		super(properties, vB);
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext ctx) {
+		return defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+	}
+
+	@Override
+	public PushReaction getPistonPushReaction(BlockState state) {
+		return PushReaction.PUSH_ONLY;
+	}
+
+	@Override
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+	}
+
+	@Override
+	public BlockState mirror(BlockState state, Mirror mirror) {
+		return state.rotate(mirror.getRotation(state.getValue(FACING)));
+	}
+
+	@Override
+	public BlockState getConvertedState(BlockState vanillaState) {
+		return defaultBlockState().setValue(FACING, vanillaState.getValue(FACING));
+	}
+}
