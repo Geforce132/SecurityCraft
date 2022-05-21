05ace7828d290bb756fbcd997d111405d8069e91
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-05-21 20:08:51 +0200

    fix motion activated light detecting sentries and armor stands

--- src/main/java/net/geforcemods/securitycraft/blockentities/MotionActivatedLightBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/MotionActivatedLightBlockEntity.java
@@ -7,11 +7,13 @@ import net.geforcemods.securitycraft.api.CustomizableBlockEntity;
 import net.geforcemods.securitycraft.api.Option;
 import net.geforcemods.securitycraft.api.Option.DoubleOption;
 import net.geforcemods.securitycraft.blocks.MotionActivatedLightBlock;
+import net.geforcemods.securitycraft.entity.Sentry;
 import net.geforcemods.securitycraft.misc.ModuleType;
 import net.geforcemods.securitycraft.util.EntityUtils;
 import net.geforcemods.securitycraft.util.ITickingBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
@@ -30,7 +32,7 @@ public class MotionActivatedLightBlockEntity extends CustomizableBlockEntity imp
 		if (cooldown-- > 0)
 			return;
 
-		List<LivingEntity> entities = level.getEntitiesOfClass(LivingEntity.class, new AABB(pos).inflate(searchRadiusOption.get()), e -> !EntityUtils.isInvisible(e) && !e.isSpectator());
+		List<LivingEntity> entities = level.getEntitiesOfClass(LivingEntity.class, new AABB(pos).inflate(searchRadiusOption.get()), e -> !EntityUtils.isInvisible(e) && !e.isSpectator() && !(e instanceof Sentry || e instanceof ArmorStand));
 		boolean shouldBeOn = !entities.isEmpty();
 
 		if (state.getValue(MotionActivatedLightBlock.LIT) != shouldBeOn)
