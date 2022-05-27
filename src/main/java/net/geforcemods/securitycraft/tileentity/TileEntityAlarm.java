2006737d4dc4264afa5e7f68328ef9f955d3d254
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-05-28 00:06:26 +0200

    fix #331

--- src/main/java/net/geforcemods/securitycraft/tileentity/TileEntityAlarm.java
+++ src/main/java/net/geforcemods/securitycraft/tileentity/TileEntityAlarm.java
@@ -9,8 +9,9 @@ import net.geforcemods.securitycraft.blocks.BlockAlarm;
 import net.geforcemods.securitycraft.blocks.BlockOldLitAlarm;
 import net.geforcemods.securitycraft.misc.EnumModuleType;
 import net.geforcemods.securitycraft.misc.SCSounds;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.play.server.SPacketSoundEffect;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.SoundCategory;
 
@@ -36,9 +37,13 @@ public class TileEntityAlarm extends CustomizableSCTE implements ITickable {
 			return;
 		}
 
-		if (isPowered && --cooldown <= 0) {
-			for (EntityPlayer player : world.getPlayers(EntityPlayer.class, p -> p.getPosition().distanceSq(pos) <= Math.pow(range.get(), 2))) {
-				world.playSound(player, player.getPosition(), SCSounds.ALARM.event, SoundCategory.BLOCKS, 0.3F, 1.0F);
+		if (!world.isRemote && isPowered && --cooldown <= 0) {
+			double rangeSqr = Math.pow(range.get(), 2);
+
+			for (EntityPlayerMP player : world.getPlayers(EntityPlayerMP.class, p -> p.getPosition().distanceSq(pos) <= rangeSqr)) {
+				float volume = (float) (1.0F - ((player.getPosition().distanceSq(pos)) / rangeSqr));
+
+				player.connection.sendPacket(new SPacketSoundEffect(SCSounds.ALARM.event, SoundCategory.BLOCKS, pos.getX(), pos.getY(), pos.getZ(), volume, 1.0F));
 			}
 
 			setCooldown(delay.get() * 20);
