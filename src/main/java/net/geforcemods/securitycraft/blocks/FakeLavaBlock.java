2a9f87a1c28ec4d36408f24cafbb910253f2cea6
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-05-27 20:45:37 +0200

    remove unused import
    from 6b44feab9fc6a073a73fbc700fceb87f5113fa31 <.<

--- src/main/java/net/geforcemods/securitycraft/blocks/FakeLavaBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/FakeLavaBlock.java
@@ -10,7 +10,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.fluid.FlowingFluid;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
-import net.minecraft.util.SharedConstants;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
