839c2fc097a0c4c31134f199afa66a0e16d3653e
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-08-09 14:34:48 +0200

    fix nether portals being able to replace reinforced blocks

---
+++ src/main/java/net/geforcemods/securitycraft/mixin/PortalForcerMixin.java
@@ -0,0 +1,29 @@
+package net.geforcemods.securitycraft.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.geforcemods.securitycraft.blocks.reinforced.IReinforcedBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.portal.PortalForcer;
+
+/**
+ * Makes sure that a portal cannot spawn and replace reinforced blocks with its bottom layer. The rest of the portal frame
+ * cannot replace reinforced blocks, as it needs empty blocks to spawn there.
+ */
+@Mixin(PortalForcer.class)
+public class PortalForcerMixin {
+	@Redirect(method = "canHostFrame", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"))
+	private BlockState addReinforcedBlockCheck(ServerLevel level, BlockPos offsetPos) {
+		BlockState offsetState = level.getBlockState(offsetPos);
+
+		if (offsetState.getBlock() instanceof IReinforcedBlock)
+			return Blocks.AIR.defaultBlockState(); //this causes the check to pass (because air's material is not solid) and return false for "canHostFrame"
+		else
+			return offsetState; //block is not a reinforced block, proceed as normal
+	}
+}
