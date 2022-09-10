Geforce132/SecurityCraft

commit bf960c3b3abbe5ac65928e339fc46f19005a51ce

fix sentry name not showing up in srat

src/main/java/net/geforcemods/securitycraft/screen/SentryRemoteAccessToolScreen.java
@@ -34,7 +34,6 @@ public class SentryRemoteAccessToolScreen extends Screen {
 	private final Component modifyAll = Utils.localize("gui.securitycraft:srat.modifyAll");
 	private ItemStack srat;
 	private Button[][] guiButtons = new Button[12][3]; // 12 sentries, 3 actions (mode, targets, unbind)
-	private Component[] names = new Component[12];
 	private Button[] guiButtonsGlobal = new Button[3];
 	private static final int MODE = 0, TARGETS = 1, UNBIND = 2;
 	private int xSize = 440, ySize = 215;
@@ -110,11 +109,6 @@ public class SentryRemoteAccessToolScreen extends Screen {
 
 				guiButtons[i][UNBIND].active = true;
 
-				if (names[i] != null)
-					lines[i] = names[i];
-				else
-					lines[i] = Utils.getFormattedCoordinates(sentryPos);
-
 				if (Minecraft.getInstance().player.level.isLoaded(sentryPos) && isSentryVisibleToPlayer(sentryPos)) {
 					List<Sentry> sentries = Minecraft.getInstance().player.level.getEntitiesOfClass(Sentry.class, new AABB(sentryPos));
 
@@ -129,9 +123,11 @@ public class SentryRemoteAccessToolScreen extends Screen {
 							int nameY = startY + (i % 6) * 30 + 7;
 							TextHoverChecker posTooltipText = new TextHoverChecker(nameY + 4, nameY + 18, nameX, nameX + nameWidth + 2, line);
 
-							names[i] = sentry.getCustomName();
+							lines[i] = sentry.getCustomName();
 							hoverCheckers.add(posTooltipText);
 						}
+						else
+							lines[i] = Utils.getFormattedCoordinates(sentryPos);
 
 						guiButtons[i][MODE].active = true;
 						guiButtons[i][TARGETS].active = mode != SentryMode.IDLE;

src/main/java/net/geforcemods/securitycraft/util/PlayerUtils.java
@@ -98,7 +98,6 @@ public class PlayerUtils {
 	}
 
 	public static void sendMessageToPlayer(Player player, MutableComponent prefix, MutableComponent text, ChatFormatting color, boolean shouldSendFromClient) {
-		//TODO: is this still formatted with color etc.?
 		if (player.level.isClientSide == shouldSendFromClient) {
 			//@formatter:off
 			player.sendSystemMessage(Component.literal("[")
@@ -113,7 +112,6 @@ public class PlayerUtils {
 	 * Sends the given {@link ICommandSource} a chat message, followed by a link prefixed with a colon. <p>
 	 */
 	public static void sendMessageEndingWithLink(CommandSource sender, MutableComponent prefix, MutableComponent text, String link, ChatFormatting color) {
-		//TODO: is this still formatted with color etc.?
 		//@formatter:off
 		sender.sendSystemMessage(Component.literal("[")
 				.append(prefix.setStyle(Style.EMPTY.withColor(color)))

commit 15ba8f00794a71a28d0c4ff429336be3f54ae856

fix a crash caused by bf960c3b3abbe5ac65928e339fc46f19005a51ce

src/main/java/net/geforcemods/securitycraft/screen/SentryRemoteAccessToolScreen.java
@@ -107,6 +107,7 @@ public class SentryRemoteAccessToolScreen extends Screen {
 			if (coords.length == 3) {
 				BlockPos sentryPos = new BlockPos(coords[0], coords[1], coords[2]);
 
+				lines[i] = Utils.getFormattedCoordinates(sentryPos);
 				guiButtons[i][UNBIND].active = true;
 
 				if (Minecraft.getInstance().player.level.isLoaded(sentryPos) && isSentryVisibleToPlayer(sentryPos)) {
@@ -126,8 +127,6 @@ public class SentryRemoteAccessToolScreen extends Screen {
 							lines[i] = sentry.getCustomName();
 							hoverCheckers.add(posTooltipText);
 						}
-						else
-							lines[i] = Utils.getFormattedCoordinates(sentryPos);
 
 						guiButtons[i][MODE].active = true;
 						guiButtons[i][TARGETS].active = mode != SentryMode.IDLE;