Geforce132/SecurityCraft

commit 7251cbccc7b50a7bc3b9d6b85614e83d83d3684a

fix sentry name not showing up in srat

src/main/java/net/geforcemods/securitycraft/screen/SentryRemoteAccessToolScreen.java
@@ -36,7 +36,6 @@ public class SentryRemoteAccessToolScreen extends Screen {
 	private final TranslatableComponent modifyAll = Utils.localize("gui.securitycraft:srat.modifyAll");
 	private ItemStack srat;
 	private Button[][] guiButtons = new Button[12][3]; // 12 sentries, 3 actions (mode, targets, unbind)
-	private Component[] names = new Component[12];
 	private Button[] guiButtonsGlobal = new Button[3];
 	private static final int MODE = 0, TARGETS = 1, UNBIND = 2;
 	private int xSize = 440, ySize = 215;
@@ -112,11 +111,6 @@ public class SentryRemoteAccessToolScreen extends Screen {
 
 				guiButtons[i][UNBIND].active = true;
 
-				if (names[i] != null)
-					lines[i] = names[i];
-				else
-					lines[i] = Utils.getFormattedCoordinates(sentryPos);
-
 				if (Minecraft.getInstance().player.level.isLoaded(sentryPos) && isSentryVisibleToPlayer(sentryPos)) {
 					List<Sentry> sentries = Minecraft.getInstance().player.level.getEntitiesOfClass(Sentry.class, new AABB(sentryPos));
 
@@ -131,9 +125,11 @@ public class SentryRemoteAccessToolScreen extends Screen {
 							int nameY = startY + (i % 6) * 30 + 7;
 							TextHoverChecker posTooltipText = new TextHoverChecker(nameY + 4, nameY + 18, nameX, nameX + nameWidth + 2, line);
 
-							names[i] = sentry.getCustomName();
+							lines[i] = sentry.getCustomName();
 							hoverCheckers.add(posTooltipText);
 						}
+						else
+							lines[i] = Utils.getFormattedCoordinates(sentryPos);
 
 						guiButtons[i][MODE].active = true;
 						guiButtons[i][TARGETS].active = mode != SentryMode.IDLE;

commit 2cf5795f9b4ffb7ff1fb843fef317c0196bdb107

fix a crash caused by 7251cbccc7b50a7bc3b9d6b85614e83d83d3684a

src/main/java/net/geforcemods/securitycraft/screen/SentryRemoteAccessToolScreen.java
@@ -109,6 +109,7 @@ public class SentryRemoteAccessToolScreen extends Screen {
 			if (coords.length == 3) {
 				BlockPos sentryPos = new BlockPos(coords[0], coords[1], coords[2]);
 
+				lines[i] = Utils.getFormattedCoordinates(sentryPos);
 				guiButtons[i][UNBIND].active = true;
 
 				if (Minecraft.getInstance().player.level.isLoaded(sentryPos) && isSentryVisibleToPlayer(sentryPos)) {
@@ -128,8 +129,6 @@ public class SentryRemoteAccessToolScreen extends Screen {
 							lines[i] = sentry.getCustomName();
 							hoverCheckers.add(posTooltipText);
 						}
-						else
-							lines[i] = Utils.getFormattedCoordinates(sentryPos);
 
 						guiButtons[i][MODE].active = true;
 						guiButtons[i][TARGETS].active = mode != SentryMode.IDLE;