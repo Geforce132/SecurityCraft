4b0d6c7aefe4757602b4b25802b27d521782d6be
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-10-15 16:20:29 +0200

    make hue slider work and move it to a better position

--- src/main/java/net/geforcemods/securitycraft/screen/components/ColorChooser.java
+++ src/main/java/net/geforcemods/securitycraft/screen/components/ColorChooser.java
@@ -7,6 +7,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.geforcemods.securitycraft.util.ClientUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
@@ -37,6 +38,7 @@ public class ColorChooser extends Screen implements GuiEventListener, Narratable
 	@Override
 	protected void init() {
 		extraAreas.add(new Rect2i(xStart, 0, 193, minecraft.getWindow().getGuiScaledHeight())); //TODO: set proper extra areas
+		addRenderableWidget(new HueSlider(colorFieldLeft - 2, yStart + 85, 81, 19, h));
 	}
 
 	@Override
@@ -48,11 +50,14 @@ public class ColorChooser extends Screen implements GuiEventListener, Narratable
 			super.render(pose, mouseX, mouseY, partialTick);
 			ClientUtils.renderHorizontalGradientRect(pose, 0, colorFieldLeft, colorFieldTop, colorFieldLeft + colorFieldSize, colorFieldTop + colorFieldSize, 0xFFFFFFFF, ClientUtils.HSBtoRGB(h, 1.0F, 1.0F));
 			fillGradient(pose, colorFieldLeft, colorFieldTop, colorFieldLeft + colorFieldSize, colorFieldTop + colorFieldSize, 0x00000000, 0xFF000000, getBlitOffset());
+			blit(pose, mouseX - 10, mouseY - 10, 248, 38, 4, 4); //for testing
 		}
 	}
 
 	@Override
 	public boolean mouseDragged(double mouseX, double mouseY, int button, double dragX, double dragY) {
+		super.mouseDragged(mouseX, mouseY, button, dragX, dragY);
+
 		if (!disabled && button == 0 && clickedInDragRegion) {
 			return true;
 		}
@@ -62,6 +67,8 @@ public class ColorChooser extends Screen implements GuiEventListener, Narratable
 
 	@Override
 	public boolean mouseClicked(double mouseX, double mouseY, int button) {
+		super.mouseClicked(mouseX, mouseY, button);
+
 		if (!disabled) {
 			//clickedInDragRegion = dragHoverChecker.checkHover(mouseX, mouseY);
 		}
@@ -71,15 +78,12 @@ public class ColorChooser extends Screen implements GuiEventListener, Narratable
 
 	@Override
 	public boolean mouseReleased(double mouseX, double mouseY, int button) {
+		super.mouseReleased(mouseX, mouseY, button);
+
 		if (!disabled)
 			clickedInDragRegion = false;
 
 		return false;
-	}
-
-	@Override
-	public List<? extends GuiEventListener> children() {
-		return List.of();
 	}
 
 	@Override
@@ -96,5 +100,21 @@ public class ColorChooser extends Screen implements GuiEventListener, Narratable
 
 	public int getColor() {
 		return ClientUtils.HSBtoRGB(h, s, b);
+	}
+
+	class HueSlider extends CallbackSlider {
+		public HueSlider(int x, int y, int width, int height, double currentValue) {
+			super(x, y, width, height, Component.empty(), Component.empty(), 0.0D, 360.0D, currentValue, 1.0D, 0, false, slider -> ColorChooser.this.h = slider.getValueInt() / 360.0F);
+		}
+
+		@Override
+		protected void renderBg(PoseStack pose, Minecraft minecraft, int mouseX, int mouseY) {}
+
+		@Override
+		public void renderButton(PoseStack pose, int mouseX, int mouseY, float partialTick) {
+			RenderSystem._setShaderTexture(0, TEXTURE);
+			RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+			blit(pose, x + (int) (value * (width - 8)), y, 250, isHoveredOrFocused() ? height : 0, 6, height);
+		}
 	}
 }
