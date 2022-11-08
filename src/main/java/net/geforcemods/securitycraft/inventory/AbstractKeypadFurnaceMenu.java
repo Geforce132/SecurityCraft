c390cb376938c434d06255416c61e722cec3ec22
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-11-08 14:13:02 +0100

    use ContainerOpenersCounter for password-protected furnaces

--- src/main/java/net/geforcemods/securitycraft/inventory/AbstractKeypadFurnaceMenu.java
+++ src/main/java/net/geforcemods/securitycraft/inventory/AbstractKeypadFurnaceMenu.java
@@ -1,7 +1,6 @@
 package net.geforcemods.securitycraft.inventory;
 
 import net.geforcemods.securitycraft.blockentities.AbstractKeypadFurnaceBlockEntity;
-import net.geforcemods.securitycraft.blocks.AbstractKeypadFurnaceBlock;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractFurnaceMenu;
@@ -11,33 +10,29 @@ import net.minecraft.world.inventory.RecipeBookType;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.LevelEvent;
-import net.minecraft.world.level.gameevent.GameEvent;
 
 public abstract class AbstractKeypadFurnaceMenu extends AbstractFurnaceMenu {
 	private final Block furnaceBlock;
 	public final AbstractKeypadFurnaceBlockEntity be;
-	private final ContainerLevelAccess worldPosCallable;
+	private final ContainerLevelAccess containerLevelAccess;
 
-	protected AbstractKeypadFurnaceMenu(MenuType<?> menuType, RecipeType<? extends AbstractCookingRecipe> recipeType, RecipeBookType recipeBookType, Block furnaceBlock, int windowId, Inventory inventory, AbstractKeypadFurnaceBlockEntity be) {
+	protected AbstractKeypadFurnaceMenu(MenuType<?> menuType, RecipeType<? extends AbstractCookingRecipe> recipeType, RecipeBookType recipeBookType, int windowId, Inventory inventory, AbstractKeypadFurnaceBlockEntity be) {
 		super(menuType, recipeType, recipeBookType, windowId, inventory, be, be.getFurnaceData());
 
-		this.furnaceBlock = furnaceBlock;
+		furnaceBlock = be.getBlockState().getBlock();
 		this.be = be;
-		worldPosCallable = ContainerLevelAccess.create(be.getLevel(), be.getBlockPos());
+		containerLevelAccess = ContainerLevelAccess.create(be.getLevel(), be.getBlockPos());
+		be.startOpen(inventory.player);
 	}
 
 	@Override
 	public boolean stillValid(Player player) {
-		return stillValid(worldPosCallable, player, furnaceBlock);
+		return stillValid(containerLevelAccess, player, furnaceBlock);
 	}
 
 	@Override
 	public void removed(Player player) {
-		worldPosCallable.execute((level, pos) -> {
-			level.levelEvent(player, LevelEvent.SOUND_CLOSE_IRON_DOOR, pos, 0);
-			level.gameEvent(player, GameEvent.CONTAINER_CLOSE, pos);
-			level.setBlockAndUpdate(pos, be.getBlockState().setValue(AbstractKeypadFurnaceBlock.OPEN, false));
-		});
+		super.removed(player);
+		be.stopOpen(player);
 	}
 }
