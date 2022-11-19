a0079596f82e24e7b7638baa6e091a97485209dc
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-11-19 14:58:09 +0100

    move unsorted mixin to separate package

--- src/main/java/net/geforcemods/securitycraft/mixin/portal/PortalForcerMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/portal/PortalForcerMixin.java
@@ -1,4 +1,4 @@
-package net.geforcemods.securitycraft.mixin;
+package net.geforcemods.securitycraft.mixin.portal;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
