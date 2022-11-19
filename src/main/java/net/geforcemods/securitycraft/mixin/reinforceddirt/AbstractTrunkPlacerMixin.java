4169d897da731e22d8658105b8296fc7a8dcd1b5
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-11-19 14:52:52 +0100

    move unsorted mixins to separate packages

--- src/main/java/net/geforcemods/securitycraft/mixin/reinforceddirt/AbstractTrunkPlacerMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/reinforceddirt/AbstractTrunkPlacerMixin.java
@@ -1,4 +1,4 @@
-package net.geforcemods.securitycraft.mixin;
+package net.geforcemods.securitycraft.mixin.reinforceddirt;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
