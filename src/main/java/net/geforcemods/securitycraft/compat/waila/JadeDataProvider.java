c90b0aaba8998ae49160ee6485377bdd280b99b3
Snownee <i@snownee.com>
2022-12-01 18:21:02 +0800

    improved Jade integration

--- src/main/java/net/geforcemods/securitycraft/compat/waila/JadeDataProvider.java
+++ src/main/java/net/geforcemods/securitycraft/compat/waila/JadeDataProvider.java
@@ -1,8 +1,6 @@
 package net.geforcemods.securitycraft.compat.waila;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import net.geforcemods.securitycraft.ClientHandler;
 import net.geforcemods.securitycraft.SCContent;
@@ -10,10 +8,7 @@ import net.geforcemods.securitycraft.SecurityCraft;
 import net.geforcemods.securitycraft.api.IModuleInventory;
 import net.geforcemods.securitycraft.api.IOwnable;
 import net.geforcemods.securitycraft.api.IPasswordProtected;
-import net.geforcemods.securitycraft.blocks.DisguisableBlock;
-import net.geforcemods.securitycraft.blocks.InventoryScannerFieldBlock;
-import net.geforcemods.securitycraft.blocks.LaserFieldBlock;
-import net.geforcemods.securitycraft.blocks.OwnableBlock;
+import net.geforcemods.securitycraft.blocks.*;
 import net.geforcemods.securitycraft.blocks.mines.BaseFullMineBlock;
 import net.geforcemods.securitycraft.blocks.mines.FurnaceMineBlock;
 import net.geforcemods.securitycraft.blocks.reinforced.ReinforcedCauldronBlock;
@@ -30,8 +25,11 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.RegistryObject;
 import snownee.jade.api.BlockAccessor;
@@ -52,66 +50,37 @@ import snownee.jade.impl.ui.TextElement;
 
 @WailaPlugin(SecurityCraft.MODID)
 public class JadeDataProvider extends WailaCompatConstants implements IWailaPlugin {
-	public static final SpoofBlockIcon SPOOF_BLOCK_ICON = new SpoofBlockIcon();
 	public static final SecurityCraftInfo SECURITYCRAFT_INFO = new SecurityCraftInfo();
-	public static final SpoofBlockName SPOOF_BLOCK_NAME = new SpoofBlockName();
-	public static final SpoofModName SPOOF_MOD_NAME = new SpoofModName();
-	private final List<Class<?>> isRegistered = new ArrayList<>();
 
 	@Override
+	@OnlyIn(Dist.CLIENT)
 	public void registerClient(IWailaClientRegistration registration) {
 		registration.addConfig(SHOW_OWNER, true);
 		registration.addConfig(SHOW_MODULES, true);
 		registration.addConfig(SHOW_PASSWORDS, true);
 		registration.addConfig(SHOW_CUSTOM_NAME, true);
 
-		for (RegistryObject<Block> registryObject : SCContent.BLOCKS.getEntries()) {
-			Block block = registryObject.get();
-
-			if (!(block instanceof OwnableBlock) && !Utils.getRegistryName(block).getPath().matches("(?!(reinforced_)).*?crystal_.*") && !(block instanceof ReinforcedCauldronBlock) && !(block instanceof ReinforcedPaneBlock)) {
-				if (!isRegistered.contains(block.getClass())) {
-					registration.registerBlockComponent(SECURITYCRAFT_INFO, block.getClass());
-					isRegistered.add(block.getClass());
-				}
-			}
-
-			if (block instanceof IOverlayDisplay)
-				registration.usePickedResult(block);
-		}
+		registration.registerBlockComponent(SECURITYCRAFT_INFO, Block.class);
+		registration.registerEntityComponent(SECURITYCRAFT_INFO, Sentry.class);
 
 		registration.addBeforeRenderCallback((tooltip, rect, poseStack, accessor, color) -> ClientHandler.isPlayerMountedOnCamera());
-		registration.registerBlockComponent(SPOOF_BLOCK_NAME, BaseFullMineBlock.class);
-		registration.registerBlockComponent(SPOOF_BLOCK_NAME, FurnaceMineBlock.class);
-		registration.registerBlockComponent(SECURITYCRAFT_INFO, OwnableBlock.class);
-		registration.registerBlockComponent(SECURITYCRAFT_INFO, InventoryScannerFieldBlock.class);
-		registration.registerBlockComponent(SECURITYCRAFT_INFO, LaserFieldBlock.class);
-		registration.registerBlockComponent(SECURITYCRAFT_INFO, ReinforcedCauldronBlock.class);
-		registration.registerBlockComponent(SECURITYCRAFT_INFO, ReinforcedPaneBlock.class);
-		registration.registerBlockIcon(SPOOF_BLOCK_ICON, DisguisableBlock.class);
-		registration.registerBlockIcon(SPOOF_BLOCK_ICON, BaseFullMineBlock.class);
-		registration.registerBlockIcon(SPOOF_BLOCK_ICON, FurnaceMineBlock.class);
-		registration.registerEntityComponent(SECURITYCRAFT_INFO, Sentry.class);
-		registration.registerBlockComponent(SPOOF_MOD_NAME, BaseFullMineBlock.class);
-		registration.registerBlockComponent(SPOOF_MOD_NAME, FurnaceMineBlock.class);
-		isRegistered.clear();
-	}
-
-	private static class SpoofBlockIcon implements IBlockComponentProvider {
-		@Override
-		public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {}
-
-		@Override
-		public IElement getIcon(BlockAccessor data, IPluginConfig config, IElement currentIcon) {
-			if (data.getBlock() instanceof IOverlayDisplay display)
-				return ItemStackElement.of(display.getDisplayStack(data.getLevel(), data.getBlockState(), data.getPosition()));
-
-			return ItemStackElement.EMPTY;
-		}
-
-		@Override
-		public ResourceLocation getUid() {
-			return new ResourceLocation(SecurityCraft.MODID, "spoof_block_icon");
-		}
+		registration.addRayTraceCallback((hit, accessor, original) -> {
+			if (accessor instanceof BlockAccessor blockAccessor) {
+				if (blockAccessor.getBlock() instanceof IOverlayDisplay block) {
+					ItemStack fake = block.getDisplayStack(blockAccessor.getLevel(), blockAccessor.getBlockState(), blockAccessor.getPosition());
+					return registration.blockAccessor().from(blockAccessor).fakeBlock(fake).build();
+				}
+				if (blockAccessor.getBlock() instanceof FakeWaterBlock) {
+					BlockState camo = Blocks.WATER.defaultBlockState();
+					return registration.blockAccessor().from(blockAccessor).blockState(camo).build();
+				}
+				if (blockAccessor.getBlock() instanceof FakeLavaBlock) {
+					BlockState camo = Blocks.LAVA.defaultBlockState();
+					return registration.blockAccessor().from(blockAccessor).blockState(camo).build();
+				}
+			}
+			return accessor;
+		});
 	}
 
 	private static class SecurityCraftInfo implements IBlockComponentProvider, IEntityComponentProvider {
@@ -200,43 +169,4 @@ public class JadeDataProvider extends WailaCompatConstants implements IWailaPlug
 		}
 	}
 
-	private static class SpoofBlockName implements IBlockComponentProvider {
-		@Override
-		public void appendTooltip(ITooltip tooltip, BlockAccessor data, IPluginConfig config) {
-			if (tooltip instanceof Tooltip head)
-				head.lines.get(0).getAlignedElements(Align.LEFT).set(0, new TextElement(Component.translatable(((IOverlayDisplay) data.getBlock()).getDisplayStack(data.getLevel(), data.getBlockState(), data.getPosition()).getDescriptionId()).setStyle(ITEM_NAME_STYLE)));
-		}
-
-		@Override
-		public int getDefaultPriority() {
-			return TooltipPosition.HEAD;
-		}
-
-		@Override
-		public ResourceLocation getUid() {
-			return new ResourceLocation(SecurityCraft.MODID, "spoof_block_name");
-		}
-	}
-
-	private static class SpoofModName implements IBlockComponentProvider {
-		@Override
-		public void appendTooltip(ITooltip tooltip, BlockAccessor data, IPluginConfig config) {
-			if (tooltip instanceof Tooltip tail) {
-				ItemStack disguisedAs = ((IOverlayDisplay) data.getBlock()).getDisplayStack(data.getLevel(), data.getBlockState(), data.getPosition());
-				Component modName = Component.literal(ModList.get().getModContainerById(Utils.getRegistryName(disguisedAs.getItem()).getNamespace()).get().getModInfo().getDisplayName()).setStyle(MOD_NAME_STYLE);
-
-				tail.lines.get(tail.lines.size() - 1).getAlignedElements(Align.LEFT).set(0, new TextElement(modName));
-			}
-		}
-
-		@Override
-		public int getDefaultPriority() {
-			return TooltipPosition.TAIL;
-		}
-
-		@Override
-		public ResourceLocation getUid() {
-			return new ResourceLocation(SecurityCraft.MODID, "spoof_mod_name");
-		}
-	}
 }
