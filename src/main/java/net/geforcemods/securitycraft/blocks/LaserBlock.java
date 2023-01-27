6f767251ff52e2c9ce9b1df9b0976b6c4c3341ad
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-01-27 17:37:15 +0100

    don't open the screen if the laser is disabled

--- src/main/java/net/geforcemods/securitycraft/blocks/LaserBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/LaserBlock.java
@@ -61,8 +61,12 @@ public class LaserBlock extends DisguisableBlock {
 	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
 		LaserBlockBlockEntity be = (LaserBlockBlockEntity) level.getBlockEntity(pos);
 
-		if (!level.isClientSide && be.isOwnedBy(player))
-			SecurityCraft.channel.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) player), new OpenLaserScreen(pos, be.getSideConfig()));
+		if (!level.isClientSide && be.isOwnedBy(player)) {
+			if (!be.isEnabled())
+				player.displayClientMessage(Utils.localize("gui.securitycraft:scManual.disabled"), true);
+			else
+				SecurityCraft.channel.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) player), new OpenLaserScreen(pos, be.getSideConfig()));
+		}
 
 		return InteractionResult.SUCCESS;
 	}
--- src/main/java/net/geforcemods/securitycraft/screen/LaserScreen.java
+++ src/main/java/net/geforcemods/securitycraft/screen/LaserScreen.java
@@ -1,7 +1,6 @@
 package net.geforcemods.securitycraft.screen;
 
 import java.util.EnumMap;
-import java.util.List;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -16,21 +15,17 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.FormattedCharSequence;
 
 public class LaserScreen extends Screen {
 	private static final ResourceLocation TEXTURE = new ResourceLocation(SecurityCraft.MODID, "textures/gui/container/block_pocket_manager.png");
 	private int xSize = 176, ySize = 194, leftPos, topPos;
 	private LaserBlockBlockEntity be;
 	private EnumMap<Direction, Boolean> sideConfig;
-	private List<FormattedCharSequence> completelyDisabledText;
 
 	public LaserScreen(LaserBlockBlockEntity be, EnumMap<Direction, Boolean> sideConfig) {
 		super(Component.translatable(SCContent.LASER_BLOCK.get().getDescriptionId()));
 		this.be = be;
-
-		if (be.isEnabled())
-			this.sideConfig = sideConfig;
+		this.sideConfig = sideConfig;
 	}
 
 	@Override
@@ -38,17 +33,12 @@ public class LaserScreen extends Screen {
 		super.init();
 		leftPos = (width - xSize) / 2;
 		topPos = (height - ySize) / 2;
+		sideConfig.forEach((dir, enabled) -> {
+			CallbackCheckbox checkbox = new CallbackCheckbox(leftPos + 10, topPos + dir.get3DDataValue() * 25 + 25, 20, 20, Component.translatable("gui.securitycraft:laser." + dir.getName() + "Enabled"), enabled, newValue -> onChangeValue(dir, newValue), 0x404040);
 
-		if (be.isEnabled()) {
-			sideConfig.forEach((dir, enabled) -> {
-				CallbackCheckbox checkbox = new CallbackCheckbox(leftPos + 10, topPos + dir.get3DDataValue() * 25 + 25, 20, 20, Component.translatable("gui.securitycraft:laser." + dir.getName() + "Enabled"), enabled, newValue -> onChangeValue(dir, newValue), 0x404040);
-
-				checkbox.active = be.isEnabled();
-				addRenderableWidget(checkbox);
-			});
-		}
-		else
-			completelyDisabledText = font.split(Component.translatable("gui.securitycraft:laser.disabled"), 150);
+			checkbox.active = be.isEnabled();
+			addRenderableWidget(checkbox);
+		});
 	}
 
 	@Override
@@ -59,17 +49,6 @@ public class LaserScreen extends Screen {
 		blit(pose, leftPos, topPos, 0, 0, xSize, ySize);
 		super.render(pose, mouseX, mouseY, partialTicks);
 		font.draw(pose, title, leftPos + xSize / 2 - font.width(title) / 2, topPos + 6, 0x404040);
-
-		if (!be.isEnabled()) {
-			int i = 0;
-			int baseX = leftPos + xSize / 2;
-			int baseY = topPos + ySize / 2 - (completelyDisabledText.size() * font.lineHeight) / 2;
-
-			for (FormattedCharSequence text : completelyDisabledText) {
-				font.draw(pose, text, baseX - font.width(text) / 2, baseY + font.lineHeight * i, 0xFF0000);
-				i++;
-			}
-		}
 	}
 
 	public void onChangeValue(Direction dir, boolean newValue) {
