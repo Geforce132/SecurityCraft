aa057c3629d86b9a24a186b8ee2c50bb14ff8041
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-02-04 15:20:19 +0100

    Fixed Sentry head stuttering when being fully extended or retracted

--- src/main/java/net/geforcemods/securitycraft/entity/sentry/Sentry.java
+++ src/main/java/net/geforcemods/securitycraft/entity/sentry/Sentry.java
@@ -129,6 +129,7 @@ public class Sentry extends PathfinderMob implements RangedAttackMob, IEMPAffect
 		else {
 			oHeadRotation = headRotation;
 			headRotation = entityData.get(HEAD_ROTATION);
+			oHeadYTranslation = headYTranslation;
 
 			if (!shutDown && !animate && headYTranslation > 0.0F && getMode().isAggressive()) {
 				animateUpwards = true;
@@ -136,8 +137,6 @@ public class Sentry extends PathfinderMob implements RangedAttackMob, IEMPAffect
 			}
 
 			if (animate) { //no else if because animate can be changed in the above if statement
-				oHeadYTranslation = headYTranslation;
-
 				if (animateUpwards && headYTranslation > UPWARDS_ANIMATION_LIMIT) {
 					headYTranslation -= ANIMATION_STEP_SIZE;
 
