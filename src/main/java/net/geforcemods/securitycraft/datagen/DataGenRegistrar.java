41d3c75218814a77c0ef293b247ff4e9e72effcc
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-03-25 15:40:24 +0100

    datagen pack.mcmeta

--- src/main/java/net/geforcemods/securitycraft/datagen/DataGenRegistrar.java
+++ src/main/java/net/geforcemods/securitycraft/datagen/DataGenRegistrar.java
@@ -1,16 +1,25 @@
 package net.geforcemods.securitycraft.datagen;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.mojang.bridge.game.PackType;
 
 import net.geforcemods.securitycraft.SecurityCraft;
+import net.minecraft.DetectedVersion;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.loot.LootTableProvider.SubProviderEntry;
+import net.minecraft.data.metadata.PackMetadataGenerator;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
@@ -37,6 +46,12 @@ public class DataGenRegistrar {
 		//		if (ModList.get().isLoaded("projecte"))
 		//			generator.addProvider(event.includeServer(), new ProjectECompatConversionProvider(generator));
 
+		//@formatter:off
+		generator.addProvider(true, new PackMetadataGenerator(output)
+                .add(PackMetadataSection.TYPE, new PackMetadataSection(Component.literal("SecurityCraft resources & data"),
+                        DetectedVersion.BUILT_IN.getPackVersion(PackType.RESOURCE),
+                        Arrays.stream(net.minecraft.server.packs.PackType.values()).collect(Collectors.toMap(Function.identity(), type -> type.getVersion(DetectedVersion.BUILT_IN))))));
+		//@formatter:on
 		generator.addProvider(event.includeServer(), new RecipeGenerator(output));
 	}
 }
