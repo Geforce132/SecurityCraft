2b716a29c8d53b54660b30e490ad6a9772c272e7
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-03-28 12:00:46 +0200

    add python script for sorting the language files

---
+++ sort_language_file_content.py
@@ -0,0 +1,14 @@
+languages = ["de_de", "en_gb", "en_us", "es_es", "fr_fr", "it_it", "ja_jp", "ko_kr"]
+
+for language in languages:
+	file_name = "src/main/resources/assets/securitycraft/lang/" + language + ".json"
+
+	with open(file_name, encoding="UTF-8") as file:
+		lines = file.readlines()
+		lines.sort()
+		lines.insert(0, "{\n")
+		del lines[len(lines) - 2]
+
+	with open(file_name, "w", encoding="UTF-8") as file:
+		for line in lines:
+			file.write(line)
