1b9fea3a5a561f096042c6d0329a1abc66af19b8
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-03-28 19:43:15 +0200

    fix python script

--- sort_language_file_content.py
+++ sort_language_file_content.py
@@ -1,7 +1,7 @@
 languages = ["de_de", "en_gb", "en_us", "es_es", "fr_fr", "it_it"]
 
 for language in languages:
-	file_name = "src/main/resources/assets/securitycraft/lang/" + language + ".json"
+	file_name = "src/main/resources/assets/securitycraft/lang/" + language + ".lang"
 
 	with open(file_name, encoding="UTF-8") as file:
 		lines = file.readlines()
