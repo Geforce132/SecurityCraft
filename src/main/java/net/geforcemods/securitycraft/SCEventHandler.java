f82fc43f9a7b7fbd187b34bf3db06e15c5c7f5ef
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-04-07 19:01:27 +0200

    Fixed blocks not being placeable on the side of SC doors

--- src/main/java/net/geforcemods/securitycraft/SCEventHandler.java
+++ src/main/java/net/geforcemods/securitycraft/SCEventHandler.java
@@ -178,7 +178,7 @@ public class SCEventHandler {
 	public static void highestPriorityOnRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
 		ItemStack stack = event.getItemStack();
 
-		if (!stack.isEmpty() && !stack.is(SCTags.Items.CAN_INTERACT_WITH_DOORS)) {
+		if (!stack.isEmpty() && !(stack.getItem() instanceof BlockItem) && !stack.is(SCTags.Items.CAN_INTERACT_WITH_DOORS)) {
 			Block block = event.getWorld().getBlockState(event.getPos()).getBlock();
 
 			if (block == SCContent.KEYPAD_DOOR.get())
