1b4b36f0b60e78b5b46952353f08ca359df0b3c7
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-05-02 17:57:53 +0200

    Fixed potential NPE when opening a Customize screen without options

--- src/main/java/net/geforcemods/securitycraft/screen/CustomizeBlockScreen.java
+++ src/main/java/net/geforcemods/securitycraft/screen/CustomizeBlockScreen.java
@@ -134,12 +134,11 @@ public class CustomizeBlockScreen extends AbstractContainerScreen<CustomizeBlock
 					addRenderableWidget(optionButtons[i]);
 					hoverCheckers.add(new TextHoverChecker(optionButtons[i], getOptionDescription(i)));
 				}
-			}
-		}
 
-		for (AbstractWidget button : optionButtons) {
-			if (button != null)
-				extraAreas.add(new Rect2i(button.x, button.y, button.getWidth(), button.getHeight()));
+				for (AbstractWidget button : optionButtons) {
+					extraAreas.add(new Rect2i(button.x, button.y, button.getWidth(), button.getHeight()));
+				}
+			}
 		}
 	}
 
