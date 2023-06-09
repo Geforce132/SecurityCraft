7aa896d7fa3e5a146b153ce7bdec0520e92d64f7
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-06-09 22:31:08 +0200

    readd JEI compat

--- src/main/java/net/geforcemods/securitycraft/compat/jei/ReinforcerRecipe.java
+++ src/main/java/net/geforcemods/securitycraft/compat/jei/ReinforcerRecipe.java
@@ -1,11 +1,11 @@
-//package net.geforcemods.securitycraft.compat.jei;
-//
-//import net.geforcemods.securitycraft.util.Utils;
-//import net.minecraft.world.level.block.Block;
-//
-//public record ReinforcerRecipe(Block vanillaBlock, Block securityCraftBlock) {
-//	@Override
-//	public String toString() {
-//		return "Reinforcer Recipe - Vanilla Block: " + Utils.getRegistryName(vanillaBlock).toString() + " - Reinforced Block: " + Utils.getRegistryName(securityCraftBlock).toString();
-//	}
-//}
+package net.geforcemods.securitycraft.compat.jei;
+
+import net.geforcemods.securitycraft.util.Utils;
+import net.minecraft.world.level.block.Block;
+
+public record ReinforcerRecipe(Block vanillaBlock, Block securityCraftBlock) {
+	@Override
+	public String toString() {
+		return "Reinforcer Recipe - Vanilla Block: " + Utils.getRegistryName(vanillaBlock).toString() + " - Reinforced Block: " + Utils.getRegistryName(securityCraftBlock).toString();
+	}
+}
