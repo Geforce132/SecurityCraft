8014b244d79cc2c6e6c31038e14530959fc11dfc
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-06-17 22:35:34 +0200

    Fixed some mixin method references and parameters

--- src/main/java/net/geforcemods/securitycraft/mixin/suspiciousmines/BrushItemMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/suspiciousmines/BrushItemMixin.java
@@ -17,6 +17,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 
 /**
  * The brush is hardcoded to only be able to successfully brush vanilla blocks. Because the suspicious mines should also
@@ -25,7 +26,7 @@ import net.minecraft.world.phys.BlockHitResult;
 @Mixin(BrushItem.class)
 public class BrushItemMixin {
 	@Inject(method = "onUseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;isClientSide()Z"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
-	private void securitycraft$checkForSuspiciousMine(Level level, LivingEntity entity, ItemStack stack, int tick, CallbackInfo ci, Player player, BlockHitResult blockHitResult, BlockPos pos, int newUseDuration, BlockState brushedState) {
+	private void securitycraft$checkForSuspiciousMine(Level level, LivingEntity entity, ItemStack stack, int tick, CallbackInfo ci, Player player, HitResult hitResult, BlockHitResult blockHitResult, int i, boolean flag, BlockPos pos, BlockState brushedState) {
 		if (!level.isClientSide() && brushedState.is(SCTags.Blocks.SUSPICIOUS_MINES)) {
 			if (level.getBlockEntity(pos) instanceof BrushableMineBlockEntity be) {
 				boolean brushFinished = be.brush(level.getGameTime(), player, blockHitResult.getDirection());
