374d0c49016923baa64c69b09afb793e23f0bb33
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-06-18 10:18:33 +0200

    readd and fix ftb teams integration

--- src/main/java/net/geforcemods/securitycraft/compat/ftbteams/FTBTeamsCompat.java
+++ src/main/java/net/geforcemods/securitycraft/compat/ftbteams/FTBTeamsCompat.java
@@ -1,15 +1,17 @@
 package net.geforcemods.securitycraft.compat.ftbteams;
 
+import java.util.Optional;
 import java.util.UUID;
 
-import dev.ftb.mods.ftbteams.FTBTeamsAPI;
+import dev.ftb.mods.ftbteams.api.FTBTeamsAPI;
+import dev.ftb.mods.ftbteams.api.Team;
+import dev.ftb.mods.ftbteams.api.property.TeamProperties;
 import net.geforcemods.securitycraft.api.Owner;
-import net.minecraft.world.scores.Team;
 
 public class FTBTeamsCompat {
 	public static boolean areOnSameTeam(Owner owner1, Owner owner2) {
 		try {
-			return FTBTeamsAPI.arePlayersInSameTeam(UUID.fromString(owner1.getUUID()), UUID.fromString(owner2.getUUID()));
+			return FTBTeamsAPI.api().getManager().arePlayersInSameTeam(UUID.fromString(owner1.getUUID()), UUID.fromString(owner2.getUUID()));
 		}
 		catch (IllegalArgumentException e) {
 			return false;
@@ -18,10 +20,13 @@ public class FTBTeamsCompat {
 
 	public static TeamRepresentation getTeamRepresentation(Owner owner) {
 		try {
-			Team team = FTBTeamsAPI.getPlayerTeam(UUID.fromString(owner.getUUID()));
+			Optional<Team> optional = FTBTeamsAPI.api().getManager().getTeamForPlayerID(UUID.fromString(owner.getUUID()));
 
-			if (team != null)
-				return new TeamRepresentation(team.getDisplayName(), team.getColor());
+			if (optional.isPresent()) {
+				Team team = optional.get();
+
+				return new TeamRepresentation(team.getProperty(TeamProperties.DISPLAY_NAME), team.getProperty(TeamProperties.COLOR).toStyle().getColor().getValue());
+			}
 		}
 		catch (IllegalArgumentException e) {}
 
