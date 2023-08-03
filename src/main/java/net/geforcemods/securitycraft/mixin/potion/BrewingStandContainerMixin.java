7c2035de4979e08d31b9b2a47c368b2d3d4d0a5b
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-08-03 17:17:10 +0200

    fix #455

--- src/main/java/net/geforcemods/securitycraft/mixin/potion/BrewingStandContainerMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/potion/BrewingStandContainerMixin.java
@@ -14,7 +14,7 @@ import net.minecraft.item.PotionItem;
  * shift-clicked into the ingredient slot, which is not the vanilla behavior. This mixin prevents any potion from being
  * shift-clicked into the ingredient slot, making them always end up in the potion slots
  */
-@Mixin(BrewingStandContainer.class)
+@Mixin(value = BrewingStandContainer.class, priority = 1100)
 public class BrewingStandContainerMixin {
 	@Redirect(method = "quickMoveStack", at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/container/Slot;mayPlace(Lnet/minecraft/item/ItemStack;)Z"))
 	private boolean securitycraft$preventPotionShiftClickToTopSlot(Slot ingredientSlot, ItemStack stack) {
