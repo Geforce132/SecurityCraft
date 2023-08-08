95ba0d0505e29a868aaa232ee6effda93cef57aa
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-08-08 17:38:38 +0200

    add randomly colored lens to item tab

--- src/main/java/net/geforcemods/securitycraft/items/LensItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/LensItem.java
@@ -1,7 +1,14 @@
 package net.geforcemods.securitycraft.items;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.geforcemods.securitycraft.SecurityCraft;
+import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.DyeableLeatherItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -11,6 +18,21 @@ public class LensItem extends Item implements DyeableLeatherItem {
 
 	public LensItem(Item.Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	public void fillItemCategory(CreativeModeTab category, NonNullList<ItemStack> items) {
+		if (allowdedIn(category)) {
+			int colorAmount = SecurityCraft.RANDOM.nextInt(1, 4);
+			List<DyeItem> list = new ArrayList<>();
+
+			for (int i = 0; i < colorAmount; i++) {
+				list.add(DyeItem.byColor(DyeColor.byId(SecurityCraft.RANDOM.nextInt(16))));
+			}
+
+			items.add(new ItemStack(this));
+			items.add(DyeableLeatherItem.dyeArmor(new ItemStack(this), list));
+		}
 	}
 
 	@Override
