6f4961675d3d29b5117ae3b163343a2a16f43caa
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-09-04 17:04:09 +0200

    fix some menu slots being off by one pixel

--- src/main/java/net/geforcemods/securitycraft/inventory/DisguiseModuleMenu.java
+++ src/main/java/net/geforcemods/securitycraft/inventory/DisguiseModuleMenu.java
@@ -21,7 +21,7 @@ public class DisguiseModuleMenu extends StateSelectorAccessMenu {
 		this.playerInventory = playerInventory;
 		inventory = moduleInventory;
 		moduleInventory.setMenu(this);
-		addSlot(new AddonSlot(inventory, 0, 79, 20));
+		addSlot(new AddonSlot(inventory, 0, 80, 20));
 
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 9; j++) {
