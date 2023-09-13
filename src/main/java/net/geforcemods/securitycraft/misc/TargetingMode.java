fc33bfeace8c791fe7d9d2923398937e9e197585
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-09-13 09:30:48 +0200

    add floor trap

---
+++ src/main/java/net/geforcemods/securitycraft/misc/TargetingMode.java
@@ -0,0 +1,30 @@
+package net.geforcemods.securitycraft.misc;
+
+import net.minecraft.network.chat.Component;
+
+public enum TargetingMode {
+	PLAYERS("gui.securitycraft:srat.targets3", true, false),
+	PLAYERS_AND_MOBS("gui.securitycraft:srat.targets1", true, true),
+	MOBS("gui.securitycraft:srat.targets2", false, true);
+
+	private final String translationKey;
+	private final boolean allowsPlayers, allowsMobs;
+
+	private TargetingMode(String translationKey, boolean allowsPlayers, boolean allowsMobs) {
+		this.translationKey = translationKey;
+		this.allowsPlayers = allowsPlayers;
+		this.allowsMobs = allowsMobs;
+	}
+
+	public Component translate() {
+		return Component.translatable(translationKey);
+	}
+
+	public boolean allowsPlayers() {
+		return allowsPlayers;
+	}
+
+	public boolean allowsMobs() {
+		return allowsMobs;
+	}
+}
