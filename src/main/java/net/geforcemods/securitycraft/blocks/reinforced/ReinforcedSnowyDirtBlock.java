14478c891ecc3256526a65a3b0339dd278feea88
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-12-11 16:33:50 +0100

    Cleaned up ReinforcedSnowyDirtBlock
    Also fixed plants not growing on adjacent Reinforced Grass Blocks when bonemealing one

--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedSnowyDirtBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedSnowyDirtBlock.java
@@ -8,23 +8,16 @@ import net.geforcemods.securitycraft.api.OwnableBlockEntity;
 import net.geforcemods.securitycraft.misc.OwnershipEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.GrassBlock;
 import net.minecraft.world.level.block.SnowyDirtBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -33,7 +26,7 @@ import net.minecraft.world.level.levelgen.feature.configurations.TreeConfigurati
 import net.neoforged.neoforge.common.IPlantable;
 import net.neoforged.neoforge.common.NeoForge;
 
-public class ReinforcedSnowyDirtBlock extends SnowyDirtBlock implements IReinforcedBlock, BonemealableBlock, EntityBlock {
+public class ReinforcedSnowyDirtBlock extends SnowyDirtBlock implements IReinforcedBlock, EntityBlock {
 	private Block vanillaBlock;
 
 	public ReinforcedSnowyDirtBlock(BlockBehaviour.Properties properties, Block vB) {
@@ -42,46 +35,9 @@ public class ReinforcedSnowyDirtBlock extends SnowyDirtBlock implements IReinfor
 	}
 
 	@Override
-	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
-		if (facing != Direction.UP)
-			return super.updateShape(state, facing, facingState, level, currentPos, facingPos);
-		else
-			return state.setValue(SNOWY, isSnowySetting(facingState));
-	}
-
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-		BlockState state = ctx.getLevel().getBlockState(ctx.getClickedPos().above());
-		return defaultBlockState().setValue(SNOWY, isSnowySetting(state));
-	}
-
-	private static boolean isSnowySetting(BlockState state) {
-		return state.is(BlockTags.SNOW);
-	}
-
-	@Override
 	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource rand) {
-		if (this == SCContent.REINFORCED_MYCELIUM.get()) {
-			super.animateTick(state, level, pos, rand);
-
-			if (rand.nextInt(10) == 0)
-				level.addParticle(ParticleTypes.MYCELIUM, (double) pos.getX() + (double) rand.nextFloat(), pos.getY() + 1.1D, (double) pos.getZ() + (double) rand.nextFloat(), 0.0D, 0.0D, 0.0D);
-		}
-	}
-
-	@Override
-	public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state) {
-		return this == SCContent.REINFORCED_GRASS_BLOCK.get() && level.getBlockState(pos.above()).isAir();
-	}
-
-	@Override
-	public boolean isBonemealSuccess(Level level, RandomSource rand, BlockPos pos, BlockState state) {
-		return this == SCContent.REINFORCED_GRASS_BLOCK.get();
-	}
-
-	@Override
-	public void performBonemeal(ServerLevel level, RandomSource rand, BlockPos pos, BlockState state) {
-		((GrassBlock) Blocks.GRASS_BLOCK).performBonemeal(level, rand, pos, state);
+		if (this == SCContent.REINFORCED_MYCELIUM.get())
+			Blocks.MYCELIUM.animateTick(state, level, pos, rand);
 	}
 
 	@Override
