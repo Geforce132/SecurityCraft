cf25c5d3f0052c7c15bf979ce833594445cc50ca
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-12-12 17:12:41 +0100

    fix reinforced ladders having no owner

--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedLadderBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedLadderBlock.java
@@ -1,18 +1,44 @@
 package net.geforcemods.securitycraft.blocks.reinforced;
 
+import net.geforcemods.securitycraft.api.INameSetter;
 import net.geforcemods.securitycraft.api.IReinforcedBlock;
+import net.geforcemods.securitycraft.api.OwnableBlockEntity;
+import net.geforcemods.securitycraft.misc.OwnershipEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.LadderBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.NeoForge;
 
-public class ReinforcedLadderBlock extends LadderBlock implements IReinforcedBlock {
+public class ReinforcedLadderBlock extends LadderBlock implements IReinforcedBlock, EntityBlock {
 	public ReinforcedLadderBlock(BlockBehaviour.Properties properties) {
 		super(properties);
 	}
 
 	@Override
+	public void setPlacedBy(Level level, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
+		if (placer instanceof Player player)
+			NeoForge.EVENT_BUS.post(new OwnershipEvent(level, pos, player));
+
+		if (stack.hasCustomHoverName() && level.getBlockEntity(pos) instanceof INameSetter nameable)
+			nameable.setCustomName(stack.getHoverName());
+	}
+
+	@Override
 	public Block getVanillaBlock() {
 		return Blocks.LADDER;
+	}
+
+	@Override
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return new OwnableBlockEntity(pos, state);
 	}
 }
