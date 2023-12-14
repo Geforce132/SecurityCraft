009ec38f0df5f9fae3761dab5dad0760708c2b8e
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-12-14 11:41:18 +0100

    improve capability checks for extraction permission

--- src/main/java/net/geforcemods/securitycraft/blockentities/KeypadChestBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/KeypadChestBlockEntity.java
@@ -133,7 +133,7 @@ public class KeypadChestBlockEntity extends ChestBlockEntity implements IPasscod
 	@Override
 	public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
 		if (cap == ForgeCapabilities.ITEM_HANDLER)
-			return BlockUtils.getProtectedCapability(side, this, () -> super.getCapability(cap, side), this::getInsertOnlyHandler).cast();
+			return BlockUtils.isAllowedToExtractFromProtectedBlock(side, this) ? super.getCapability(cap, side) : getInsertOnlyHandler().cast();
 		else
 			return super.getCapability(cap, side);
 	}
