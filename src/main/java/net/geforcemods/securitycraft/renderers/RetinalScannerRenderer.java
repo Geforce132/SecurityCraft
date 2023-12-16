28b015fa1e83b07e192a85a59a626ac40ba3e14a
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-12-16 22:30:46 +0100

    fix face not rendering on retinal scanner with empty disguise module

--- src/main/java/net/geforcemods/securitycraft/renderers/RetinalScannerRenderer.java
+++ src/main/java/net/geforcemods/securitycraft/renderers/RetinalScannerRenderer.java
@@ -12,6 +12,7 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 
 import net.geforcemods.securitycraft.ClientHandler;
 import net.geforcemods.securitycraft.ConfigHandler;
+import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.blockentities.RetinalScannerBlockEntity;
 import net.geforcemods.securitycraft.blocks.RetinalScannerBlock;
 import net.geforcemods.securitycraft.misc.ModuleType;
@@ -47,7 +48,10 @@ public class RetinalScannerRenderer extends TileEntityRenderer<RetinalScannerBlo
 
 		Direction direction = be.getBlockState().getValue(RetinalScannerBlock.FACING);
 
-		if (!be.isModuleEnabled(ModuleType.DISGUISE) && direction != null) {
+		if (direction != null) {
+			if (be.isModuleEnabled(ModuleType.DISGUISE) && SCContent.DISGUISE_MODULE.get().getBlockAddon(be.getModule(ModuleType.DISGUISE).getTag()) != null)
+				return;
+
 			pose.pushPose();
 
 			switch (direction) {
