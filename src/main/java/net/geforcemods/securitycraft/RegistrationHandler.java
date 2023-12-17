8f12933eae7b097f068e1fe9c98593d15d20a484
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-12-17 21:50:16 +0100

    Refactored sentry head animation logic

--- src/main/java/net/geforcemods/securitycraft/RegistrationHandler.java
+++ src/main/java/net/geforcemods/securitycraft/RegistrationHandler.java
@@ -7,7 +7,6 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 import net.geforcemods.securitycraft.misc.SCSounds;
-import net.geforcemods.securitycraft.network.client.InitSentryAnimation;
 import net.geforcemods.securitycraft.network.client.OpenScreen;
 import net.geforcemods.securitycraft.network.client.PlayAlarmSound;
 import net.geforcemods.securitycraft.network.client.RefreshDisguisableModel;
@@ -137,7 +136,6 @@ public class RegistrationHandler {
 		int id = 0;
 
 		//client
-		registerPacket(id++, InitSentryAnimation.class, InitSentryAnimation::encode, InitSentryAnimation::new, InitSentryAnimation::handle);
 		registerPacket(id++, OpenScreen.class, OpenScreen::encode, OpenScreen::new, OpenScreen::handle);
 		registerPacket(id++, PlayAlarmSound.class, PlayAlarmSound::encode, PlayAlarmSound::new, PlayAlarmSound::handle);
 		registerPacket(id++, RefreshDisguisableModel.class, RefreshDisguisableModel::encode, RefreshDisguisableModel::new, RefreshDisguisableModel::handle);
