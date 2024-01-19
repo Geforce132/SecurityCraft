41b19c8ef2e93b9934df7e9b9ed491ff8e41d2a0
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-01-19 22:21:31 +0100

    organize and refactor sc command

---
+++ src/main/java/net/geforcemods/securitycraft/commands/DumpCommand.java
@@ -0,0 +1,77 @@
+package net.geforcemods.securitycraft.commands;
+
+import java.lang.reflect.Field;
+import java.util.Locale;
+import java.util.Map;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.ArgumentBuilder;
+
+import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
+import net.geforcemods.securitycraft.SCContent;
+import net.minecraft.ChatFormatting;
+import net.minecraft.Util;
+import net.minecraft.commands.CommandRuntimeException;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class DumpCommand {
+	private static final Map<String, DeferredRegister<?>> REGISTRIES = Util.make(() -> {
+		Map<String, DeferredRegister<?>> map = new Object2ObjectArrayMap<>();
+
+		for (Field field : SCContent.class.getFields()) {
+			try {
+				Object object = field.get(null);
+
+				if (!(object instanceof DeferredRegister))
+					return map;
+
+				map.put(field.getName().toLowerCase(Locale.ROOT), (DeferredRegister<?>) object);
+			}
+			catch (IllegalArgumentException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return map;
+	});
+
+	public static ArgumentBuilder<CommandSourceStack, ?> register() {
+		//@formatter:off
+		return Commands.literal("dump")
+				.then(Commands.argument("registry", StringArgumentType.word())
+						.suggests((ctx, builder) -> SharedSuggestionProvider.suggest(REGISTRIES.keySet(), builder))
+						.executes(ctx -> { //@formatter:on
+							String registry = ctx.getArgument("registry", String.class);
+
+							if (!REGISTRIES.containsKey(registry))
+								throw new CommandRuntimeException(Component.translatable("commands.securitycraft.dump.notFound", registry));
+
+							final String lineSeparator = System.lineSeparator();
+							final String finalResult;
+							final var registryObjects = REGISTRIES.get(registry).getEntries();
+							String result = "";
+
+							for (DeferredHolder<?, ?> ro : registryObjects) {
+								result += ro.getId().toString() + lineSeparator;
+							}
+
+							finalResult = result;
+							ctx.getSource().getPlayerOrException().sendSystemMessage(Component.literal("[") //@formatter:off
+									.append(Component.literal("SecurityCraft").withStyle(ChatFormatting.GOLD))
+									.append(Component.literal("] "))
+									.append(Component.translatable("commands.securitycraft.dump.result", registryObjects.size())
+											.withStyle(style -> style
+													.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal(registry)))
+													.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, finalResult.substring(0, finalResult.lastIndexOf(lineSeparator)))))));
+							//@formatter:on
+							return 0;
+						}));
+	}
+}
