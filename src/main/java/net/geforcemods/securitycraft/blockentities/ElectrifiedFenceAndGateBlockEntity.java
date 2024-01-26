80d98d64db90ae97455975f4a926f1cd6666a2b9
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-01-26 17:36:26 +0100

    add allowlist support to electrified iron fence and fence gate

---
+++ src/main/java/net/geforcemods/securitycraft/blockentities/ElectrifiedFenceAndGateBlockEntity.java
@@ -0,0 +1,22 @@
+package net.geforcemods.securitycraft.blockentities;
+
+import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.misc.ModuleType;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class ElectrifiedFenceAndGateBlockEntity extends AllowlistOnlyBlockEntity {
+	public ElectrifiedFenceAndGateBlockEntity(BlockPos pos, BlockState state) {
+		super(SCContent.ABSTRACT_BLOCK_ENTITY.get(), pos, state);
+	}
+
+	public ElectrifiedFenceAndGateBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+		super(type, pos, state);
+	}
+
+	@Override
+	public String getModuleDescriptionId(String blockName, ModuleType module) {
+		return "module.generic.electrified_fence_and_gate.whitelist_module.description";
+	}
+}
