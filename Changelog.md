b888dca737a91e2ce183226caa79d749c2293881
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-02-04 21:12:12 +0100

    fix BP activation desync

--- Changelog.md
+++ Changelog.md
@@ -66,6 +66,7 @@
 - Fix: The Keypad Trapdoor doesn't have any info in JEI
 - Fix: Shadow around the Bouncing Betty, IMS, Mine, and Portable Radar
 - Fix: The Bouncing Betty, IMS, Mine, and Portable Radar cause suffocation
+- Fix: The collision of Block Pocket Wall blocks gets desynchronized when activating a block pocket
 - Removed: "Is always active" option of Keypads and Key Panels. The same behavior can be achieved by setting the "Signal length" option to 0 and activating the block
 - Misc.: Small optimizations to certain network and client performance
 
--- src/main/java/net/geforcemods/securitycraft/blockentities/BlockPocketBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/BlockPocketBlockEntity.java
@@ -3,6 +3,7 @@ package net.geforcemods.securitycraft.blockentities;
 import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.api.OwnableBlockEntity;
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
@@ -13,13 +14,21 @@ public class BlockPocketBlockEntity extends OwnableBlockEntity implements ITicka
 	private BlockPos managerPos;
 
 	public void setManager(BlockPocketManagerBlockEntity manager) {
+		IBlockState state = world.getBlockState(pos);
+
 		this.manager = manager;
 		managerPos = manager.getPos();
+		markDirty();
+		world.notifyBlockUpdate(pos, state, state, 3);
 	}
 
 	public void removeManager() {
+		IBlockState state = world.getBlockState(pos);
+
 		managerPos = null;
 		manager = null;
+		markDirty();
+		world.notifyBlockUpdate(pos, state, state, 3);
 	}
 
 	public BlockPocketManagerBlockEntity getManager() {
