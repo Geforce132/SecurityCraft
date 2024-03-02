5a44603037241ddefaee005f5885b0b02fb9e9c3
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-03-02 13:31:29 +0100

    simplify KeyBindings class

--- src/main/java/net/geforcemods/securitycraft/misc/KeyBindings.java
+++ src/main/java/net/geforcemods/securitycraft/misc/KeyBindings.java
@@ -19,14 +19,16 @@ public class KeyBindings {
 	private KeyBindings() {}
 
 	public static void init() {
-		cameraZoomIn = new KeyBinding("key.securitycraft.cameraZoomIn", Keyboard.KEY_EQUALS, "key.categories.securitycraft");
-		cameraZoomOut = new KeyBinding("key.securitycraft.cameraZoomOut", Keyboard.KEY_MINUS, "key.categories.securitycraft");
-		cameraEmitRedstone = new KeyBinding("key.securitycraft.cameraEmitRedstone", Keyboard.KEY_R, "key.categories.securitycraft");
-		cameraActivateNightVision = new KeyBinding("key.securitycraft.cameraActivateNightVision", Keyboard.KEY_N, "key.categories.securitycraft");
+		cameraZoomIn = register("cameraZoomIn", Keyboard.KEY_EQUALS);
+		cameraZoomOut = register("cameraZoomOut", Keyboard.KEY_MINUS);
+		cameraEmitRedstone = register("cameraEmitRedstone", Keyboard.KEY_R);
+		cameraActivateNightVision = register("cameraActivateNightVision", Keyboard.KEY_N);
+	}
 
-		ClientRegistry.registerKeyBinding(cameraZoomIn);
-		ClientRegistry.registerKeyBinding(cameraZoomOut);
-		ClientRegistry.registerKeyBinding(cameraEmitRedstone);
-		ClientRegistry.registerKeyBinding(cameraActivateNightVision);
+	private static KeyBinding register(String name, int defaultKey) {
+		KeyBinding keyMapping = new KeyBinding("key.securitycraft." + name, defaultKey, "key.categories.securitycraft");
+
+		ClientRegistry.registerKeyBinding(keyMapping);
+		return keyMapping;
 	}
 }
