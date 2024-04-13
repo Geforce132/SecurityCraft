9992eadc8a94bcd9af30a6385337c0b91077b4dc
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-04-13 22:01:48 +0200

    simplify JadeDataProvider

--- src/main/java/net/geforcemods/securitycraft/compat/waila/JadeDataProvider.java
+++ src/main/java/net/geforcemods/securitycraft/compat/waila/JadeDataProvider.java
@@ -39,17 +39,15 @@ import snownee.jade.impl.Tooltip;
 import snownee.jade.impl.ui.TextElement;
 
 @WailaPlugin(SecurityCraft.MODID)
-public final class JadeDataProvider extends WailaCompatConstants implements IWailaPlugin {
-	public static final SecurityCraftInfo SECURITYCRAFT_INFO = new SecurityCraftInfo();
-
+public final class JadeDataProvider extends WailaCompatConstants implements IWailaPlugin, IBlockComponentProvider, IEntityComponentProvider {
 	@Override
 	public void registerClient(IWailaClientRegistration registration) {
 		registration.addConfig(SHOW_OWNER, true);
 		registration.addConfig(SHOW_MODULES, true);
 		registration.addConfig(SHOW_CUSTOM_NAME, true);
 
-		registration.registerBlockComponent(SECURITYCRAFT_INFO, Block.class);
-		registration.registerEntityComponent(SECURITYCRAFT_INFO, Sentry.class);
+		registration.registerBlockComponent(this, Block.class);
+		registration.registerEntityComponent(this, Sentry.class);
 
 		registration.addBeforeRenderCallback((tooltip, rect, poseStack, accessor, color) -> ClientHandler.isPlayerMountedOnCamera());
 		registration.addRayTraceCallback((hit, accessor, original) -> {
@@ -74,84 +72,82 @@ public final class JadeDataProvider extends WailaCompatConstants implements IWai
 		});
 	}
 
-	private static class SecurityCraftInfo implements IBlockComponentProvider, IEntityComponentProvider {
-		@Override
-		public void appendTooltip(ITooltip tooltip, BlockAccessor data, IPluginConfig config) {
-			Block block = data.getBlock();
-			boolean disguised = false;
+	@Override
+	public void appendTooltip(ITooltip tooltip, BlockAccessor data, IPluginConfig config) {
+		Block block = data.getBlock();
+		boolean disguised = false;
 
-			if (block instanceof DisguisableBlock) {
-				Optional<BlockState> disguisedBlockState = DisguisableBlock.getDisguisedBlockState(data.getLevel(), data.getPosition());
+		if (block instanceof DisguisableBlock) {
+			Optional<BlockState> disguisedBlockState = DisguisableBlock.getDisguisedBlockState(data.getLevel(), data.getPosition());
 
-				if (disguisedBlockState.isPresent()) {
-					disguised = true;
-					block = disguisedBlockState.get().getBlock();
-				}
+			if (disguisedBlockState.isPresent()) {
+				disguised = true;
+				block = disguisedBlockState.get().getBlock();
 			}
+		}
 
-			if (block instanceof IOverlayDisplay display && !display.shouldShowSCInfo(data.getLevel(), data.getBlockState(), data.getPosition()))
+		if (block instanceof IOverlayDisplay display && !display.shouldShowSCInfo(data.getLevel(), data.getBlockState(), data.getPosition()))
+			return;
+
+		BlockEntity be = data.getBlockEntity();
+
+		if (be != null) {
+			if (tooltip instanceof Tooltip t && data.getBlock() instanceof IOverlayDisplay overlayDisplay)
+				t.lines.get(0).getAlignedElements(Align.LEFT).set(0, new TextElement(Component.translatable(overlayDisplay.getDisplayStack(data.getLevel(), data.getBlockState(), data.getPosition()).getDescriptionId()).setStyle(ITEM_NAME_STYLE)));
+
+			//last part is a little cheaty to prevent owner info from being displayed on non-sc blocks
+			if (config.get(SHOW_OWNER) && be instanceof IOwnable ownable && Utils.getRegistryName(block).getNamespace().equals(SecurityCraft.MODID))
+				tooltip.add(Utils.localize("waila.securitycraft:owner", PlayerUtils.getOwnerComponent(ownable.getOwner())));
+
+			if (disguised)
 				return;
 
-			BlockEntity be = data.getBlockEntity();
-
-			if (be != null) {
-				if (tooltip instanceof Tooltip t && data.getBlock() instanceof IOverlayDisplay overlayDisplay)
-					t.lines.get(0).getAlignedElements(Align.LEFT).set(0, new TextElement(Component.translatable(overlayDisplay.getDisplayStack(data.getLevel(), data.getBlockState(), data.getPosition()).getDescriptionId()).setStyle(ITEM_NAME_STYLE)));
-
-				//last part is a little cheaty to prevent owner info from being displayed on non-sc blocks
-				if (config.get(SHOW_OWNER) && be instanceof IOwnable ownable && Utils.getRegistryName(block).getNamespace().equals(SecurityCraft.MODID))
-					tooltip.add(Utils.localize("waila.securitycraft:owner", PlayerUtils.getOwnerComponent(ownable.getOwner())));
-
-				if (disguised)
-					return;
-
-				//if the te is ownable, show modules only when it's owned, otherwise always show
-				if (config.get(SHOW_MODULES) && be instanceof IModuleInventory inv && (!(be instanceof IOwnable ownable) || ownable.isOwnedBy(data.getPlayer()))) {
-					if (!inv.getInsertedModules().isEmpty())
-						tooltip.add(EQUIPPED);
-
-					for (ModuleType module : inv.getInsertedModules()) {
-						tooltip.add(Component.literal("- ").append(Component.translatable(module.getTranslationKey())));
-					}
-				}
-			}
-		}
-
-		@Override
-		public void appendTooltip(ITooltip tooltip, EntityAccessor data, IPluginConfig config) {
-			Entity entity = data.getEntity();
-
-			if (entity instanceof Sentry sentry) {
-				SentryMode mode = sentry.getMode();
-
-				if (config.get(SHOW_OWNER))
-					tooltip.add(Utils.localize("waila.securitycraft:owner", PlayerUtils.getOwnerComponent(sentry.getOwner())));
-
-				if (config.get(SHOW_MODULES) && sentry.isOwnedBy(data.getPlayer()) && (!sentry.getAllowlistModule().isEmpty() || !sentry.getDisguiseModule().isEmpty() || sentry.hasSpeedModule())) {
+			//if the te is ownable, show modules only when it's owned, otherwise always show
+			if (config.get(SHOW_MODULES) && be instanceof IModuleInventory inv && (!(be instanceof IOwnable ownable) || ownable.isOwnedBy(data.getPlayer()))) {
+				if (!inv.getInsertedModules().isEmpty())
 					tooltip.add(EQUIPPED);
 
-					if (!sentry.getAllowlistModule().isEmpty())
-						tooltip.add(ALLOWLIST_MODULE);
-
-					if (!sentry.getDisguiseModule().isEmpty())
-						tooltip.add(DISGUISE_MODULE);
-
-					if (sentry.hasSpeedModule())
-						tooltip.add(SPEED_MODULE);
+				for (ModuleType module : inv.getInsertedModules()) {
+					tooltip.add(Component.literal("- ").append(Component.translatable(module.getTranslationKey())));
 				}
-
-				MutableComponent modeDescription = Utils.localize(mode.getModeKey());
-
-				if (mode != SentryMode.IDLE)
-					modeDescription.append("- ").append(Utils.localize(mode.getTargetKey()));
-
-				tooltip.add(modeDescription);
 			}
 		}
+	}
 
-		@Override
-		public ResourceLocation getUid() {
-			return new ResourceLocation(SecurityCraft.MODID, "info");
+	@Override
+	public void appendTooltip(ITooltip tooltip, EntityAccessor data, IPluginConfig config) {
+		Entity entity = data.getEntity();
+
+		if (entity instanceof Sentry sentry) {
+			SentryMode mode = sentry.getMode();
+
+			if (config.get(SHOW_OWNER))
+				tooltip.add(Utils.localize("waila.securitycraft:owner", PlayerUtils.getOwnerComponent(sentry.getOwner())));
+
+			if (config.get(SHOW_MODULES) && sentry.isOwnedBy(data.getPlayer()) && (!sentry.getAllowlistModule().isEmpty() || !sentry.getDisguiseModule().isEmpty() || sentry.hasSpeedModule())) {
+				tooltip.add(EQUIPPED);
+
+				if (!sentry.getAllowlistModule().isEmpty())
+					tooltip.add(ALLOWLIST_MODULE);
+
+				if (!sentry.getDisguiseModule().isEmpty())
+					tooltip.add(DISGUISE_MODULE);
+
+				if (sentry.hasSpeedModule())
+					tooltip.add(SPEED_MODULE);
+			}
+
+			MutableComponent modeDescription = Utils.localize(mode.getModeKey());
+
+			if (mode != SentryMode.IDLE)
+				modeDescription.append("- ").append(Utils.localize(mode.getTargetKey()));
+
+			tooltip.add(modeDescription);
 		}
+	}
+
+	@Override
+	public ResourceLocation getUid() {
+		return new ResourceLocation(SecurityCraft.MODID, "info");
 	}
 }
