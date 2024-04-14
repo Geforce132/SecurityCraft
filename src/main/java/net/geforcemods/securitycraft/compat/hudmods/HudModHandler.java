c6dd01fb215acc8a0eea64c154719463d84c5bc6
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-04-14 16:44:11 +0200

    unify Jade, TOP, and WTHIT compat for easier maintainability

---
+++ src/main/java/net/geforcemods/securitycraft/compat/hudmods/HudModHandler.java
@@ -0,0 +1,112 @@
+package net.geforcemods.securitycraft.compat.hudmods;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import net.geforcemods.securitycraft.SecurityCraft;
+import net.geforcemods.securitycraft.api.IModuleInventory;
+import net.geforcemods.securitycraft.api.IOwnable;
+import net.geforcemods.securitycraft.blocks.DisguisableBlock;
+import net.geforcemods.securitycraft.compat.IOverlayDisplay;
+import net.geforcemods.securitycraft.entity.sentry.Sentry;
+import net.geforcemods.securitycraft.entity.sentry.Sentry.SentryMode;
+import net.geforcemods.securitycraft.misc.ModuleType;
+import net.geforcemods.securitycraft.util.PlayerUtils;
+import net.geforcemods.securitycraft.util.Utils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Nameable;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class HudModHandler {
+	protected static final ResourceLocation SHOW_OWNER = new ResourceLocation(SecurityCraft.MODID, "showowner");
+	protected static final ResourceLocation SHOW_MODULES = new ResourceLocation(SecurityCraft.MODID, "showmodules");
+	protected static final ResourceLocation SHOW_CUSTOM_NAME = new ResourceLocation(SecurityCraft.MODID, "showcustomname");
+	protected static final Style MOD_NAME_STYLE = Style.EMPTY.applyFormat(ChatFormatting.BLUE).withItalic(true);
+	protected static final Style ITEM_NAME_STYLE = Style.EMPTY.applyFormat(ChatFormatting.WHITE);
+	protected static final MutableComponent EQUIPPED = Utils.localize("waila.securitycraft:equipped").withStyle(ChatFormatting.GRAY);
+	protected static final MutableComponent ALLOWLIST_MODULE = Component.literal("- ").append(Component.translatable(ModuleType.ALLOWLIST.getTranslationKey())).withStyle(ChatFormatting.GRAY);
+	protected static final MutableComponent DISGUISE_MODULE = Component.literal("- ").append(Component.translatable(ModuleType.DISGUISE.getTranslationKey())).withStyle(ChatFormatting.GRAY);
+	protected static final MutableComponent SPEED_MODULE = Component.literal("- ").append(Component.translatable(ModuleType.SPEED.getTranslationKey())).withStyle(ChatFormatting.GRAY);
+
+	protected HudModHandler() {}
+
+	public void addOwnerModuleNameInfo(Level level, BlockPos pos, BlockState state, Block block, BlockEntity be, Player player, Consumer<Component> lineAdder, Predicate<ResourceLocation> configGetter) {
+		boolean disguised = false;
+
+		if (block instanceof DisguisableBlock) {
+			Optional<BlockState> disguisedBlockState = DisguisableBlock.getDisguisedBlockState(level, pos);
+
+			if (disguisedBlockState.isPresent()) {
+				disguised = true;
+				block = disguisedBlockState.get().getBlock();
+			}
+		}
+
+		if (block instanceof IOverlayDisplay display && !display.shouldShowSCInfo(level, state, pos))
+			return;
+
+		if (be != null) {
+			if (configGetter.test(SHOW_OWNER) && be instanceof IOwnable ownable)
+				lineAdder.accept(Utils.localize("waila.securitycraft:owner", PlayerUtils.getOwnerComponent(ownable.getOwner())).withStyle(ChatFormatting.GRAY));
+
+			if (disguised)
+				return;
+
+			//if the te is ownable, show modules only when it's owned, otherwise always show
+			if (configGetter.test(SHOW_MODULES) && be instanceof IModuleInventory inv && !inv.getInsertedModules().isEmpty() && (!(be instanceof IOwnable ownable) || ownable.isOwnedBy(player))) {
+				lineAdder.accept(EQUIPPED);
+
+				for (ModuleType module : inv.getInsertedModules()) {
+					lineAdder.accept(Component.literal("- ").append(Component.translatable(module.getTranslationKey())).withStyle(ChatFormatting.GRAY));
+				}
+			}
+
+			if (configGetter.test(SHOW_CUSTOM_NAME) && be instanceof Nameable nameable && nameable.hasCustomName()) {
+				Component text = nameable.getCustomName();
+				Component name = text == null ? Component.empty() : text;
+
+				lineAdder.accept(Utils.localize("waila.securitycraft:customName", name).withStyle(ChatFormatting.GRAY));
+			}
+		}
+	}
+
+	public void addEntityInfo(Entity entity, Player player, Consumer<Component> lineAdder, Predicate<ResourceLocation> configGetter) {
+		if (entity instanceof Sentry sentry) {
+			SentryMode mode = sentry.getMode();
+
+			if (configGetter.test(SHOW_OWNER))
+				lineAdder.accept(Utils.localize("waila.securitycraft:owner", PlayerUtils.getOwnerComponent(sentry.getOwner())).withStyle(ChatFormatting.GRAY));
+
+			if (configGetter.test(SHOW_MODULES) && sentry.isOwnedBy(player) && (!sentry.getAllowlistModule().isEmpty() || !sentry.getDisguiseModule().isEmpty() || sentry.hasSpeedModule())) {
+				lineAdder.accept(EQUIPPED);
+
+				if (!sentry.getAllowlistModule().isEmpty())
+					lineAdder.accept(ALLOWLIST_MODULE);
+
+				if (!sentry.getDisguiseModule().isEmpty())
+					lineAdder.accept(DISGUISE_MODULE);
+
+				if (sentry.hasSpeedModule())
+					lineAdder.accept(SPEED_MODULE);
+			}
+
+			MutableComponent modeDescription = Utils.localize(mode.getModeKey());
+
+			if (mode != SentryMode.IDLE)
+				modeDescription.append("- ").append(Utils.localize(mode.getTargetKey()));
+
+			lineAdder.accept(modeDescription.withStyle(ChatFormatting.GRAY));
+		}
+	}
+}
