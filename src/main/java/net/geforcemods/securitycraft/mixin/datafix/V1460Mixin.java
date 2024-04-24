caefe7d39e76cdd71f33fe5ac04b14286b7c3d16
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2024-04-24 20:33:43 +0200

    Fixed SC item container inventory conversions when upgrading world

--- src/main/java/net/geforcemods/securitycraft/mixin/datafix/V1460Mixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/datafix/V1460Mixin.java
@@ -1,5 +1,6 @@
 package net.geforcemods.securitycraft.mixin.datafix;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -7,21 +8,26 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.google.common.collect.Lists;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mojang.datafixers.DSL;
 import com.mojang.datafixers.schemas.Schema;
 import com.mojang.datafixers.types.templates.TypeTemplate;
+import com.mojang.datafixers.util.Pair;
 
 import net.minecraft.util.datafix.fixes.References;
 import net.minecraft.util.datafix.schemas.V1460;
 
-/**
- * Registers SecurityCraft's block entities to datafixers, so the item stacks in them can be fixed.
- */
 @Mixin(V1460.class)
 public class V1460Mixin {
+	/**
+	 * Registers SecurityCraft's block entities to datafixers, so the item stacks in them can be fixed.
+	 */
 	@Inject(method = "registerBlockEntities", at = @At("TAIL"))
 	private void securitycraft$registerBlockEntities(Schema schema, CallbackInfoReturnable<Map<String, Supplier<TypeTemplate>>> ci, @Local Map<String, Supplier<TypeTemplate>> map) {
 		registerInventory(schema, map, "securitycraft:keypad_chest");
@@ -71,5 +77,27 @@ public class V1460Mixin {
 	@Unique
 	private static void registerInventory(Schema schema, Map<String, Supplier<TypeTemplate>> map, String blockEntityType, String key) {
 		schema.register(map, blockEntityType, () -> DSL.optionalFields(key, DSL.list(References.ITEM_STACK.in(schema))));
+	}
+
+	/**
+	 * Captures the schema method parameter necessary for registering item stack data fixers, which is used in the mixin below.
+	 */
+	@Inject(method = "lambda$registerTypes$35",at = @At("HEAD"))
+	private static void securitycraft$captureSchema(Schema schema, CallbackInfoReturnable<TypeTemplate> callbackInfo, @Share("schema") LocalRef<Schema> schemaRef) {
+		schemaRef.set(schema);
+	}
+
+	/**
+	 * Adds "ItemInventory" to the list of item inventory NBT paths that should get fixed by datafixer, so the items stored in
+	 * these inventories keep their data when Minecraft updates. This is currently necessary for the proper conversion of the
+	 * inventories of Briefcases, Keycard Holders and Disguise Modules.
+	 */
+	@SuppressWarnings("unchecked")
+	@ModifyArg(method = "lambda$registerTypes$35", at = @At(value = "INVOKE", target = "Lcom/mojang/datafixers/DSL;optionalFields([Lcom/mojang/datafixers/util/Pair;)Lcom/mojang/datafixers/types/templates/TypeTemplate;"))
+	private static Pair<String, TypeTemplate>[] securitycraft$registerCustomInventoryTag(Pair<String, TypeTemplate>[] originalFields, @Share("schema") LocalRef<Schema> schemaRef) {
+		List<Pair<String, TypeTemplate>> newFields = Lists.newArrayList(originalFields);
+
+		newFields.add(Pair.of("ItemInventory", DSL.list(References.ITEM_STACK.in(schemaRef.get()))));
+		return newFields.toArray(new Pair[0]);
 	}
 }
