cbf69bcb7f47e1a244cf164f417e7349959a201c
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-05-15 16:04:55 +0200

    make raft not placeable on lava

--- src/main/java/net/geforcemods/securitycraft/items/SecuritySeaRaftItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/SecuritySeaRaftItem.java
@@ -2,19 +2,34 @@ package net.geforcemods.securitycraft.items;
 
 import net.geforcemods.securitycraft.entity.SecuritySeaRaft;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.item.BoatItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
 public class SecuritySeaRaftItem extends BoatItem {
 	public SecuritySeaRaftItem(Item.Properties properties) {
 		super(true, Boat.Type.BAMBOO, properties);
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+		BlockHitResult hitResult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.ANY);
+
+		if (!level.getFluidState(hitResult.getBlockPos()).is(FluidTags.LAVA))
+			return super.use(level, player, hand);
+		else
+			return InteractionResultHolder.fail(player.getItemInHand(hand));
 	}
 
 	@Override
