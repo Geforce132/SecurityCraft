6c37b234cc8a6f105e3613112214eabe1e7bd950
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2024-05-17 20:08:41 +0200

    Made floor trap particles spawning more consistent

--- src/main/java/net/geforcemods/securitycraft/blocks/FloorTrapBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/FloorTrapBlock.java
@@ -6,7 +6,6 @@ import net.geforcemods.securitycraft.misc.ModuleType;
 import net.geforcemods.securitycraft.util.LevelUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -56,20 +55,14 @@ public class FloorTrapBlock extends SometimesVisibleBlock {
 
 	@Override
 	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
-		return level.isClientSide ? null : createTickerHelper(type, SCContent.FLOOR_TRAP_BLOCK_ENTITY.get(), LevelUtils::blockEntityTicker);
+		if (level.isClientSide)
+			return state.getValue(INVISIBLE) ? createTickerHelper(type, SCContent.FLOOR_TRAP_BLOCK_ENTITY.get(), FloorTrapBlockEntity::particleTick) : null;
+
+		return createTickerHelper(type, SCContent.FLOOR_TRAP_BLOCK_ENTITY.get(), LevelUtils::blockEntityTicker);
 	}
 
 	@Override
 	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
 		return new FloorTrapBlockEntity(pos, state);
-	}
-
-	@Override
-	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
-		if (state.getValue(INVISIBLE)) {
-			for (int i = 0; i < 3; i++) {
-				level.addParticle(SCContent.FLOOR_TRAP_CLOUD.get(), false, pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
-			}
-		}
 	}
 }
