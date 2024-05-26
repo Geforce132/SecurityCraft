3edd309d9a8a7a19951b5870361280fa39f6caa2
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-05-26 23:13:06 +0200

    remove unused admin tool-related code from secret sign block classes

--- src/main/java/net/geforcemods/securitycraft/blocks/SecretStandingSignBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/SecretStandingSignBlock.java
@@ -1,6 +1,5 @@
 package net.geforcemods.securitycraft.blocks;
 
-import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.api.IModuleInventory;
 import net.geforcemods.securitycraft.blockentities.SecretSignBlockEntity;
 import net.geforcemods.securitycraft.misc.OwnershipEvent;
@@ -10,7 +9,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.StandingSignBlock;
@@ -59,9 +57,6 @@ public class SecretStandingSignBlock extends StandingSignBlock {
 
 	@Override
 	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-		if (!level.isClientSide && player.getItemInHand(hand).getItem() == SCContent.ADMIN_TOOL.get())
-			return SCContent.ADMIN_TOOL.get().useOn(new UseOnContext(player, hand, hit));
-
 		SecretSignBlockEntity be = (SecretSignBlockEntity) level.getBlockEntity(pos);
 
 		if (be != null && be.isPlayerAllowedToSeeText(player))
