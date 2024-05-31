4765071a71bdd9a4169af8aa7854c9dc59c16fef
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-05-31 09:19:16 +0200

    revert Crowdin-related changes
    unfortunately using Crowdin did not work out

--- src/main/java/net/geforcemods/securitycraft/screen/SCManualScreen.java
+++ src/main/java/net/geforcemods/securitycraft/screen/SCManualScreen.java
@@ -36,14 +36,13 @@ import net.geforcemods.securitycraft.screen.components.IngredientDisplay;
 import net.geforcemods.securitycraft.screen.components.TextHoverChecker;
 import net.geforcemods.securitycraft.util.BlockUtils;
 import net.geforcemods.securitycraft.util.Utils;
-import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.component.DataComponents;
@@ -54,7 +53,6 @@ import net.minecraft.network.chat.FormattedText;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -98,15 +96,13 @@ public class SCManualScreen extends Screen {
 	private static int lastPage = -1;
 	private final MutableComponent intro1 = Utils.localize("gui.securitycraft:scManual.intro.1").setStyle(Style.EMPTY.withUnderlined(true));
 	private final Component ourPatrons = Utils.localize("gui.securitycraft:scManual.patreon.title");
-	private final Style crowdinLinkStyle = Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://crowdin.com/project/securitycraft"));
 	private List<HoverChecker> hoverCheckers = new ArrayList<>();
 	private int currentPage = lastPage;
 	private NonNullList<Ingredient> recipe;
 	private IngredientDisplay[] displays = new IngredientDisplay[9];
 	private int startX = -1;
 	private List<FormattedText> subpages = new ArrayList<>();
-	private List<FormattedCharSequence> translationCredits;
-	private HoverChecker translationCreditsArea;
+	private List<FormattedCharSequence> author = new ArrayList<>();
 	private int currentSubpage = 0;
 	private List<FormattedCharSequence> intro2;
 	private PatronList patronList;
@@ -147,8 +143,6 @@ public class SCManualScreen extends Screen {
 
 			return key1.compareTo(key2);
 		});
-		translationCredits = font.split(Utils.localize("gui.securitycraft:scManual.crowdin").withStyle(ChatFormatting.BLUE, ChatFormatting.UNDERLINE), 180);
-		translationCreditsArea = new HoverChecker(180, 200, width / 2 - 90, width / 2 + 90);
 	}
 
 	@Override
@@ -207,8 +201,8 @@ public class SCManualScreen extends Screen {
 				guiGraphics.drawString(font, text, width / 2 - font.width(text) / 2, 150 + 10 * i, 0, false);
 			}
 
-			for (int i = 0; i < translationCredits.size(); i++) {
-				FormattedCharSequence text = translationCredits.get(i);
+			for (int i = 0; i < author.size(); i++) {
+				FormattedCharSequence text = author.get(i);
 
 				guiGraphics.drawString(font, text, width / 2 - font.width(text) / 2, 180 + 10 * i, 0, false);
 			}
@@ -324,6 +318,12 @@ public class SCManualScreen extends Screen {
 			recipe = null;
 			nextSubpage.visible = false;
 			previousSubpage.visible = false;
+
+			if (I18n.exists("gui.securitycraft:scManual.author"))
+				author = font.split(Utils.localize("gui.securitycraft:scManual.author"), 180);
+			else
+				author.clear();
+
 			intro2 = font.split(Utils.localize("gui.securitycraft:scManual.intro.2"), 202);
 			patronList.fetchPatrons();
 			return;
@@ -573,11 +573,6 @@ public class SCManualScreen extends Screen {
 	public boolean mouseClicked(double mouseX, double mouseY, int button) {
 		if (patronList != null)
 			patronList.mouseClicked(mouseX, mouseY, button);
-
-		if (translationCreditsArea.checkHover(mouseX, mouseY)) {
-			handleComponentClicked(crowdinLinkStyle);
-			minecraft.getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
-		}
 
 		return super.mouseClicked(mouseX, mouseY, button);
 	}
