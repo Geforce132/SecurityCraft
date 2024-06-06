0c3ffa1733a15a8eecb8b539f4023d4ef46cebef
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-06-06 20:18:25 +0200

    update mod dependencies

--- src/main/java/net/geforcemods/securitycraft/compat/ftbteams/FTBTeamsCompat.java
+++ src/main/java/net/geforcemods/securitycraft/compat/ftbteams/FTBTeamsCompat.java
@@ -2,7 +2,15 @@ package net.geforcemods.securitycraft.compat.ftbteams;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
 
+import dev.ftb.mods.ftbteams.api.FTBTeamsAPI;
+import dev.ftb.mods.ftbteams.api.FTBTeamsAPI.API;
+import dev.ftb.mods.ftbteams.api.Team;
+import dev.ftb.mods.ftbteams.api.client.ClientTeamManager;
+import dev.ftb.mods.ftbteams.api.client.KnownClientPlayer;
+import dev.ftb.mods.ftbteams.api.property.TeamProperties;
 import net.geforcemods.securitycraft.api.Owner;
 import net.geforcemods.securitycraft.util.TeamUtils.TeamRepresentation;
 import net.minecraft.server.level.ServerPlayer;
@@ -11,70 +19,70 @@ public class FTBTeamsCompat {
 	private FTBTeamsCompat() {}
 
 	public static boolean areOnSameTeam(Owner owner1, Owner owner2) {
-		//		try {
-		//			API api = FTBTeamsAPI.api();
-		//
-		//			if (api.isManagerLoaded())
-		//				return api.getManager().arePlayersInSameTeam(UUID.fromString(owner1.getUUID()), UUID.fromString(owner2.getUUID()));
-		//			else if (api.isClientManagerLoaded()) {
-		//				ClientTeamManager manager = api.getClientManager();
-		//				Optional<KnownClientPlayer> optional1 = manager.getKnownPlayer(UUID.fromString(owner1.getUUID()));
-		//
-		//				if (optional1.isPresent()) {
-		//					Optional<KnownClientPlayer> optional2 = manager.getKnownPlayer(UUID.fromString(owner2.getUUID()));
-		//
-		//					return optional2.isPresent() && optional1.get().teamId().equals(optional2.get().teamId());
-		//				}
-		//			}
-		//		}
-		//		catch (IllegalArgumentException e) {}
+		try {
+			API api = FTBTeamsAPI.api();
+
+			if (api.isManagerLoaded())
+				return api.getManager().arePlayersInSameTeam(UUID.fromString(owner1.getUUID()), UUID.fromString(owner2.getUUID()));
+			else if (api.isClientManagerLoaded()) {
+				ClientTeamManager manager = api.getClientManager();
+				Optional<KnownClientPlayer> optional1 = manager.getKnownPlayer(UUID.fromString(owner1.getUUID()));
+
+				if (optional1.isPresent()) {
+					Optional<KnownClientPlayer> optional2 = manager.getKnownPlayer(UUID.fromString(owner2.getUUID()));
+
+					return optional2.isPresent() && optional1.get().teamId().equals(optional2.get().teamId());
+				}
+			}
+		}
+		catch (IllegalArgumentException e) {}
 
 		return false;
 	}
 
 	public static TeamRepresentation getTeamRepresentation(Owner owner) {
-		//		try {
-		//			API api = FTBTeamsAPI.api();
-		//			Team team = null;
-		//
-		//			if (api.isManagerLoaded()) {
-		//				Optional<Team> optional = api.getManager().getTeamForPlayerID(UUID.fromString(owner.getUUID()));
-		//
-		//				if (optional.isPresent())
-		//					team = optional.get();
-		//			}
-		//			else if (api.isClientManagerLoaded()) {
-		//				ClientTeamManager manager = api.getClientManager();
-		//				Optional<KnownClientPlayer> optionalPlayer = manager.getKnownPlayer(UUID.fromString(owner.getUUID()));
-		//
-		//				if (optionalPlayer.isPresent()) {
-		//					Optional<Team> optionalTeam = manager.getTeamByID(optionalPlayer.get().teamId());
-		//
-		//					if (optionalTeam.isPresent())
-		//						team = optionalTeam.get();
-		//				}
-		//			}
-		//
-		//			if (team != null)
-		//				return new TeamRepresentation(team.getProperty(TeamProperties.DISPLAY_NAME), team.getProperty(TeamProperties.COLOR).toStyle().getColor().getValue());
-		//		}
-		//		catch (IllegalArgumentException e) {}
+		try {
+			API api = FTBTeamsAPI.api();
+			Team team = null;
+
+			if (api.isManagerLoaded()) {
+				Optional<Team> optional = api.getManager().getTeamForPlayerID(UUID.fromString(owner.getUUID()));
+
+				if (optional.isPresent())
+					team = optional.get();
+			}
+			else if (api.isClientManagerLoaded()) {
+				ClientTeamManager manager = api.getClientManager();
+				Optional<KnownClientPlayer> optionalPlayer = manager.getKnownPlayer(UUID.fromString(owner.getUUID()));
+
+				if (optionalPlayer.isPresent()) {
+					Optional<Team> optionalTeam = manager.getTeamByID(optionalPlayer.get().teamId());
+
+					if (optionalTeam.isPresent())
+						team = optionalTeam.get();
+				}
+			}
+
+			if (team != null)
+				return new TeamRepresentation(team.getProperty(TeamProperties.DISPLAY_NAME), team.getProperty(TeamProperties.COLOR).toStyle().getColor().getValue());
+		}
+		catch (IllegalArgumentException e) {}
 
 		return null;
 	}
 
 	public static Collection<ServerPlayer> getOnlinePlayersInTeam(Owner owner) {
-		//		try {
-		//			API api = FTBTeamsAPI.api();
-		//
-		//			if (api.isManagerLoaded()) {
-		//				Optional<Team> optional = api.getManager().getTeamForPlayerID(UUID.fromString(owner.getUUID()));
-		//
-		//				if (optional.isPresent())
-		//					return optional.get().getOnlineMembers();
-		//			}
-		//		}
-		//		catch (IllegalArgumentException e) {}
+		try {
+			API api = FTBTeamsAPI.api();
+
+			if (api.isManagerLoaded()) {
+				Optional<Team> optional = api.getManager().getTeamForPlayerID(UUID.fromString(owner.getUUID()));
+
+				if (optional.isPresent())
+					return optional.get().getOnlineMembers();
+			}
+		}
+		catch (IllegalArgumentException e) {}
 
 		return new ArrayList<>();
 	}
--- src/main/java/net/geforcemods/securitycraft/compat/sodium/SodiumCompat.java
+++ src/main/java/net/geforcemods/securitycraft/compat/sodium/SodiumCompat.java
@@ -1,15 +1,17 @@
 package net.geforcemods.securitycraft.compat.sodium;
 
+import me.jellysquid.mods.sodium.client.render.chunk.map.ChunkStatus;
+import me.jellysquid.mods.sodium.client.render.chunk.map.ChunkTrackerHolder;
 import net.minecraft.client.multiplayer.ClientLevel;
 
 public class SodiumCompat {
 	private SodiumCompat() {}
 
 	public static void onChunkStatusAdded(ClientLevel level, int x, int z) {
-		//		ChunkTrackerHolder.get(level).onChunkStatusAdded(x, z, ChunkStatus.FLAG_HAS_BLOCK_DATA);
+		ChunkTrackerHolder.get(level).onChunkStatusAdded(x, z, ChunkStatus.FLAG_HAS_BLOCK_DATA);
 	}
 
 	public static void onChunkStatusRemoved(ClientLevel level, int x, int z) {
-		//		ChunkTrackerHolder.get(level).onChunkStatusRemoved(x, z, ChunkStatus.FLAG_HAS_BLOCK_DATA);
+		ChunkTrackerHolder.get(level).onChunkStatusRemoved(x, z, ChunkStatus.FLAG_HAS_BLOCK_DATA);
 	}
 }
