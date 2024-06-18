5c6b738dd6670ae9bcb6b788b9f31b038e4e3696
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-06-18 11:34:09 +0200

    actually readd jei integration

--- src/main/java/net/geforcemods/securitycraft/compat/jei/VanillaToSecurityCraftCategory.java
+++ src/main/java/net/geforcemods/securitycraft/compat/jei/VanillaToSecurityCraftCategory.java
@@ -1,30 +1,43 @@
 package net.geforcemods.securitycraft.compat.jei;
 
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import net.geforcemods.securitycraft.util.Utils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+
 public class VanillaToSecurityCraftCategory extends BaseCategory {
-	//	private static final Component TITLE = Utils.localize("jei.securitycraft.category.reinforcing");
-	//
-	//	public VanillaToSecurityCraftCategory(IGuiHelper helper) {
-	//		super(helper);
-	//	}
-	//
-	//	@Override
-	//	public void draw(ReinforcerRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
-	//		guiGraphics.drawString(Minecraft.getInstance().font, OUTPUT_TEXT, 24, 5, 0x404040, false);
-	//	}
-	//
-	//	@Override
-	//	public Component getTitle() {
-	//		return TITLE;
-	//	}
-	//
-	//	@Override
-	//	public RecipeType<ReinforcerRecipe> getRecipeType() {
-	//		return SCJEIPlugin.VTS;
-	//	}
-	//
-	//	@Override
-	//	public void setRecipe(IRecipeLayoutBuilder builder, ReinforcerRecipe recipe, IFocusGroup focuses) {
-	//		builder.addSlot(RecipeIngredientRole.INPUT, 1, 1).addIngredient(VanillaTypes.ITEM_STACK, new ItemStack(recipe.vanillaBlock()));
-	//		builder.addSlot(RecipeIngredientRole.OUTPUT, 91, 1).addIngredient(VanillaTypes.ITEM_STACK, new ItemStack(recipe.securityCraftBlock()));
-	//	}
+	private static final Component TITLE = Utils.localize("jei.securitycraft.category.reinforcing");
+
+	public VanillaToSecurityCraftCategory(IGuiHelper helper) {
+		super(helper);
+	}
+
+	@Override
+	public void draw(ReinforcerRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+		guiGraphics.drawString(Minecraft.getInstance().font, OUTPUT_TEXT, 24, 5, 0x404040, false);
+	}
+
+	@Override
+	public Component getTitle() {
+		return TITLE;
+	}
+
+	@Override
+	public RecipeType<ReinforcerRecipe> getRecipeType() {
+		return SCJEIPlugin.VTS;
+	}
+
+	@Override
+	public void setRecipe(IRecipeLayoutBuilder builder, ReinforcerRecipe recipe, IFocusGroup focuses) {
+		builder.addSlot(RecipeIngredientRole.INPUT, 1, 1).addIngredient(VanillaTypes.ITEM_STACK, new ItemStack(recipe.vanillaBlock()));
+		builder.addSlot(RecipeIngredientRole.OUTPUT, 91, 1).addIngredient(VanillaTypes.ITEM_STACK, new ItemStack(recipe.securityCraftBlock()));
+	}
 }
