2798d8d205392e2bac6ff3081331f0946359ce8a
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-07-01 10:44:37 +0200

    make sure bubble columns can survive on reinforced soul sand/magma block

--- src/main/java/net/geforcemods/securitycraft/mixin/reinforced/BubbleColumnBlockMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/reinforced/BubbleColumnBlockMixin.java
@@ -4,8 +4,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.geforcemods.securitycraft.SCContent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BubbleColumnBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,10 +20,16 @@ import net.minecraft.world.level.block.state.BlockState;
 @Mixin(BubbleColumnBlock.class)
 public class BubbleColumnBlockMixin {
 	@Inject(method = "getColumnState", at = @At("TAIL"), cancellable = true)
-	private static void securitycraft$supportReinforcedVersions(BlockState state, CallbackInfoReturnable<BlockState> cir) {
+	private static void securitycraft$getColumnStateOfReinforcedVersions(BlockState state, CallbackInfoReturnable<BlockState> cir) {
 		if (state.is(SCContent.REINFORCED_SOUL_SAND.get()))
 			cir.setReturnValue(Blocks.BUBBLE_COLUMN.defaultBlockState().setValue(BubbleColumnBlock.DRAG_DOWN, false));
 		else if (state.is(SCContent.REINFORCED_MAGMA_BLOCK.get()))
 			cir.setReturnValue(Blocks.BUBBLE_COLUMN.defaultBlockState().setValue(BubbleColumnBlock.DRAG_DOWN, true));
+	}
+
+	@Inject(method = "canSurvive", at = @At("TAIL"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+	private void securitycraft$reinforcedVersionsCanSurvive(BlockState state, LevelReader level, BlockPos pos, CallbackInfoReturnable<Boolean> cir, BlockState stateBelow) {
+		if (stateBelow.is(SCContent.REINFORCED_SOUL_SAND.get()) || stateBelow.is(SCContent.REINFORCED_MAGMA_BLOCK.get()))
+			cir.setReturnValue(true);
 	}
 }
