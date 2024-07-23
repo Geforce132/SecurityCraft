c63dcd01969c424f910503b98afc26d1f83b6317
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-07-23 20:44:34 +0200

    add item capabilities to fake liquid buckets
    fixes the buckets not working with other mods' tanks

--- src/main/java/net/geforcemods/securitycraft/items/FakeLiquidBucketItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/FakeLiquidBucketItem.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockSource;
 import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.DispensibleContainerItem;
 import net.minecraft.world.item.ItemStack;
@@ -12,27 +13,32 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper;
 
 public class FakeLiquidBucketItem extends BucketItem {
 	public FakeLiquidBucketItem(Supplier<? extends Fluid> supplier, Properties builder) {
 		super(supplier, builder);
 
 		DispenserBlock.registerBehavior(this, new DefaultDispenseItemBehavior() {
-			private final DefaultDispenseItemBehavior instance = new DefaultDispenseItemBehavior();
-
 			@Override
 			public ItemStack execute(BlockSource source, ItemStack stack) {
 				DispensibleContainerItem bucket = (DispensibleContainerItem) stack.getItem();
-				BlockPos pos = source.getPos().relative(source.getBlockState().getValue(DispenserBlock.FACING));
+				BlockPos dispenseAt = source.getPos().relative(source.getBlockState().getValue(DispenserBlock.FACING));
 				Level level = source.getLevel();
 
-				if (bucket.emptyContents(null, level, pos, null)) {
-					bucket.checkExtraContent(null, level, stack, pos);
+				if (bucket.emptyContents(null, level, dispenseAt, null)) {
+					bucket.checkExtraContent(null, level, stack, dispenseAt);
 					return new ItemStack(Items.BUCKET);
 				}
 				else
-					return instance.dispense(source, stack);
+					return dispense(source, stack);
 			}
 		});
+	}
+
+	@Override
+	public ICapabilityProvider initCapabilities(ItemStack stack, CompoundTag nbt) {
+		return new FluidBucketWrapper(stack);
 	}
 }
