c35cf5c4d875fff7e927ceb1882633ea7f159ef4
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-07-23 20:54:30 +0200

    make smart module save camera zoom as well

--- src/main/java/net/geforcemods/securitycraft/entity/camera/CameraController.java
+++ src/main/java/net/geforcemods/securitycraft/entity/camera/CameraController.java
@@ -52,8 +52,8 @@ public class CameraController {
 				//@formatter:off
 				new ViewMovementKeyHandler(mc.options.keyUp, CameraController::moveViewUp),
 				new ViewMovementKeyHandler(mc.options.keyDown, CameraController::moveViewDown),
-				new ViewMovementKeyHandler(mc.options.keyLeft, cam -> moveViewHorizontally(cam, cam.getYRot() - getMovementSpeed(cam) * cam.zoomAmount)),
-				new ViewMovementKeyHandler(mc.options.keyRight, cam -> moveViewHorizontally(cam, cam.getYRot() + getMovementSpeed(cam) * cam.zoomAmount))
+				new ViewMovementKeyHandler(mc.options.keyLeft, cam -> moveViewHorizontally(cam, cam.getYRot() - getMovementSpeed(cam) * cam.getZoomAmount())),
+				new ViewMovementKeyHandler(mc.options.keyRight, cam -> moveViewHorizontally(cam, cam.getYRot() + getMovementSpeed(cam) * cam.getZoomAmount()))
 				//@formatter:on
 			};
 		}
@@ -132,7 +132,7 @@ public class CameraController {
 	}
 
 	public static void moveViewUp(SecurityCamera cam) {
-		float next = cam.getXRot() - getMovementSpeed(cam) * cam.zoomAmount;
+		float next = cam.getXRot() - getMovementSpeed(cam) * cam.getZoomAmount();
 
 		if (cam.isCameraDown()) {
 			if (next > 40F)
@@ -143,7 +143,7 @@ public class CameraController {
 	}
 
 	public static void moveViewDown(SecurityCamera cam) {
-		float next = cam.getXRot() + getMovementSpeed(cam) * cam.zoomAmount;
+		float next = cam.getXRot() + getMovementSpeed(cam) * cam.getZoomAmount();
 
 		if (cam.isCameraDown()) {
 			if (next < 90F)
@@ -181,7 +181,7 @@ public class CameraController {
 			Minecraft.getInstance().level.playLocalSound(cam.blockPosition(), SCSounds.CAMERAZOOMIN.event, SoundSource.BLOCKS, 1.0F, 1.0F, true);
 
 		cam.zooming = true;
-		cam.zoomAmount = Math.max(cam.zoomAmount - 0.1F, 0.1F);
+		cam.setZoomAmount(Math.max(cam.getZoomAmount() - 0.1F, 0.1F));
 	}
 
 	public static void zoomOut(SecurityCamera cam) {
@@ -189,7 +189,7 @@ public class CameraController {
 			Minecraft.getInstance().level.playLocalSound(cam.blockPosition(), SCSounds.CAMERAZOOMIN.event, SoundSource.BLOCKS, 1.0F, 1.0F, true);
 
 		cam.zooming = true;
-		cam.zoomAmount = Math.min(cam.zoomAmount + 0.1F, 1.4F);
+		cam.setZoomAmount(Math.min(cam.getZoomAmount() + 0.1F, 1.4F));
 	}
 
 	public static void toggleRedstone(SecurityCamera cam) {
