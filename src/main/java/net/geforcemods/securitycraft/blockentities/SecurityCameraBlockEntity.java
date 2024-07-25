3507320ec7ee5eae117ea771a5ed2219837a27cc
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-07-25 14:30:36 +0200

    don't set initial camera zoom if it hasn't existed before

--- src/main/java/net/geforcemods/securitycraft/blockentities/SecurityCameraBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/SecurityCameraBlockEntity.java
@@ -106,7 +106,9 @@ public class SecurityCameraBlockEntity extends DisguisableBlockEntity implements
 		lens.fromTag(tag.getList("lens", Tag.TAG_COMPOUND));
 		initialXRotation = tag.getFloat("initial_x_rotation");
 		initialYRotation = tag.getFloat("initial_y_rotation");
-		initialZoom = tag.getFloat("initial_zoom");
+
+		if (tag.contains("initial_zoom"))
+			initialZoom = tag.getFloat("initial_zoom");
 	}
 
 	public static IItemHandler getCapability(SecurityCameraBlockEntity be, Direction side) {
