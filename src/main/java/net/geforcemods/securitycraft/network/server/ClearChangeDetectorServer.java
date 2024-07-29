6b44d2c545f0988ed3dd31ba5bbdb425a32ced9d
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-07-29 23:13:34 +0200

    fix bcd not turning off redstone signal when all entries are removed

--- src/main/java/net/geforcemods/securitycraft/network/server/ClearChangeDetectorServer.java
+++ src/main/java/net/geforcemods/securitycraft/network/server/ClearChangeDetectorServer.java
@@ -2,11 +2,17 @@ package net.geforcemods.securitycraft.network.server;
 
 import java.util.function.Supplier;
 
+import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.blockentities.BlockChangeDetectorBlockEntity;
+import net.geforcemods.securitycraft.blocks.AbstractPanelBlock;
+import net.geforcemods.securitycraft.blocks.BlockChangeDetectorBlock;
+import net.geforcemods.securitycraft.util.BlockUtils;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkEvent;
 
 public class ClearChangeDetectorServer {
@@ -28,15 +34,23 @@ public class ClearChangeDetectorServer {
 
 	public void handle(Supplier<NetworkEvent.Context> ctx) {
 		ServerPlayerEntity player = ctx.get().getSender();
-		TileEntity te = player.level.getBlockEntity(pos);
+		World level = player.level;
+		TileEntity te = level.getBlockEntity(pos);
 
 		if (te instanceof BlockChangeDetectorBlockEntity) {
 			BlockChangeDetectorBlockEntity be = (BlockChangeDetectorBlockEntity) te;
 
 			if (be.isOwnedBy(player)) {
+				BlockState state = be.getBlockState();
+
 				be.getEntries().clear();
 				be.setChanged();
-				be.getLevel().sendBlockUpdated(be.getBlockPos(), be.getBlockState(), be.getBlockState(), 2);
+				level.sendBlockUpdated(pos, state, state, 2);
+
+				if (state.getValue(BlockChangeDetectorBlock.POWERED)) {
+					level.setBlockAndUpdate(pos, state.setValue(BlockChangeDetectorBlock.POWERED, false));
+					BlockUtils.updateIndirectNeighbors(level, pos, SCContent.BLOCK_CHANGE_DETECTOR.get(), AbstractPanelBlock.getConnectedDirection(state).getOpposite());
+				}
 			}
 		}
 	}
