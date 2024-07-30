8bde5326711ba34057cee5711aabf670b3540f9f
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-07-30 10:50:58 +0200

    fix bcd not turning off redstone signal when all entries are removed

--- src/main/java/net/geforcemods/securitycraft/network/server/ClearChangeDetectorServer.java
+++ src/main/java/net/geforcemods/securitycraft/network/server/ClearChangeDetectorServer.java
@@ -2,10 +2,14 @@ package net.geforcemods.securitycraft.network.server;
 
 import io.netty.buffer.ByteBuf;
 import net.geforcemods.securitycraft.blockentities.BlockChangeDetectorBlockEntity;
+import net.geforcemods.securitycraft.blocks.BlockChangeDetectorBlock;
+import net.geforcemods.securitycraft.util.BlockUtils;
 import net.geforcemods.securitycraft.util.Utils;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -34,15 +38,26 @@ public class ClearChangeDetectorServer implements IMessage {
 		public IMessage onMessage(ClearChangeDetectorServer message, MessageContext ctx) {
 			Utils.addScheduledTask(ctx.getServerHandler().player.world, () -> {
 				EntityPlayer player = ctx.getServerHandler().player;
-				TileEntity tile = player.world.getTileEntity(message.pos);
+				World level = player.world;
+				BlockPos pos = message.pos;
+				TileEntity tile = level.getTileEntity(pos);
 
 				if (tile instanceof BlockChangeDetectorBlockEntity) {
 					BlockChangeDetectorBlockEntity te = (BlockChangeDetectorBlockEntity) tile;
 
 					if (te.isOwnedBy(player)) {
+						IBlockState state = level.getBlockState(pos);
+
 						te.getEntries().clear();
 						te.markDirty();
 						te.sync();
+
+						if (state.getValue(BlockChangeDetectorBlock.POWERED)) {
+							BlockChangeDetectorBlock block = (BlockChangeDetectorBlock) state.getBlock();
+
+							level.setBlockState(pos, state.withProperty(BlockChangeDetectorBlock.POWERED, false));
+							BlockUtils.updateIndirectNeighbors(level, pos, block, block.getConnectedDirection(state).getOpposite());
+						}
 					}
 				}
 			});
