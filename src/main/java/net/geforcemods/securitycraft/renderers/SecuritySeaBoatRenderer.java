4598841614bad4488d13f4344799a9343d9da1f4
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-08-23 11:06:49 +0200

    Fix startup crash when modded boats are present
    Closes #538

--- src/main/java/net/geforcemods/securitycraft/renderers/SecuritySeaBoatRenderer.java
+++ src/main/java/net/geforcemods/securitycraft/renderers/SecuritySeaBoatRenderer.java
@@ -1,7 +1,5 @@
 package net.geforcemods.securitycraft.renderers;
 
-import java.util.stream.Stream;
-
 import com.google.common.collect.ImmutableMap;
 import com.mojang.datafixers.util.Pair;
 
@@ -12,26 +10,40 @@ import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.entity.BoatRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.entity.vehicle.Boat.Type;
+import net.neoforged.fml.common.asm.enumextension.ExtensionInfo;
 
 public class SecuritySeaBoatRenderer extends BoatRenderer {
 	public SecuritySeaBoatRenderer(EntityRendererProvider.Context ctx) {
 		super(ctx, true);
-		//@formatter:off
-        boatResources = Stream.of(Boat.Type.values())
-                .collect(ImmutableMap.toImmutableMap(
-                        type -> type,
-                        type -> Pair.of(SecurityCraft.resLoc("textures/entity/security_sea_boat/" + type.getName() + ".png"), createBoatModel(ctx, type, true))));
+
+		ImmutableMap.Builder<Boat.Type, Pair<ResourceLocation, ListModel<Boat>>> mapBuilder = ImmutableMap.builder();
+		ExtensionInfo extensionInfo = Boat.Type.getExtensionInfo();
+		int size;
+
+		if (!extensionInfo.extended())
+			size = Boat.Type.values().length;
+		else
+			size = extensionInfo.vanillaCount();
+
+		for (int i = 0; i < size; i++) {
+			Boat.Type type = Boat.Type.values()[i];
+
+			mapBuilder.put(type, Pair.of(SecurityCraft.resLoc("textures/entity/security_sea_boat/" + type.getName() + ".png"), createBoatModel(ctx, type, true)));
+		}
+
+		boatResources = mapBuilder.build();
 	}
 
 	@Override
 	public ListModel<Boat> createBoatModel(Context ctx, Type type, boolean chestBoat) {
-        ModelPart modelPart = ctx.bakeLayer(ModelLayers.createChestBoatModelName(type));
+		ModelPart modelPart = ctx.bakeLayer(ModelLayers.createChestBoatModelName(type));
 
-        if (type == Boat.Type.BAMBOO)
-            return new SecuritySeaRaftModel(modelPart);
-         else
-            return new SecuritySeaBoatModel(modelPart);
+		if (type == Boat.Type.BAMBOO)
+			return new SecuritySeaRaftModel(modelPart);
+		else
+			return new SecuritySeaBoatModel(modelPart);
 	}
 }
