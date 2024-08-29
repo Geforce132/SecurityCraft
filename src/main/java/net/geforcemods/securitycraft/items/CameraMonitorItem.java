e616b9c7556c654aa1d5245b1b34b2737394b7ab
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-08-29 16:16:39 +0200

    Make camera monitor remember camera names

--- src/main/java/net/geforcemods/securitycraft/items/CameraMonitorItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/CameraMonitorItem.java
@@ -3,6 +3,8 @@ package net.geforcemods.securitycraft.items;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import net.geforcemods.securitycraft.ClientHandler;
 import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.blockentities.SecurityCameraBlockEntity;
@@ -26,6 +28,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 public class CameraMonitorItem extends Item {
 	public CameraMonitorItem(Item.Properties properties) {
@@ -77,10 +80,14 @@ public class CameraMonitorItem extends Item {
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 		ItemStack stack = player.getItemInHand(hand);
 
-		if (!stack.hasTag() || !hasCameraAdded(stack.getTag())) {
-			PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.CAMERA_MONITOR.get().getDescriptionId()), Utils.localize("messages.securitycraft:cameraMonitor.rightclickToView"), ChatFormatting.RED);
-			return InteractionResultHolder.pass(stack);
+		if (!stack.hasTag()) {
+			if (!hasCameraAdded(stack.getTag())) {
+				PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.CAMERA_MONITOR.get().getDescriptionId()), Utils.localize("messages.securitycraft:cameraMonitor.rightclickToView"), ChatFormatting.RED);
+				return InteractionResultHolder.pass(stack);
+			}
 		}
+		else
+			updateTagWithNames(stack, level);
 
 		if (level.isClientSide && stack.getItem() == SCContent.CAMERA_MONITOR.get())
 			ClientHandler.displayCameraMonitorScreen(player.getInventory(), (CameraMonitorItem) stack.getItem(), stack.getTag());
@@ -134,18 +141,24 @@ public class CameraMonitorItem extends Item {
 		return false;
 	}
 
-	public static List<GlobalPos> getCameraPositions(CompoundTag tag) {
-		ArrayList<GlobalPos> list = new ArrayList<>();
+	public static List<Pair<GlobalPos, String>> getCameraPositions(CompoundTag tag) {
+		List<Pair<GlobalPos, String>> list = new ArrayList<>();
 
 		for (int i = 1; i <= 30; i++) {
 			if (tag != null && tag.contains("Camera" + i)) {
 				String[] coords = tag.getString("Camera" + i).split(" ");
+				BlockPos pos = new BlockPos(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]), Integer.parseInt(coords[2]));
+				String nameKey = "camera" + i + "_name";
+				String cameraName = null;
+
+				if (tag.contains(nameKey))
+					cameraName = tag.getString(nameKey);
 
 				//default to overworld if there is no dimension saved
-				list.add(GlobalPos.of(coords.length == 4 ? ResourceKey.create(Registries.DIMENSION, new ResourceLocation(coords[3])) : Level.OVERWORLD, new BlockPos(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]), Integer.parseInt(coords[2]))));
+				list.add(Pair.of(GlobalPos.of(coords.length == 4 ? ResourceKey.create(Registries.DIMENSION, new ResourceLocation(coords[3])) : Level.OVERWORLD, pos), cameraName));
 			}
 			else
-				list.add(null);
+				list.add(Pair.of(null, null));
 		}
 
 		return list;
@@ -163,5 +176,33 @@ public class CameraMonitorItem extends Item {
 		}
 
 		return amount;
+	}
+
+	private void updateTagWithNames(ItemStack stack, Level level) {
+		if (!stack.hasTag())
+			return;
+
+		CompoundTag tag = stack.getTag();
+
+		for (int i = 1; i <= 30; i++) {
+			String cameraString = tag.getString("Camera" + i);
+			String[] globalPos = cameraString.split(" ");
+			String nameKey = "camera" + i + "_name";
+
+			if (globalPos.length == 3 || (globalPos.length == 4 && level.dimension().location().toString().equals(globalPos[3]))) {
+				BlockPos camPos = new BlockPos(Integer.parseInt(globalPos[0]), Integer.parseInt(globalPos[1]), Integer.parseInt(globalPos[2]));
+
+				if (level.isLoaded(camPos)) {
+					BlockEntity be = level.getBlockEntity(camPos);
+
+					if (be instanceof SecurityCameraBlockEntity camera && camera.hasCustomName()) {
+						tag.putString(nameKey, camera.getCustomName().getString());
+						continue;
+					}
+
+					tag.remove(nameKey);
+				}
+			}
+		}
 	}
 }
