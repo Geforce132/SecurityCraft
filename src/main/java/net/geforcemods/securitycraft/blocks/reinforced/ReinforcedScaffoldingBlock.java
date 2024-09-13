3d159d7a5a7f399c4a59f19ce3ecbaf89d878c2a
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-09-13 15:06:23 +0200

    Fix reinforced scaffolding losing owner and only allow owner to climb it

--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedScaffoldingBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedScaffoldingBlock.java
@@ -1,13 +1,16 @@
 package net.geforcemods.securitycraft.blocks.reinforced;
 
 import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.api.IOwnable;
 import net.geforcemods.securitycraft.api.IReinforcedBlock;
 import net.geforcemods.securitycraft.api.OwnableBlockEntity;
 import net.geforcemods.securitycraft.misc.OwnershipEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.entity.player.Player;
@@ -24,6 +27,10 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.common.NeoForge;
 
 public class ReinforcedScaffoldingBlock extends ScaffoldingBlock implements EntityBlock, IReinforcedBlock {
@@ -48,8 +55,25 @@ public class ReinforcedScaffoldingBlock extends ScaffoldingBlock implements Enti
 	}
 
 	@Override
+	protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext ctx) {
+		if (ctx instanceof EntityCollisionContext ectx && ownsScaffolding(level, pos, ectx.getEntity()))
+			return super.getCollisionShape(state, level, pos, ctx);
+		else
+			return Shapes.empty();
+	}
+
+	@Override
 	public boolean isScaffolding(BlockState state, LevelReader level, BlockPos pos, LivingEntity entity) {
-		return true;
+		return ownsScaffolding(level, pos, entity);
+	}
+
+	@Override
+	public boolean isLadder(BlockState state, LevelReader level, BlockPos pos, LivingEntity entity) {
+		return super.isLadder(state, level, pos, entity) && ownsScaffolding(level, pos, entity);
+	}
+
+	public boolean ownsScaffolding(BlockGetter level, BlockPos pos, Entity entity) {
+		return level.getBlockEntity(pos) instanceof IOwnable ownable && ownable.isOwnedBy(entity);
 	}
 
 	@Override
@@ -65,9 +89,14 @@ public class ReinforcedScaffoldingBlock extends ScaffoldingBlock implements Enti
 	protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
 		int distance = getDistance(level, pos);
 		BlockState newState = state.setValue(DISTANCE, distance).setValue(BOTTOM, isBottom(level, pos, distance));
+
 		if (newState.getValue(DISTANCE) == 7) {
-			if (state.getValue(DISTANCE) == 7)
-				FallingBlockEntity.fall(level, pos, newState);
+			if (state.getValue(DISTANCE) == 7) {
+				CompoundTag data = level.getBlockEntity(pos).saveWithoutMetadata(level.registryAccess());
+				FallingBlockEntity entity = FallingBlockEntity.fall(level, pos, newState);
+
+				entity.blockData = data;
+			}
 			else
 				level.destroyBlock(pos, true);
 		}
