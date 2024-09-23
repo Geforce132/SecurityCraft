fedfe7357806450e9b39483ba6ba56e5ff3e2fad
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-09-23 12:10:36 +0200

    Change reinforced observer to need owner validation

--- src/main/java/net/geforcemods/securitycraft/blockentities/ReinforcedObserverBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/ReinforcedObserverBlockEntity.java
@@ -1,7 +1,11 @@
 package net.geforcemods.securitycraft.blockentities;
 
 import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.api.Owner;
+import net.geforcemods.securitycraft.blocks.reinforced.ReinforcedObserverBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -12,6 +16,17 @@ public class ReinforcedObserverBlockEntity extends DisguisableBlockEntity {
 
 	public ReinforcedObserverBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
 		super(type, pos, state);
+	}
+
+	@Override
+	public void onOwnerChanged(BlockState state, Level level, BlockPos pos, Player player, Owner oldOwner, Owner newOwner) {
+		level.setBlockAndUpdate(pos, state.setValue(ReinforcedObserverBlock.POWERED, false));
+		super.onOwnerChanged(state, level, pos, player, oldOwner, newOwner);
+	}
+
+	@Override
+	public boolean needsValidation() {
+		return true;
 	}
 
 	@Override
