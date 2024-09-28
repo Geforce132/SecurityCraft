5d003518bf1f8c188c948d15914ce72772b53ae6
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-09-28 16:58:35 +0200

    Automatically open the check passcode screen after setting a passcode

--- src/main/java/net/geforcemods/securitycraft/network/server/SetBriefcasePasscodeAndOwner.java
+++ src/main/java/net/geforcemods/securitycraft/network/server/SetBriefcasePasscodeAndOwner.java
@@ -4,6 +4,7 @@ import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.SecurityCraft;
 import net.geforcemods.securitycraft.components.OwnerData;
 import net.geforcemods.securitycraft.components.PasscodeData;
+import net.geforcemods.securitycraft.network.client.OpenScreen;
 import net.geforcemods.securitycraft.util.PasscodeUtils;
 import net.geforcemods.securitycraft.util.PlayerUtils;
 import net.minecraft.network.RegistryFriendlyByteBuf;
@@ -11,8 +12,10 @@ import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
 public record SetBriefcasePasscodeAndOwner(String passcode) implements CustomPacketPayload {
@@ -36,8 +39,11 @@ public record SetBriefcasePasscodeAndOwner(String passcode) implements CustomPac
 			if (!stack.has(SCContent.OWNER_DATA))
 				stack.set(SCContent.OWNER_DATA, OwnerData.fromPlayer(player, true));
 
-			if (!passcode.isEmpty() && !stack.has(SCContent.PASSCODE_DATA))
-				PasscodeData.hashAndSetPasscode(stack, passcode, newPasscodeData -> {});
+			if (!passcode.isEmpty() && !stack.has(SCContent.PASSCODE_DATA)) {
+				PasscodeData.hashAndSetPasscode(stack, passcode, newPasscodeData -> {
+					PacketDistributor.sendToPlayer((ServerPlayer) player, new OpenScreen(OpenScreen.DataType.CHECK_PASSCODE_FOR_BRIEFCASE));
+				});
+			}
 		}
 	}
 }
