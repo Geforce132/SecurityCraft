d1def0aede679e8a81d7bc71a6beddfb4e081ecd
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-11-15 10:45:00 +0100

    Add allowlist module support to the panic button

--- src/main/java/net/geforcemods/securitycraft/SCContent.java
+++ src/main/java/net/geforcemods/securitycraft/SCContent.java
@@ -40,6 +40,7 @@ import net.geforcemods.securitycraft.blockentities.KeypadTrapdoorBlockEntity;
 import net.geforcemods.securitycraft.blockentities.LaserBlockBlockEntity;
 import net.geforcemods.securitycraft.blockentities.MineBlockEntity;
 import net.geforcemods.securitycraft.blockentities.MotionActivatedLightBlockEntity;
+import net.geforcemods.securitycraft.blockentities.PanicButtonBlockEntity;
 import net.geforcemods.securitycraft.blockentities.PortableRadarBlockEntity;
 import net.geforcemods.securitycraft.blockentities.ProjectorBlockEntity;
 import net.geforcemods.securitycraft.blockentities.ProtectoBlockEntity;
@@ -2891,6 +2892,8 @@ public class SCContent {
 				return new ReinforcedObserverBlockEntity(pos, state);
 			else if (state.is(MINE))
 				return new MineBlockEntity(pos, state);
+			else if (state.is(PANIC_BUTTON))
+				return new PanicButtonBlockEntity(pos, state);
 			else
 				return new OwnableBlockEntity(pos, state);
 		}, beOwnableBlocks.toArray(new Block[beOwnableBlocks.size()])).build(null);
