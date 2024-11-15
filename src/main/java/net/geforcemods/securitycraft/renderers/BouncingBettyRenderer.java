ec7290b5cb6666bebd931c5b688586ee01f88d61
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-11-15 11:45:05 +0100

    Fix bouncing betty being rendered incorrectly

--- src/main/java/net/geforcemods/securitycraft/renderers/BouncingBettyRenderer.java
+++ src/main/java/net/geforcemods/securitycraft/renderers/BouncingBettyRenderer.java
@@ -5,39 +5,45 @@ import com.mojang.math.Axis;
 
 import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.entity.BouncingBetty;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.entity.TntMinecartRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.inventory.InventoryMenu;
 
 public class BouncingBettyRenderer extends EntityRenderer<BouncingBetty> {
+	private final BlockRenderDispatcher blockRenderer;
+
 	public BouncingBettyRenderer(EntityRendererProvider.Context ctx) {
 		super(ctx);
 		shadowRadius = 0.5F;
+		blockRenderer = ctx.getBlockRenderDispatcher();
 	}
 
 	@Override
 	public void render(BouncingBetty entity, float entityYaw, float partialTicks, PoseStack pose, MultiBufferSource buffer, int packedLight) {
 		pose.pushPose();
-		pose.translate(0.0D, 0.5D, 0.0D);
+		pose.translate(0.0F, 0.5F, 0.0F);
 
-		if (entity.getFuse() - partialTicks + 1.0F < 10.0F) {
-			float alpha = 1.0F - (entity.getFuse() - partialTicks + 1.0F) / 10.0F;
-			alpha = Mth.clamp(alpha, 0.0F, 1.0F);
-			alpha *= alpha;
-			alpha *= alpha;
-			float scale = 1.0F + alpha * 0.3F;
+		int fuse = entity.getFuse();
+
+		if (fuse - partialTicks + 1.0F < 10.0F) {
+			float scale = 1.0F - (fuse - partialTicks + 1.0F) / 10.0F;
+
+			scale = Mth.clamp(scale, 0.0F, 1.0F);
+			scale *= scale;
+			scale *= scale;
+			scale = 1.0F + scale * 0.3F;
 			pose.scale(scale, scale, scale);
 		}
 
 		pose.mulPose(Axis.YP.rotationDegrees(-90.0F));
-		pose.translate(-0.5D, -0.5D, 0.5D);
+		pose.translate(-0.5F, -0.5F, 0.5F);
 		pose.mulPose(Axis.YP.rotationDegrees(90.0F));
-		Minecraft.getInstance().getBlockRenderer().renderSingleBlock(SCContent.BOUNCING_BETTY.get().defaultBlockState(), pose, buffer, packedLight, OverlayTexture.NO_OVERLAY);
+		TntMinecartRenderer.renderWhiteSolidBlock(blockRenderer, SCContent.BOUNCING_BETTY.get().defaultBlockState(), pose, buffer, packedLight, fuse / 5 % 2 == 0);
 		pose.popPose();
 		super.render(entity, entityYaw, partialTicks, pose, buffer, packedLight);
 	}
