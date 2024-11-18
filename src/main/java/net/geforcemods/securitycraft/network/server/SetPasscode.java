de219e71102c6c1d29f8ce4b3c640a944b932bbf
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-11-18 15:15:12 +0100

    Improve adjacent block passcode setting in SetPasscode

--- src/main/java/net/geforcemods/securitycraft/network/server/SetPasscode.java
+++ src/main/java/net/geforcemods/securitycraft/network/server/SetPasscode.java
@@ -4,16 +4,11 @@ import java.util.function.Supplier;
 
 import net.geforcemods.securitycraft.api.IOwnable;
 import net.geforcemods.securitycraft.api.IPasscodeProtected;
-import net.geforcemods.securitycraft.blockentities.KeypadChestBlockEntity;
-import net.geforcemods.securitycraft.blockentities.KeypadDoorBlockEntity;
 import net.geforcemods.securitycraft.util.PasscodeUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.ChestBlock;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.properties.ChestType;
 import net.minecraftforge.network.NetworkEvent;
 
 public class SetPasscode {
@@ -62,35 +57,8 @@ public class SetPasscode {
 
 		if (passcodeProtected != null && (!(passcodeProtected instanceof IOwnable ownable) || ownable.isOwnedBy(player))) {
 			passcodeProtected.hashAndSetPasscode(passcode, b -> passcodeProtected.openPasscodeGUI(level, pos, player));
-
-			if (pos != null) {
-				if (passcodeProtected instanceof KeypadChestBlockEntity chestBe)
-					checkAndUpdateAdjacentChest(chestBe, level, pos, passcode, passcodeProtected.getSalt());
-				else if (passcodeProtected instanceof KeypadDoorBlockEntity doorBe)
-					checkAndUpdateAdjacentDoor(doorBe, level, passcode, passcodeProtected.getSalt());
-			}
+			passcodeProtected.setPasscodeInAdjacentBlock(passcode);
 		}
-	}
-
-	private static void checkAndUpdateAdjacentChest(KeypadChestBlockEntity be, Level level, BlockPos pos, String codeToSet, byte[] salt) {
-		if (be.getBlockState().getValue(ChestBlock.TYPE) != ChestType.SINGLE) {
-			BlockPos offsetPos = pos.relative(ChestBlock.getConnectedDirection(be.getBlockState()));
-			BlockEntity otherBe = level.getBlockEntity(offsetPos);
-
-			if (otherBe instanceof KeypadChestBlockEntity chestBe && be.getOwner().owns(chestBe)) {
-				chestBe.hashAndSetPasscode(codeToSet, salt);
-				level.sendBlockUpdated(offsetPos, otherBe.getBlockState(), otherBe.getBlockState(), 2);
-			}
-		}
-	}
-
-	private static void checkAndUpdateAdjacentDoor(KeypadDoorBlockEntity be, Level level, String codeToSet, byte[] salt) {
-		be.runForOtherHalf(otherBe -> {
-			if (be.getOwner().owns(otherBe)) {
-				otherBe.hashAndSetPasscode(codeToSet, salt);
-				level.sendBlockUpdated(otherBe.getBlockPos(), otherBe.getBlockState(), otherBe.getBlockState(), 2);
-			}
-		});
 	}
 
 	private IPasscodeProtected getPasscodeProtected(Level level) {
