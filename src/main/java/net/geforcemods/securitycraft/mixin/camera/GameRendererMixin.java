52fcbad03c11cd7ec373d1c06c3d8be9700db51b
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-01-02 11:46:54 +0100

    Make public mixin methods private and make more use of MixinExtras

--- src/main/java/net/geforcemods/securitycraft/mixin/camera/GameRendererMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/camera/GameRendererMixin.java
@@ -5,11 +5,10 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -35,12 +34,12 @@ public class GameRendererMixin {
 	/**
 	 * Makes sure camera zooming works, because the fov is only updated when the camera entity is the player itself
 	 */
-	@ModifyConstant(method = "tickFov", constant = @Constant(floatValue = 1.0F))
-	private float securitycraft$modifyInitialFValue(float f) {
+	@ModifyExpressionValue(method = "tickFov", at = @At(value = "CONSTANT", args = "floatValue=1.0F"))
+	private float securitycraft$modifyInitialFValue(float original) {
 		if (minecraft.cameraEntity instanceof SecurityCamera cam)
 			return cam.getZoomAmount();
 		else
-			return f;
+			return original;
 	}
 
 	/**
