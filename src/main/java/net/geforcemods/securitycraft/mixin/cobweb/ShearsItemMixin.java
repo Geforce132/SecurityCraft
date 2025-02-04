e321948d7d76a3d2b9cb8bcaf3b1fcee4c64f29f
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-02-05 00:02:01 +0100

    Remove accidentally added unused translation lines
    Also fix a mixin method name

--- src/main/java/net/geforcemods/securitycraft/mixin/cobweb/ShearsItemMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/cobweb/ShearsItemMixin.java
@@ -20,7 +20,7 @@ import net.minecraft.world.item.component.Tool;
 public class ShearsItemMixin {
 	@SuppressWarnings("rawtypes")
 	@WrapOperation(method = "createToolProperties", at = @At(value = "INVOKE", target = "Ljava/util/List;of(Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;)Ljava/util/List;"))
-	private static List securitycraft$addReinforcedCobwebToSwordProperties(Object minesAndDropsCobweb, Object overrideLeavesSpeed, Object overrideWoolSpeed, Object overrideVineAndGlowLichenSpeed, Operation<List> original) {
+	private static List securitycraft$addReinforcedCobwebToShearsProperties(Object minesAndDropsCobweb, Object overrideLeavesSpeed, Object overrideWoolSpeed, Object overrideVineAndGlowLichenSpeed, Operation<List> original) {
 		List list = new ArrayList();
 
 		list.add(Tool.Rule.minesAndDrops(SCTags.Blocks.REINFORCED_COBWEB, 15.0F));
