ce77c5148c1d718ba7060e84e67c02b504521c88
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-02-06 16:25:54 +0100

    Fix reinforced blocks' note block instruments not working

---
+++ src/main/java/net/geforcemods/securitycraft/mixin/noteblock/NoteBlockInstrumentMixin.java
@@ -0,0 +1,22 @@
+package net.geforcemods.securitycraft.mixin.noteblock;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.geforcemods.securitycraft.api.IReinforcedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+
+/**
+ * Fixes instruments of reinforced blocks to match their vanilla counterparts
+ */
+@Mixin(NoteBlockInstrument.class)
+public class NoteBlockInstrumentMixin {
+	@Inject(method = "byStateBelow", at = @At("HEAD"), cancellable = true)
+	private static void securitycraft$delegateReinforcedBlockToVanillaCounterpart(BlockState state, CallbackInfoReturnable<NoteBlockInstrument> cir) {
+		if (state.getBlock() instanceof IReinforcedBlock reinforcedBlock)
+			cir.setReturnValue(NoteBlockInstrument.byStateBelow(reinforcedBlock.getVanillaBlock().defaultBlockState()));
+	}
+}
