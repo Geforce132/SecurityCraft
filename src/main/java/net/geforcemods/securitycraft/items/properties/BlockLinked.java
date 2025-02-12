7ac2feac989749d4c967d8b518f11b540a9e3c15
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-02-12 11:50:06 +0100

    Fix mine remote access tool being able to identify block mines

--- src/main/java/net/geforcemods/securitycraft/items/properties/BlockLinked.java
+++ src/main/java/net/geforcemods/securitycraft/items/properties/BlockLinked.java
@@ -1,5 +1,6 @@
 package net.geforcemods.securitycraft.items.properties;
 
+import net.geforcemods.securitycraft.api.IBlockMine;
 import net.geforcemods.securitycraft.api.IDisguisable;
 import net.geforcemods.securitycraft.api.IOwnable;
 import net.geforcemods.securitycraft.components.GlobalPositionComponent;
@@ -28,7 +29,7 @@ public record BlockLinked(DataComponentType<?> positionComponent, HitCheck hitCh
 
 					//if the block is not ownable/not owned by the player looking at it, don't show the indicator if it's disguised
 					if (!(be instanceof IOwnable ownable) || !ownable.isOwnedBy(player)) {
-						if (IDisguisable.getDisguisedBlockState(be).isPresent())
+						if (be.getBlockState().getBlock() instanceof IBlockMine || IDisguisable.getDisguisedBlockState(be).isPresent())
 							return returnBasedOnComponent(positions);
 					}
 
