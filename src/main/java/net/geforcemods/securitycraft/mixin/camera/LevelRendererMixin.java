3a42ba273cf047223bd2da188cc07c0528fa43cd
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-02-18 23:09:52 +0100

    Removed redundant variable and cleared up a mixin javadoc

--- src/main/java/net/geforcemods/securitycraft/mixin/camera/LevelRendererMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/camera/LevelRendererMixin.java
@@ -122,10 +122,9 @@ public abstract class LevelRendererMixin {
 	}
 
 	/**
-	 * Sets the correct fog distance for rendering a frame feed, depending on clientside configuration settings.
-	 * Note that the frame block entity render distance option is not respected for this, because only one frame feed is
-	 * captured per camera regardless of amount of linked frames, and thus any per-block setting cannot be included in the
-	 * calculation.
+	 * Sets the correct fog distance for rendering a frame feed, depending on clientside view distance configuration settings.
+	 * Note that the frame block entity chunk loading distance option is not respected for this, since it is only supposed to
+	 * affect the server by setting a limit on forceloaded chunks and unfit to be handled on the client side.
 	 */
 	@ModifyVariable(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/FogRenderer;computeFogColor(Lnet/minecraft/client/Camera;FLnet/minecraft/client/multiplayer/ClientLevel;IF)Lorg/joml/Vector4f;"), ordinal = 1)
 	private float securitycraft$modifyFogRenderDistance(float original) {
