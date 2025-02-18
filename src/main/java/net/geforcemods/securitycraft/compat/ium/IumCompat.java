5dc3c749255c478dac12e7d371129c8aa156156f
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-02-18 21:03:30 +0100

    Removed apparently redundant Sodium compat code for frames
    Please reinstate if it turns out that it is, in fact, needed

--- src/main/java/net/geforcemods/securitycraft/compat/ium/IumCompat.java
+++ src/main/java/net/geforcemods/securitycraft/compat/ium/IumCompat.java
@@ -1,6 +1,5 @@
 package net.geforcemods.securitycraft.compat.ium;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.function.Supplier;
@@ -17,41 +16,23 @@ public enum IumCompat {
 	//@formatter:off
 	EMBEDDIUM(
 			() -> getClass("org.embeddedt.embeddium.impl.render.chunk.map.ChunkTrackerHolder"),
-			() -> getClass("org.embeddedt.embeddium.impl.render.chunk.map.ChunkTracker"),
-			() -> getClass("org.embeddedt.embeddium.impl.render.chunk.RenderSectionManager"),
-			() -> getClass("org.embeddedt.embeddium.impl.render.chunk.lists.SortedRenderLists"),
-			() -> getClass("org.embeddedt.embeddium.impl.render.EmbeddiumWorldRenderer")),
+			() -> getClass("org.embeddedt.embeddium.impl.render.chunk.map.ChunkTracker")),
 	SODIUM(
 			() -> getClass("net.caffeinemc.mods.sodium.client.render.chunk.map.ChunkTrackerHolder"),
-			() -> getClass("net.caffeinemc.mods.sodium.client.render.chunk.map.ChunkTracker"),
-			() -> getClass("net.caffeinemc.mods.sodium.client.render.chunk.RenderSectionManager"),
-			() -> getClass("net.caffeinemc.mods.sodium.client.render.chunk.lists.SortedRenderLists"),
-			() -> getClass("net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer")),
+			() -> getClass("net.caffeinemc.mods.sodium.client.render.chunk.map.ChunkTracker")),
 	//@formatter:on
-	NONE(null, null, null, null, null);
+	NONE(null, null);
 
 	private static final int FLAG_HAS_BLOCK_DATA = 1; //from the ChunkStatus class
 	private final Supplier<Class<?>> chunkTrackerHolderClass;
 	private final Supplier<Class<?>> chunkTrackerClass;
-	private final Supplier<Class<?>> renderSectionManagerClass;
-	private final Supplier<Class<?>> sortedRenderListsClass;
-	private final Supplier<Class<?>> levelRendererClass;
-	private Field renderSectionManager$renderLists;
-	private Field levelRenderer$renderSectionManager;
 	private Method chunkTrackerHolder$get;
 	private Method chunkTracker$onChunkStatusAdded;
 	private Method chunkTracker$onChunkStatusRemoved;
-	private Method levelRenderer$instance;
-	private Method levelRenderer$scheduleTerrainUpdate;
-	private Object sortedRenderLists$EMPTY;
-	private Object previousRenderLists;
 
-	private IumCompat(Supplier<Class<?>> chunkTrackerHolder, Supplier<Class<?>> chunkTracker, Supplier<Class<?>> renderSectionManager, Supplier<Class<?>> sortedRenderLists, Supplier<Class<?>> levelRenderer) {
+	private IumCompat(Supplier<Class<?>> chunkTrackerHolder, Supplier<Class<?>> chunkTracker) {
 		this.chunkTrackerHolderClass = chunkTrackerHolder;
 		this.chunkTrackerClass = chunkTracker;
-		this.renderSectionManagerClass = renderSectionManager;
-		this.sortedRenderListsClass = sortedRenderLists;
-		this.levelRendererClass = levelRenderer;
 	}
 
 	public void onChunkStatusAdded(ClientLevel level, int x, int z) {
@@ -78,81 +59,17 @@ public enum IumCompat {
 		}
 	}
 
-	public Object getOldRenderLists(Object instance) {
-		if (this == NONE)
-			return null;
-
-		try {
-			return renderSectionManager$renderLists.get(instance);
-		}
-		catch (IllegalArgumentException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-
-		return sortedRenderLists$EMPTY;
-	}
-
-	public void setRenderLists(Object instance, Object newRenderLists) {
-		if (this == NONE)
-			return;
-
-		try {
-			renderSectionManager$renderLists.set(instance, newRenderLists);
-		}
-		catch (IllegalArgumentException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void switchToEmptyRenderLists() {
-		if (this == NONE)
-			return;
-
-		switchRenderLists(sortedRenderLists$EMPTY);
-	}
-
-	public void switchToPreviousRenderLists() {
-		if (this == NONE)
-			return;
-
-		switchRenderLists(previousRenderLists);
-	}
-
-	private void switchRenderLists(Object newRenderLists) {
-		try {
-			Object levelRenderer = levelRenderer$instance.invoke(null);
-			Object renderSectionManager = levelRenderer$renderSectionManager.get(levelRenderer);
-
-			levelRenderer$scheduleTerrainUpdate.invoke(levelRenderer);
-			previousRenderLists = getOldRenderLists(renderSectionManager);
-			setRenderLists(renderSectionManager, newRenderLists);
-			levelRenderer$scheduleTerrainUpdate.invoke(levelRenderer);
-		}
-		catch (IllegalAccessException | InvocationTargetException e) {
-			e.printStackTrace();
-		}
-	}
-
 	public void resolve() {
 		if (this == NONE)
 			return;
 
 		try {
 			Class<?> chunkTracker = chunkTrackerClass.get();
-			Class<?> levelRenderer = levelRendererClass.get();
-
-			renderSectionManager$renderLists = renderSectionManagerClass.get().getDeclaredField("renderLists");
-			renderSectionManager$renderLists.setAccessible(true);
-			levelRenderer$renderSectionManager = levelRenderer.getDeclaredField("renderSectionManager");
-			levelRenderer$renderSectionManager.setAccessible(true);
 			chunkTrackerHolder$get = chunkTrackerHolderClass.get().getDeclaredMethod("get", ClientLevel.class);
 			chunkTracker$onChunkStatusAdded = chunkTracker.getDeclaredMethod("onChunkStatusAdded", int.class, int.class, int.class);
 			chunkTracker$onChunkStatusRemoved = chunkTracker.getDeclaredMethod("onChunkStatusRemoved", int.class, int.class, int.class);
-			levelRenderer$instance = levelRenderer.getDeclaredMethod("instance");
-			levelRenderer$scheduleTerrainUpdate = levelRenderer.getDeclaredMethod("scheduleTerrainUpdate");
-			sortedRenderLists$EMPTY = sortedRenderListsClass.get().getMethod("empty").invoke(null);
 		}
-		catch (NoSuchMethodException | SecurityException | NoSuchFieldException | IllegalAccessException | InvocationTargetException e) {
+		catch (NoSuchMethodException | SecurityException e) {
 			e.printStackTrace();
 		}
 	}
