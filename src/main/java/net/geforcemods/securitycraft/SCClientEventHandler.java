0e0aef9293c1774ef01b32ec05cafb620b0e16ee
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-02-21 01:57:34 +0100

    Fix frames not working with Sodium/Embeddium and higher render distance

--- src/main/java/net/geforcemods/securitycraft/SCClientEventHandler.java
+++ src/main/java/net/geforcemods/securitycraft/SCClientEventHandler.java
@@ -212,7 +212,6 @@ public class SCClientEventHandler {
 		int oldWidth = window.getWidth();
 		int oldHeight = window.getHeight();
 		List<LevelRenderer.RenderChunkInfo> oldVisibleSections = mc.levelRenderer.renderChunksInFrustum.clone();
-		int oldServerRenderDistance = mc.options.serverRenderDistance;
 		int newFrameFeedViewDistance = CameraController.getFrameFeedViewDistance(null);
 		double oldX = player.getX();
 		double oldXO = player.xOld;
@@ -234,7 +233,6 @@ public class SCClientEventHandler {
 		mc.gameRenderer.setRenderHand(false);
 		mc.gameRenderer.setPanoramicMode(true);
 		mc.levelRenderer.graphicsChanged();
-		mc.options.setServerRenderDistance(newFrameFeedViewDistance);
 		window.setWidth(100);
 		window.setHeight(100); //Different width/height values seem to have no effect, although the ratio needs to be 1:1
 		mc.options.setCameraType(CameraType.FIRST_PERSON);
@@ -328,7 +326,6 @@ public class SCClientEventHandler {
 		mc.levelRenderer.renderChunksInFrustum.addAll(oldVisibleSections);
 		window.setWidth(oldWidth);
 		window.setHeight(oldHeight);
-		mc.options.setServerRenderDistance(oldServerRenderDistance);
 		mc.gameRenderer.setRenderHand(true);
 		mc.gameRenderer.setPanoramicMode(false);
 		mc.levelRenderer.graphicsChanged();
