14b3a67f2bf918530287a9fb2488cb432e3a6448
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-03-01 17:48:07 +0100

    Improved camera rotation sync between two clients

--- src/main/java/net/geforcemods/securitycraft/blockentities/SecurityCameraBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/SecurityCameraBlockEntity.java
@@ -167,6 +167,7 @@ public class SecurityCameraBlockEntity extends DisguisableBlockEntity implements
 	public void saveAdditional(CompoundTag tag) {
 		super.saveAdditional(tag);
 		tag.putDouble("camera_rotation", cameraRotation);
+		tag.putBoolean("add_to_rotation", addToRotation);
 		tag.putBoolean("shutDown", shutDown);
 		tag.put("lens", lens.createTag());
 		tag.putFloat("initial_x_rotation", initialXRotation);
@@ -183,6 +184,7 @@ public class SecurityCameraBlockEntity extends DisguisableBlockEntity implements
 
 			cameraRotation = newCamRotation;
 			oCameraRotation = newCamRotation;
+			addToRotation = tag.getBoolean("add_to_rotation");
 		}
 
 		shutDown = tag.getBoolean("shutDown");
@@ -333,6 +335,7 @@ public class SecurityCameraBlockEntity extends DisguisableBlockEntity implements
 		}
 
 		playerViewedFrames.add(framePos.asLong());
+		level.sendBlockUpdated(worldPosition, getBlockState(), getBlockState(), 3); //Syncs the camera's rotation to all clients again, in case a client is desynched
 	}
 
 	public void unlinkFrameForPlayer(UUID playerUUID, BlockPos framePos) {
