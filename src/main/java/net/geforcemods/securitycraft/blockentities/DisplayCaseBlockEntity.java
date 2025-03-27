702a1fa58b01fb6482366e51d86ad2c672fff45d
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-03-27 20:26:17 +0100

    Unified passcode and salt key NBT saving

--- src/main/java/net/geforcemods/securitycraft/blockentities/DisplayCaseBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/DisplayCaseBlockEntity.java
@@ -16,7 +16,6 @@ import net.geforcemods.securitycraft.blocks.DisplayCaseBlock;
 import net.geforcemods.securitycraft.misc.ModuleType;
 import net.geforcemods.securitycraft.misc.SCSounds;
 import net.geforcemods.securitycraft.util.ITickingBlockEntity;
-import net.geforcemods.securitycraft.util.PasscodeUtils;
 import net.geforcemods.securitycraft.util.Utils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
@@ -110,12 +109,7 @@ public class DisplayCaseBlockEntity extends CustomizableBlockEntity implements I
 		tag.putBoolean("ShouldBeOpen", shouldBeOpen);
 		cooldownLeft = getCooldownEnd() - System.currentTimeMillis();
 		tag.putLong("cooldownLeft", cooldownLeft <= 0 ? -1 : cooldownLeft);
-
-		if (saltKey != null)
-			tag.putUUID("saltKey", saltKey);
-
-		if (passcode != null)
-			tag.putString("passcode", PasscodeUtils.bytesToString(passcode));
+		savePasscodeAndSalt(tag);
 	}
 
 	@Override
