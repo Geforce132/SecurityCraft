2eed699ad34557eb0dc840cd2d3dc8853dd39e43
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-03-30 12:09:39 +0200

    Fix secret sign text loading incorrectly

---
+++ src/main/java/net/geforcemods/securitycraft/mixin/datafix/V3439Mixin.java
@@ -0,0 +1,38 @@
+package net.geforcemods.securitycraft.mixin.datafix;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.datafixers.schemas.Schema;
+import com.mojang.datafixers.types.templates.TypeTemplate;
+
+import net.minecraft.util.datafix.schemas.NamespacedSchema;
+import net.minecraft.util.datafix.schemas.V3439;
+
+/**
+ * Makes sure Minecraft correctly fixes secret (hanging) signs' text data
+ */
+@Mixin(V3439.class)
+public class V3439Mixin extends NamespacedSchema {
+	private V3439Mixin(int versionKey, Schema parent) {
+		super(versionKey, parent);
+	}
+
+	@Shadow
+	private static TypeTemplate sign(Schema schema) {
+		throw new UnsupportedOperationException("Shadowing sign method failed!");
+	}
+
+	@Inject(method = "registerBlockEntities", at = @At("TAIL"))
+	private void securitycraft$registerSigns(Schema schema, CallbackInfoReturnable<Map<String, Supplier<TypeTemplate>>> ci, @Local Map<String, Supplier<TypeTemplate>> map) {
+		register(map, "securitycraft:secret_sign", () -> sign(schema));
+		register(map, "securitycraft:secret_hanging_sign", () -> sign(schema));
+	}
+}
