724669c049d3240cf3e16c8b2ca93b94c0fcea2d
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-04-05 23:52:14 +0200

    Fixed message being sent when removing frame with linked removed camera

--- src/main/java/net/geforcemods/securitycraft/blockentities/FrameBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/FrameBlockEntity.java
@@ -245,7 +245,7 @@ public class FrameBlockEntity extends CustomizableBlockEntity implements ITickab
 				}
 			}
 
-			if (player instanceof EntityPlayerMP && newCameraPos != null) {
+			if (player instanceof EntityPlayerMP && !disableNewCamera && newCameraPos != null) {
 				TileEntity te = world.getTileEntity(newCameraPos.pos());
 
 				if (world.provider.getDimension() != newCameraPos.dimension() || !(te instanceof SecurityCameraBlockEntity))
@@ -253,7 +253,7 @@ public class FrameBlockEntity extends CustomizableBlockEntity implements ITickab
 				else {
 					SecurityCameraBlockEntity newCamera = (SecurityCameraBlockEntity) te;
 
-					if (!disableNewCamera && (!newCameraPos.equals(previousCameraPos) || !newCamera.isFrameLinked(player, pos))) {
+					if ((!newCameraPos.equals(previousCameraPos) || !newCamera.isFrameLinked(player, pos))) {
 						if (redstoneSignalDisabled())
 							PlayerUtils.sendMessageToPlayer(player, Utils.localize(SCContent.frame), Utils.localize("messages.securitycraft:frame.noRedstoneSignal", newCameraPos.pos()), TextFormatting.RED);
 						else
@@ -333,7 +333,7 @@ public class FrameBlockEntity extends CustomizableBlockEntity implements ITickab
 
 		if (module == ModuleType.ALLOWLIST)
 			switchCameras(null, null, 0, false); //Disable the frame feed for all players if the allowlist module is removed, because frames on the server side don't know which players are currently viewing them
-		if (module == ModuleType.REDSTONE) {
+		else if (module == ModuleType.REDSTONE) {
 			activatedByRedstone = false;
 			world.setBlockState(pos, getBlockState().withProperty(FrameBlock.POWERED, false));
 			markDirty();
