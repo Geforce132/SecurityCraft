303b2a663313bf12812a36f0f8455ec4861a967a
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-04-16 17:09:57 +0200

    Fade out keybinding text in camera overlay after 10 seconds

--- src/main/java/net/geforcemods/securitycraft/SCClientEventHandler.java
+++ src/main/java/net/geforcemods/securitycraft/SCClientEventHandler.java
@@ -46,6 +46,8 @@ import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.client.event.RenderLevelStageEvent.Stage;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
+import net.minecraftforge.event.TickEvent.ClientTickEvent;
+import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.level.ChunkEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -70,8 +72,15 @@ public class SCClientEventHandler {
 			new CameraKeyInfoEntry(() -> true, $ -> SMART_MODULE_NOTE, be -> be.isModuleEnabled(ModuleType.SMART))
 	};
 	//@formatter:on
+	private static float cameraInfoMessageTime;
 
 	private SCClientEventHandler() {}
+
+	@SubscribeEvent
+	public static void onClientTickPost(ClientTickEvent event) {
+		if (event.phase == Phase.END && cameraInfoMessageTime >= 0)
+			cameraInfoMessageTime--;
+	}
 
 	@SubscribeEvent
 	public static void onRenderLevelStage(RenderLevelStageEvent event) {
@@ -167,14 +176,18 @@ public class SCClientEventHandler {
 
 		font.drawShadow(pose, time, scaledWidth - font.width(time) - 4, timeY, 0xFFFFFF);
 
-		int heightOffset = 10;
+		if (cameraInfoMessageTime >= 0) {
+			float fadeOutPartialTick = Math.max(cameraInfoMessageTime + 1.0F - partialTicks, 1.0F);
+			int alpha = (int) Math.ceil(255.0F * Math.min(20.0F, fadeOutPartialTick) / 20.0F);
+			int heightOffset = 10;
 
-		for (int i = CAMERA_KEY_INFO_LIST.length - 1; i >= 0; i--) {
-			CameraKeyInfoEntry entry = CAMERA_KEY_INFO_LIST[i];
+			for (int i = CAMERA_KEY_INFO_LIST.length - 1; i >= 0; i--) {
+				CameraKeyInfoEntry entry = CAMERA_KEY_INFO_LIST[i];
 
-			if (entry.enabled().get()) {
-				entry.drawString(options, pose, font, scaledWidth, scaledHeight, heightOffset, be);
-				heightOffset += 10;
+				if (entry.enabled().get()) {
+					entry.drawString(options, pose, font, scaledWidth, scaledHeight, heightOffset, be, alpha);
+					heightOffset += 10;
+				}
 			}
 		}
 
@@ -198,6 +211,10 @@ public class SCClientEventHandler {
 		}
 		else
 			CameraRedstoneModuleState.ACTIVATED.render(pose, 12, 2);
+	}
+
+	public static void resetCameraInfoMessageTime() {
+		cameraInfoMessageTime = 200;
 	}
 
 	private enum BCDBuffer implements MultiBufferSource {
@@ -237,11 +254,11 @@ public class SCClientEventHandler {
 	}
 
 	public record CameraKeyInfoEntry(Supplier<Boolean> enabled, Function<Options, Component> text, Predicate<SecurityCameraBlockEntity> whiteText) {
-		public void drawString(Options options, PoseStack pose, Font font, int scaledWidth, int scaledHeight, int heightOffset, SecurityCameraBlockEntity be) {
+		public void drawString(Options options, PoseStack pose, Font font, int scaledWidth, int scaledHeight, int heightOffset, SecurityCameraBlockEntity be, int alpha) {
 			Component text = text().apply(options);
-			boolean whiteText = whiteText().test(be);
+			int textColor = whiteText().test(be) ? 0xFFFFFF : 0xFF3377;
 
-			font.drawShadow(pose, text, scaledWidth - font.width(text) - 8, scaledHeight - heightOffset, whiteText ? 0xFFFFFF : 0xFF3377);
+			font.drawShadow(pose, text, scaledWidth - font.width(text) - 8, scaledHeight - heightOffset, textColor + (alpha << 24));
 		}
 	}
 }
