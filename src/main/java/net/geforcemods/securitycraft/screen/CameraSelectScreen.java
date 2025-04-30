2c0b34c6ec1a7908b476886bb2253ada1df95e15
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-04-30 22:49:43 +0200

    Fix redstone module state not resetting when changing camera screen page

--- src/main/java/net/geforcemods/securitycraft/screen/CameraSelectScreen.java
+++ src/main/java/net/geforcemods/securitycraft/screen/CameraSelectScreen.java
@@ -77,6 +77,7 @@ public class CameraSelectScreen extends Screen {
 
 			cameraButtons[i] = cameraButton;
 			cameraButton.setMessage(cameraButton.getMessage().plainCopy().append(Component.literal("" + camID)));
+			redstoneModuleStates[i] = null;
 
 			if (view != null) {
 				BlockPos pos = view.pos();
