0ddeb4fee7cb618f9d1a545772e709ece4676a76
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-04-30 22:27:30 +0200

    Refactor CameraSelectScreen to be more readable

---
+++ src/main/java/net/geforcemods/securitycraft/screen/CameraMonitorScreen.java
@@ -0,0 +1,28 @@
+package net.geforcemods.securitycraft.screen;
+
+import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.items.CameraMonitorItem;
+import net.geforcemods.securitycraft.network.server.MountCamera;
+import net.minecraft.core.GlobalPos;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.network.PacketDistributor;
+
+public class CameraMonitorScreen extends CameraSelectScreen {
+	private final ItemStack stack;
+
+	public CameraMonitorScreen(ItemStack stack) {
+		super(stack.getOrDefault(SCContent.BOUND_CAMERAS, CameraMonitorItem.DEFAULT_NAMED_POSITIONS).positions(), false);
+		this.stack = stack;
+	}
+
+	@Override
+	protected void viewCamera(GlobalPos cameraPos) {
+		PacketDistributor.sendToServer(new MountCamera(cameraPos.pos()));
+		super.viewCamera(cameraPos);
+	}
+
+	@Override
+	protected void unbindCamera(GlobalPos cameraPos) {
+		CameraMonitorItem.removeCameraOnClient(cameraPos, stack);
+	}
+}
