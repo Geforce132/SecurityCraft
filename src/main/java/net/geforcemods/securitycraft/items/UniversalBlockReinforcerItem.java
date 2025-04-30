77fc0da1ed70c2046a4b98dd929d86e6e03e083f
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-04-30 16:08:29 +0200

    Add dispense behavior to the universal block reinforcer

--- src/main/java/net/geforcemods/securitycraft/items/UniversalBlockReinforcerItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/UniversalBlockReinforcerItem.java
@@ -7,9 +7,13 @@ import net.geforcemods.securitycraft.SecurityCraft;
 import net.geforcemods.securitycraft.api.IModuleInventory;
 import net.geforcemods.securitycraft.api.IOwnable;
 import net.geforcemods.securitycraft.api.IReinforcedBlock;
+import net.geforcemods.securitycraft.api.Owner;
 import net.geforcemods.securitycraft.screen.ScreenHandler.Screens;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockDispenser;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
+import net.minecraft.dispenser.IBlockSource;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
@@ -28,6 +32,30 @@ import net.minecraft.world.World;
 public class UniversalBlockReinforcerItem extends Item {
 	public UniversalBlockReinforcerItem(int damage) {
 		setMaxDamage(damage);
+		BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(this, new BehaviorDefaultDispenseItem() {
+			private final BehaviorDefaultDispenseItem defaultDispenseItemBehavior = new BehaviorDefaultDispenseItem();
+
+			@Override
+			public ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
+				IBlockState state = source.getBlockState();
+
+				if (state.getBlock() == SCContent.reinforcedDispenser) {
+					World level = source.getWorld();
+					BlockPos modifyPos = source.getBlockPos().offset(state.getValue(BlockDispenser.FACING));
+					IBlockState modifyState = level.getBlockState(modifyPos);
+
+					if (convertBlock(modifyState, level, stack, modifyPos, ((IOwnable) source.getBlockTileEntity()).getOwner())) {
+						if (!level.isRemote && stack.attemptDamageItem(1, level.rand, null))
+							stack.setCount(0);
+					}
+
+					if (!level.isAirBlock(modifyPos))
+						return stack;
+				}
+
+				return defaultDispenseItemBehavior.dispense(source, stack);
+			}
+		});
 	}
 
 	@Override
@@ -45,61 +73,70 @@ public class UniversalBlockReinforcerItem extends Item {
 	public static boolean convertBlock(ItemStack stack, BlockPos pos, EntityPlayer player) {
 		World world = player.getEntityWorld();
 
-		if (!player.capabilities.isCreativeMode) {
-			boolean isReinforcing = isReinforcing(stack);
+		if (!player.capabilities.isCreativeMode && world.isBlockModifiable(player, pos)) {
 			IBlockState state = world.getBlockState(pos);
-			Block blockToConvert = state.getBlock();
-			IBlockState convertedState = null;
+			boolean result = convertBlock(state, world, stack, pos, new Owner(player));
 
-			if (isReinforcing) {
-				Block convertedBlock = IReinforcedBlock.VANILLA_TO_SECURITYCRAFT.get(blockToConvert);
+			if (result && !world.isRemote)
+				stack.damageItem(1, player);
 
-				if (convertedBlock instanceof IReinforcedBlock)
-					convertedState = ((IReinforcedBlock) convertedBlock).convertToReinforcedState(state);
+			return result;
+		}
+		else
+			return false;
+	}
+
+	public static boolean convertBlock(IBlockState state, World world, ItemStack stack, BlockPos pos, Owner owner) {
+		boolean isReinforcing = isReinforcing(stack);
+		Block blockToConvert = state.getBlock();
+		IBlockState convertedState = null;
+
+		if (isReinforcing) {
+			Block convertedBlock = IReinforcedBlock.VANILLA_TO_SECURITYCRAFT.get(blockToConvert);
+
+			if (convertedBlock instanceof IReinforcedBlock)
+				convertedState = ((IReinforcedBlock) convertedBlock).convertToReinforcedState(state);
+		}
+		else if (blockToConvert instanceof IReinforcedBlock) {
+			try {
+				convertedState = ((IReinforcedBlock) blockToConvert).convertToVanillaState(state);
 			}
-			else if (blockToConvert instanceof IReinforcedBlock) {
-				try {
-					convertedState = ((IReinforcedBlock) blockToConvert).convertToVanillaState(state);
+			catch (IllegalStateException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (convertedState != null) {
+			TileEntity te = world.getTileEntity(pos);
+			NBTTagCompound tag = null;
+
+			if (!isReinforcing && te instanceof IOwnable && !((IOwnable) te).isOwnedBy(owner))
+				return false;
+
+			if (!world.isRemote) {
+				if (te != null) {
+					tag = te.writeToNBT(new NBTTagCompound());
+
+					if (te instanceof IModuleInventory)
+						((IModuleInventory) te).dropAllModules();
+
+					if (te instanceof IInventory)
+						((IInventory) te).clear();
 				}
-				catch (IllegalStateException e) {
-					e.printStackTrace();
+
+				world.setBlockState(pos, convertedState);
+				te = world.getTileEntity(pos);
+
+				if (te != null) { //in case the converted state gets removed immediately after it is placed down
+					if (tag != null)
+						te.readFromNBT(tag);
+
+					if (isReinforcing)
+						((IOwnable) te).setOwner(owner.getUUID(), owner.getName());
 				}
 			}
 
-			if (convertedState != null) {
-				TileEntity te = world.getTileEntity(pos);
-				NBTTagCompound tag = null;
-
-				if (!isReinforcing && te instanceof IOwnable && !((IOwnable) te).isOwnedBy(player) || !world.isBlockModifiable(player, pos))
-					return false;
-
-				if (!world.isRemote) {
-					if (te != null) {
-						tag = te.writeToNBT(new NBTTagCompound());
-
-						if (te instanceof IModuleInventory)
-							((IModuleInventory) te).dropAllModules();
-
-						if (te instanceof IInventory)
-							((IInventory) te).clear();
-					}
-
-					world.setBlockState(pos, convertedState);
-					te = world.getTileEntity(pos);
-
-					if (te != null) { //in case the converted state gets removed immediately after it is placed down
-						if (tag != null)
-							te.readFromNBT(tag);
-
-						if (isReinforcing)
-							((IOwnable) te).setOwner(player.getGameProfile().getId().toString(), player.getName());
-					}
-
-					stack.damageItem(1, player);
-				}
-
-				return true;
-			}
+			return true;
 		}
 
 		return false;
