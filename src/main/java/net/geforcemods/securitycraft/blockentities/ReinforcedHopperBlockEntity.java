72a8208589a1fa1081ddfa798791722b82064bb3
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-05-03 10:15:03 +0200

    Deduplicate code
    This really should have been done from the get go, not sure why it wasn't...

--- src/main/java/net/geforcemods/securitycraft/blockentities/ReinforcedHopperBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/ReinforcedHopperBlockEntity.java
@@ -1,7 +1,6 @@
 package net.geforcemods.securitycraft.blockentities;
 
 import java.util.EnumMap;
-import java.util.List;
 import java.util.Map;
 
 import org.jetbrains.annotations.NotNull;
@@ -85,11 +84,7 @@ public class ReinforcedHopperBlockEntity extends HopperBlockEntity implements IO
 	@Override
 	public void onLoad() {
 		super.onLoad();
-
-		if (level != null && level.isClientSide && level.getBlockEntity(worldPosition) != this) //On the client side, onLoad is usually only called without this BE being added to the level, which breaks model data update requests.
-			level.addFreshBlockEntities(List.of(this)); //By marking this BE as a fresh block entity in such cases, the client will call onLoad again on the first BE tick, on which it is registered properly.
-		else
-			DisguisableBlockEntity.onHandleUpdateTag(this);
+		DisguisableBlockEntity.onOnLoad(this);
 	}
 
 	@Override
