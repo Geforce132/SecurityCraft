f415e71c53914daf65f543a7142a8c809cdf917c
bl4ckscor3 <donalddani9612@gmx.de>
2025-08-06 20:03:53 +0200

    Clean up *ium mod compatibility code

--- src/main/java/net/geforcemods/securitycraft/SecurityCraftClient.java
+++ src/main/java/net/geforcemods/securitycraft/SecurityCraftClient.java
@@ -1,6 +1,5 @@
 package net.geforcemods.securitycraft;
 
-import net.geforcemods.securitycraft.compat.ium.IumCompat;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
@@ -10,11 +9,8 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(value = SecurityCraft.MODID, dist = Dist.CLIENT)
 public class SecurityCraftClient {
-	public static final IumCompat INSTALLED_IUM_MOD = IumCompat.getInstalledIumMod();
-
 	public SecurityCraftClient(ModContainer container) {
 		container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
 		container.registerConfig(ModConfig.Type.CLIENT, ConfigHandler.CLIENT_SPEC);
-		INSTALLED_IUM_MOD.resolve();
 	}
 }
