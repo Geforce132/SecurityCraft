42b1b86c8f621a73e0b2ed72d9626b791c8c7b47
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-08-08 22:33:23 +0200

    Made play icon hover over noise texture to reduce potential z-fighting

--- src/main/java/net/geforcemods/securitycraft/renderers/FrameBlockEntityRenderer.java
+++ src/main/java/net/geforcemods/securitycraft/renderers/FrameBlockEntityRenderer.java
@@ -1,6 +1,7 @@
 package net.geforcemods.securitycraft.renderers;
 
 import javax.vecmath.Vector3f;
+import javax.vecmath.Vector4f;
 
 import org.lwjgl.opengl.GL11;
 
@@ -50,63 +51,64 @@ public class FrameBlockEntityRenderer extends TileEntitySpecialRenderer<FrameBlo
 		EnumFacing direction = state.getValue(FrameBlock.FACING);
 		Vec3i normal = direction.getDirectionVec();
 		final float margin = 0.0625F;
-		float xStart = 0.0F;
-		float xEnd = 1.0F;
-		float zStart = 0.0F;
-		float zEnd = 1.0F;
+		Vector4f innerVertices; //Both vectors have the following format: xStart, xEnd, zStart, zEnd
+		Vector4f outerVertices;
 
 		GlStateManager.pushMatrix();
 		GlStateManager.translate(x, y, z);
 
 		switch (direction) {
 			case NORTH:
-				xStart = margin;
-				xEnd = 1 - margin;
-				zStart = zEnd = 0.05F;
+				innerVertices = new Vector4f(margin, 1 - margin, 0.05F, 0.05F);
+				outerVertices = new Vector4f(margin, 1 - margin, 0.045F, 0.045F);
 				break;
 			case SOUTH:
-				xStart = 1 - margin;
-				xEnd = margin;
-				zStart = zEnd = 0.95F;
+				innerVertices = new Vector4f(1 - margin, margin, 0.95F, 0.95F);
+				outerVertices = new Vector4f(1 - margin, margin, 0.955F, 0.955F);
 				break;
 			case WEST:
-				zStart = 1 - margin;
-				zEnd = margin;
-				xStart = xEnd = 0.05F;
+				innerVertices = new Vector4f(0.05F, 0.05F, 1 - margin, margin);
+				outerVertices = new Vector4f(0.045F, 0.045F, 1 - margin, margin);
 				break;
 			case EAST:
-				zStart = margin;
-				zEnd = 1 - margin;
-				xStart = xEnd = 0.95F;
+				innerVertices = new Vector4f(0.95F, 0.95F, margin, 1 - margin);
+				outerVertices = new Vector4f(0.955F, 0.955F, margin, 1 - margin);
 				break;
 			default:
+				innerVertices = new Vector4f(0.0F, 1.0F, 0.0F, 1.0F);
+				outerVertices = new Vector4f(0.0F, 1.0F, 0.0F, 1.0F);
+				break;
 		}
 
 		if (cameraPos == null)
-			renderSolidTexture(SELECT_CAMERA, xStart, xEnd, zStart, zEnd, normal, margin);
+			renderSolidTexture(SELECT_CAMERA, innerVertices, normal, margin);
 		else if (be.redstoneSignalDisabled()) {
-			renderNoise(xStart, xEnd, zStart, zEnd, normal, margin);
-			renderCutoutTexture(NO_REDSTONE_SIGNAL, xStart, xEnd, zStart, zEnd, normal, margin);
+			renderNoise(innerVertices, normal, margin);
+			renderCutoutTexture(NO_REDSTONE_SIGNAL, outerVertices, normal, margin);
 		}
 		else if (!be.hasClientInteracted()) {
-			renderNoise(xStart, xEnd, zStart, zEnd, normal, margin);
-			renderCutoutTexture(INACTIVE, xStart, xEnd, zStart, zEnd, normal, margin);
+			renderNoise(innerVertices, normal, margin);
+			renderCutoutTexture(INACTIVE, outerVertices, normal, margin);
 		}
 		else {
 			CameraFeed feed = FrameFeedHandler.getFeed(cameraPos);
 
 			if (feed == null || !feed.isFrameLinked(be) || !level.isBlockLoaded(cameraPos.pos()) || !(level.getTileEntity(cameraPos.pos()) instanceof SecurityCameraBlockEntity))
-				renderSolidTexture(CAMERA_NOT_FOUND, xStart, xEnd, zStart, zEnd, normal, margin);
+				renderSolidTexture(CAMERA_NOT_FOUND, innerVertices, normal, margin);
 			else if (!FrameFeedHandler.isCapturingCamera()) { //Only rendering the frame when no camera is being captured prevents screen-in-screen rendering
 				SecurityCameraBlockEntity cameraBlockEntity = (SecurityCameraBlockEntity) level.getTileEntity(cameraPos.pos());
 				Framebuffer target = feed.renderTarget();
 				Tessellator tessellator = Tessellator.getInstance(); //ImmPtl ViewAreaRenderer#drawPortalViewTriangle (adapted for quads)
+				float xStart = innerVertices.x;
+				float xEnd = innerVertices.y;
+				float zStart = innerVertices.z;
+				float zEnd = innerVertices.w;
 				BufferBuilder bufferBuilder = tessellator.getBuffer();
 				Vector3f backgroundColor = feed.backgroundColor();
 
 				GlStateManager.disableLighting();
 				GlStateManager.disableAlpha();
-				renderOverlay(xStart, xEnd, zStart, zEnd, (int) (backgroundColor.x * 255.0F), (int) (backgroundColor.y * 255.0F), (int) (backgroundColor.z * 255.0F), 255, normal, margin);
+				renderOverlay(innerVertices, (int) (backgroundColor.x * 255.0F), (int) (backgroundColor.y * 255.0F), (int) (backgroundColor.z * 255.0F), 255, normal, margin);
 				target.bindFramebufferTexture();
 				bufferBuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
 				bufferBuilder.pos(xStart, margin, zStart).tex(1, 0).color(0xFF, 0xFF, 0xFF, 0xFF).endVertex();
@@ -119,7 +121,7 @@ public class FrameBlockEntityRenderer extends TileEntitySpecialRenderer<FrameBlo
 				ItemStack lens = cameraBlockEntity.getLensContainer().getStackInSlot(0);
 
 				if (lens.getItem() instanceof ColorableItem && ((ColorableItem) lens.getItem()).hasColor(lens))
-					renderOverlay(xStart, xEnd, zStart, zEnd, ((ColorableItem) lens.getItem()).getColor(lens) + (cameraBlockEntity.getOpacity() << 24), normal, margin);
+					renderOverlay(innerVertices, ((ColorableItem) lens.getItem()).getColor(lens) + (cameraBlockEntity.getOpacity() << 24), normal, margin);
 
 				GlStateManager.enableAlpha();
 				GlStateManager.enableLighting();
@@ -129,35 +131,39 @@ public class FrameBlockEntityRenderer extends TileEntitySpecialRenderer<FrameBlo
 		GlStateManager.popMatrix();
 	}
 
-	private void renderNoise(float xStart, float xEnd, float zStart, float zEnd, Vec3i normal, float margin) {
+	private void renderNoise(Vector4f vertices, Vec3i normal, float margin) {
 		TextureAtlasSprite sprite = Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(NOISE_BACKGROUND.toString());
 
-		renderTexture(TextureMap.LOCATION_BLOCKS_TEXTURE, xStart, xEnd, zStart, zEnd, sprite.getMinU(), sprite.getMinV(), sprite.getMaxU(), sprite.getMaxV(), 255, 255, 255, 255, normal, margin);
+		renderTexture(TextureMap.LOCATION_BLOCKS_TEXTURE, vertices, sprite.getMinU(), sprite.getMinV(), sprite.getMaxU(), sprite.getMaxV(), 255, 255, 255, 255, normal, margin);
 	}
 
-	private void renderSolidTexture(ResourceLocation texture, float xStart, float xEnd, float zStart, float zEnd, Vec3i normal, float margin) {
-		renderTexture(texture, xStart, xEnd, zStart, zEnd, 0, 0, 1, 1, 255, 255, 255, 255, normal, margin);
+	private void renderSolidTexture(ResourceLocation texture, Vector4f vertices, Vec3i normal, float margin) {
+		renderTexture(texture, vertices, 0, 0, 1, 1, 255, 255, 255, 255, normal, margin);
 	}
 
-	private void renderCutoutTexture(ResourceLocation texture, float xStart, float xEnd, float zStart, float zEnd, Vec3i normal, float margin) {
+	private void renderCutoutTexture(ResourceLocation texture, Vector4f vertices, Vec3i normal, float margin) {
 		GlStateManager.enableBlend();
-		renderTexture(texture, xStart, xEnd, zStart, zEnd, 0, 0, 1, 1, 255, 255, 255, 255, normal, margin);
+		renderTexture(texture, vertices, 0, 0, 1, 1, 255, 255, 255, 255, normal, margin);
 		GlStateManager.disableBlend();
 	}
 
-	private void renderOverlay(float xStart, float xEnd, float zStart, float zEnd, int color, Vec3i normal, float margin) {
-		renderOverlay(xStart, xEnd, zStart, zEnd, color >> 16 & 255, color >> 8 & 255, color & 255, color >> 24, normal, margin);
+	private void renderOverlay(Vector4f vertices, int color, Vec3i normal, float margin) {
+		renderOverlay(vertices, color >> 16 & 255, color >> 8 & 255, color & 255, color >> 24, normal, margin);
 	}
 
-	private void renderOverlay(float xStart, float xEnd, float zStart, float zEnd, int r, int g, int b, int a, Vec3i normal, float margin) {
+	private void renderOverlay(Vector4f vertices, int r, int g, int b, int a, Vec3i normal, float margin) {
 		GlStateManager.enableBlend();
-		renderTexture(WHITE, xStart, xEnd, zStart, zEnd, 0, 0, 1, 1, r, g, b, a, normal, margin);
+		renderTexture(WHITE, vertices, 0, 0, 1, 1, r, g, b, a, normal, margin);
 		GlStateManager.disableBlend();
 	}
 
-	private void renderTexture(ResourceLocation texture, float xStart, float xEnd, float zStart, float zEnd, float u0, float v0, float u1, float v1, int r, int g, int b, int a, Vec3i normal, float margin) {
+	private void renderTexture(ResourceLocation texture, Vector4f vertices, float u0, float v0, float u1, float v1, int r, int g, int b, int a, Vec3i normal, float margin) {
 		Tessellator tessellator = Tessellator.getInstance(); //ImmPtl ViewAreaRenderer#drawPortalViewTriangle (adapted for quads)
 		BufferBuilder bufferBuilder = tessellator.getBuffer();
+		float xStart = vertices.x;
+		float xEnd = vertices.y;
+		float zStart = vertices.z;
+		float zEnd = vertices.w;
 		int nx = normal.getX();
 		int ny = normal.getY();
 		int nz = normal.getZ();
