17e81681bbdad78b24d12c00a6947f7874e1a2a7
bl4ckscor3 <donalddani9612@gmx.de>
2025-08-08 12:20:51 +0200

    Pass correct briefcase to menu when opening one

--- src/main/java/net/geforcemods/securitycraft/items/BriefcaseItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/BriefcaseItem.java
@@ -5,14 +5,12 @@ import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
-import net.geforcemods.securitycraft.SCContent;
 import net.geforcemods.securitycraft.SecurityCraft;
 import net.geforcemods.securitycraft.inventory.BriefcaseMenu;
 import net.geforcemods.securitycraft.inventory.ItemContainer;
 import net.geforcemods.securitycraft.misc.SaltData;
 import net.geforcemods.securitycraft.network.client.OpenScreen;
 import net.geforcemods.securitycraft.util.PasscodeUtils;
-import net.geforcemods.securitycraft.util.PlayerUtils;
 import net.geforcemods.securitycraft.util.Utils;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
@@ -103,7 +101,7 @@ public class BriefcaseItem extends Item implements DyeableLeatherItem {
 				NetworkHooks.openGui(player, new MenuProvider() {
 					@Override
 					public AbstractContainerMenu createMenu(int windowId, Inventory inv, Player player) {
-						return new BriefcaseMenu(windowId, inv, ItemContainer.briefcase(PlayerUtils.getItemStackFromAnyHand(player, SCContent.BRIEFCASE.get())));
+						return new BriefcaseMenu(windowId, inv, ItemContainer.briefcase(briefcase));
 					}
 
 					@Override
