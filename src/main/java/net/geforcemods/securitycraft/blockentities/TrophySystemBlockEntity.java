f1f2971e2cee7451ddad4b1879611afc4bafea94
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-08-12 10:31:46 +0200

    Fixed lens slot change not being saved properly

--- src/main/java/net/geforcemods/securitycraft/blockentities/TrophySystemBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/TrophySystemBlockEntity.java
@@ -226,6 +226,7 @@ public class TrophySystemBlockEntity extends DisguisableBlockEntity implements I
 		if (level == null)
 			return;
 
+		setChanged();
 		level.sendBlockUpdated(worldPosition, getBlockState(), getBlockState(), 2);
 	}
 
