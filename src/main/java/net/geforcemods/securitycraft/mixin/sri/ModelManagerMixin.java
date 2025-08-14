148d4f3293d7dfb896b0c67f22509949dc451e5e
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-08-14 21:20:36 +0200

    Format ModelManagerMixin

--- src/main/java/net/geforcemods/securitycraft/mixin/sri/ModelManagerMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/sri/ModelManagerMixin.java
@@ -1,6 +1,13 @@
 package net.geforcemods.securitycraft.mixin.sri;
 
+import java.util.Map;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
 import com.google.common.collect.HashMultimap;
+
 import net.geforcemods.securitycraft.ClientHandler;
 import net.minecraft.client.resources.model.AtlasSet.StitchResult;
 import net.minecraft.client.resources.model.Material;
@@ -9,11 +16,6 @@ import net.minecraft.client.resources.model.ModelManager;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.profiling.ProfilerFiller;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.Map;
 
 /**
  * Used to capture the atlas preparations for rebaking the standalone models with a different
@@ -21,10 +23,10 @@ import java.util.Map;
  */
 @Mixin(ModelManager.class)
 public class ModelManagerMixin {
-    @Redirect(method = "loadModels", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/HashMultimap;create()Lcom/google/common/collect/HashMultimap;"))
-    private HashMultimap<ModelResourceLocation, Material> securitycraft$captureAtlasPreparations(ProfilerFiller profiler, Map<ResourceLocation, StitchResult> atlasPreperations, ModelBakery modelBakery) {
-        ClientHandler.setModelBakery(modelBakery);
-        ClientHandler.setAtlasPreperations(atlasPreperations);
-        return HashMultimap.create();
-    }
+	@Redirect(method = "loadModels", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/HashMultimap;create()Lcom/google/common/collect/HashMultimap;"))
+	private HashMultimap<ModelResourceLocation, Material> securitycraft$captureAtlasPreparations(ProfilerFiller profiler, Map<ResourceLocation, StitchResult> atlasPreperations, ModelBakery modelBakery) {
+		ClientHandler.setModelBakery(modelBakery);
+		ClientHandler.setAtlasPreperations(atlasPreperations);
+		return HashMultimap.create();
+	}
 }
