2d8511ff041bcacdd40bc9b027e91f811e93b828
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-09-19 20:56:59 +0200

    Fixed key panel dupe with keypad chests

--- src/main/java/net/geforcemods/securitycraft/items/KeyPanelItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/KeyPanelItem.java
@@ -5,7 +5,10 @@ import net.geforcemods.securitycraft.api.IOwnable;
 import net.geforcemods.securitycraft.api.IPasscodeConvertible;
 import net.geforcemods.securitycraft.api.SecurityCraftAPI;
 import net.geforcemods.securitycraft.misc.SCSounds;
+import net.geforcemods.securitycraft.util.PlayerUtils;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -32,9 +35,16 @@ public class KeyPanelItem extends BlockItem {
 		if (!(level.getBlockEntity(pos) instanceof IOwnable ownable) || ownable.isOwnedBy(player)) {
 			for (IPasscodeConvertible pc : SecurityCraftAPI.getRegisteredPasscodeConvertibles()) {
 				if (pc.isUnprotectedBlock(state)) {
+					int requiredKeyPanels = pc.getRequiredKeyPanels(state);
+
+					if (requiredKeyPanels > stack.getCount()) {
+						PlayerUtils.sendMessageToPlayer(player, Component.literal("SecurityCraft"), Component.translatable("messages.securitycraft:notEnoughKeyPanels", requiredKeyPanels), ChatFormatting.RED);
+						return InteractionResult.FAIL;
+					}
+
 					if (pc.protect(player, level, pos)) {
 						if (!player.isCreative())
-							stack.shrink(1);
+							stack.shrink(requiredKeyPanels);
 
 						level.playSound(null, pos, SCSounds.LOCK.event, SoundSource.BLOCKS, 1.0F, 1.0F);
 						return InteractionResult.SUCCESS;
