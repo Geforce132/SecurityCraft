690e5ca55063374da0feee7ea5bbbd44ffb89754
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-09-28 00:08:07 +0200

    Fix compile error in GuiBlockModelRenderer

--- src/main/java/net/geforcemods/securitycraft/screen/components/GuiBlockModelRenderer.java
+++ src/main/java/net/geforcemods/securitycraft/screen/components/GuiBlockModelRenderer.java
@@ -11,13 +11,15 @@ import com.mojang.math.Axis;
 import net.geforcemods.securitycraft.util.ClientUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.render.pip.PictureInPictureRenderer;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.MultiBufferSource.BufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.block.model.BlockStateModel;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+import net.minecraft.client.renderer.feature.FeatureRenderDispatcher;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
@@ -39,19 +41,27 @@ public class GuiBlockModelRenderer extends PictureInPictureRenderer<GuiBlockMode
 	}
 
 	@Override
-	protected void renderToTexture(GuiBlockModelRenderState state, PoseStack pose) {
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	protected void renderToTexture(GuiBlockModelRenderState guiRenderState, PoseStack pose) {
 		Minecraft mc = Minecraft.getInstance();
+		BlockEntityRenderer beRenderer = guiRenderState.beRenderer();
 
 		pose.scale(-24.0F, 24.0F, -24.0F);
 		pose.translate(0.5F, -1.5F, 0.5F);
 		pose.mulPose(DEFAULT_ROTATION);
-		pose.mulPose(state.rotation());
+		pose.mulPose(guiRenderState.rotation());
 		pose.translate(-0.5F, -0.5F, -0.5F);
 		mc.gameRenderer.getLighting().setupFor(Entry.ENTITY_IN_UI);
-		renderBlockModel(mc, state.blockAndTintGetter(), state.blockState(), pose, bufferSource);
+		renderBlockModel(mc, guiRenderState.blockAndTintGetter(), guiRenderState.blockState(), pose, bufferSource);
 
-		if (state.beRenderer() != null)
-			state.beRenderer().render(state.be(), mc.getDeltaTracker().getGameTimeDeltaPartialTick(true), pose, bufferSource, LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, mc.gameRenderer.getMainCamera().getPosition());
+		if (beRenderer != null) {
+			BlockEntityRenderState beRenderState = beRenderer.createRenderState();
+			FeatureRenderDispatcher featureRenderDispatcher = mc.gameRenderer.getFeatureRenderDispatcher();
+
+			beRenderer.extractRenderState(guiRenderState.be(), beRenderState, mc.getDeltaTracker().getGameTimeDeltaPartialTick(true), mc.gameRenderer.getMainCamera().getPosition(), null);
+			beRenderer.submit(beRenderState, pose, featureRenderDispatcher.getSubmitNodeStorage(), mc.levelRenderer.levelRenderState.cameraRenderState);
+			featureRenderDispatcher.renderAllFeatures();
+		}
 	}
 
 	@Override
