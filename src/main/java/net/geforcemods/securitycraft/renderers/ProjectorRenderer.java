f4c4a03d3712bd7fcb52a84fd1a498558bcecb45
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-09-28 01:21:54 +0200

    Fix Projector rendering

--- src/main/java/net/geforcemods/securitycraft/renderers/ProjectorRenderer.java
+++ src/main/java/net/geforcemods/securitycraft/renderers/ProjectorRenderer.java
@@ -1,6 +1,7 @@
 package net.geforcemods.securitycraft.renderers;
 
-import java.util.function.BiPredicate;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
@@ -8,8 +9,8 @@ import net.geforcemods.securitycraft.ClientHandler;
 import net.geforcemods.securitycraft.blockentities.ProjectorBlockEntity;
 import net.geforcemods.securitycraft.blocks.ProjectorBlock;
 import net.geforcemods.securitycraft.renderers.state.ProjectorRenderState;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.SubmitNodeCollector;
-import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
@@ -17,13 +18,16 @@ import net.minecraft.client.renderer.state.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.core.Vec3i;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.common.util.TriPredicate;
 
 public class ProjectorRenderer implements BlockEntityRenderer<ProjectorBlockEntity, ProjectorRenderState> {
 	public static final int RENDER_DISTANCE = 100;
-	private final BiPredicate<ProjectorRenderState, Integer> yLoopBoundary = (state, y) -> state.isHanging ? y > -state.projectionHeight : y < state.projectionHeight;
+	private final TriPredicate<Integer, Boolean, Integer> yLoopBoundary = (projectionHeight, hanging, y) -> hanging ? y > -projectionHeight : y < projectionHeight;
 
 	public ProjectorRenderer(BlockEntityRendererProvider.Context ctx) {}
 
@@ -31,37 +35,14 @@ public class ProjectorRenderer implements BlockEntityRenderer<ProjectorBlockEnti
 	public void submit(ProjectorRenderState state, PoseStack pose, SubmitNodeCollector collector, CameraRenderState camera) {
 		ClientHandler.DISGUISED_BLOCK_RENDER_DELEGATE.trySubmitDelegate(state.disguiseRenderState, pose, collector, camera);
 
-		if (state.isActive && state.projectsBlock) {
-			BlockState projectedState = state.projectedState;
-			BlockPos pos;
+		for (ProjectionInfo projectionInfo : state.renderPositions) {
+			Vec3i positionalOffset = projectionInfo.positionalOffset;
 
-			ModelBlockRenderer.enableCaching();
-
-			for (int x = 0; x < state.projectionWidth; x++) {
-				for (int y = 0; yLoopBoundary.test(state, y); y = state.isHanging ? y - 1 : y + 1) {
-					pose.pushPose();
-
-					if (!state.isHorizontal)
-						pos = translateProjection(state.blockPos, pose, state.facing, x, y, state.projectionRange, state.projectionOffset);
-					else
-						pos = translateProjection(state.blockPos, pose, state.facing, x, state.projectionRange - 16, y + 1, state.projectionOffset);
-
-					if (pos != null && (state.isOverridingBlocks /*|| level.isEmptyBlock(pos)*/)) { //TODO what to do with this level check
-						collector.submitBlock(pose, projectedState, state.lightCoords, OverlayTexture.NO_OVERLAY, 0); //TODO hope this works; also I don't know what to insert for overlay; maybe even a level is needed here?
-
-						/*BlockRenderDispatcher dispatcher = Minecraft.getInstance().getBlockRenderer();
-						BlockStateModel model = dispatcher.getBlockModel(projectedState);
-						Function<ChunkSectionLayer, RenderType> toRenderType = RenderTypeHelper::getMovingBlockRenderType;
-
-						dispatcher.renderBatched(projectedState, pos, level, pose, toRenderType.andThen(buffer::getBuffer), true, model.collectParts(be.getLevel(), pos, projectedState, RandomSource.create(projectedState.getSeed(pos))));*/
-						ClientHandler.PROJECTOR_RENDER_DELEGATE.trySubmitDelegate(state.projectedBlockEntityRenderState, pose, collector, camera);
-					}
-
-					pose.popPose();
-				}
-			}
-
-			ModelBlockRenderer.clearCache();
+			pose.pushPose();
+			pose.translate(positionalOffset.getX(), positionalOffset.getY(), positionalOffset.getZ());
+			collector.submitBlock(pose, state.projectedState, projectionInfo.lightCoords, OverlayTexture.NO_OVERLAY, 0);
+			ClientHandler.PROJECTOR_RENDER_DELEGATE.trySubmitDelegate(state.projectedBlockEntityRenderState, pose, collector, camera);
+			pose.popPose();
 		}
 	}
 
@@ -76,55 +57,57 @@ public class ProjectorRenderer implements BlockEntityRenderer<ProjectorBlockEnti
 		state.disguiseRenderState = ClientHandler.DISGUISED_BLOCK_RENDER_DELEGATE.tryExtractFromDelegate(be, partialTick, cameraPos, crumblingOverlay);
 		state.projectedBlockEntityRenderState = ClientHandler.PROJECTOR_RENDER_DELEGATE.tryExtractFromDelegate(be, partialTick, cameraPos, crumblingOverlay);
 		state.projectedState = be.getProjectedState();
-		state.facing = be.getBlockState().getValue(ProjectorBlock.FACING);
-		state.isActive = be.isActive();
-		state.projectsBlock = !be.isContainerEmpty();
-		state.isHanging = be.getBlockState().getValue(ProjectorBlock.HANGING);
-		state.isHorizontal = be.isHorizontal();
-		state.isOverridingBlocks = be.isOverridingBlocks();
-		state.projectionWidth = be.getProjectionWidth();
-		state.projectionHeight = be.getProjectionHeight();
-		state.projectionRange = be.getProjectionRange();
-		state.projectionOffset = be.getProjectionOffset();
+
+		List<ProjectionInfo> renderPositions = new ArrayList<>();
+
+		if (be.isActive() && !be.isContainerEmpty()) {
+			Level level = be.getLevel();
+			boolean hanging = be.getBlockState().getValue(ProjectorBlock.HANGING);
+			Direction direction = be.getBlockState().getValue(ProjectorBlock.FACING);
+			int projectionHeight = be.getProjectionHeight();
+			int projectionRange = be.getProjectionRange();
+			int projectionOffset = be.getProjectionOffset();
+			Vec3i positionalOffset;
+
+			for (int x = 0; x < be.getProjectionWidth(); x++) {
+				for (int y = 0; yLoopBoundary.test(projectionHeight, hanging, y); y = hanging ? y - 1 : y + 1) {
+					if (!be.isHorizontal())
+						positionalOffset = getPositionalOffset(direction, x, y, projectionRange, projectionOffset);
+					else
+						positionalOffset = getPositionalOffset(direction, x, projectionRange - 16, y + 1, projectionOffset);
+
+					BlockPos renderPos = be.getBlockPos().offset(positionalOffset);
+
+					if (positionalOffset != null && (be.isOverridingBlocks() || level.isEmptyBlock(renderPos)))
+						renderPositions.add(new ProjectionInfo(positionalOffset, LevelRenderer.getLightColor(be.getLevel(), renderPos)));
+				}
+			}
+		}
+
+		state.renderPositions = renderPositions;
 	}
 
 	/**
-	 * Shifts the projection depending on the offset and range set in the projector
+	 * Gets the offset of the block position to render a block at based on the projector's settings
 	 *
-	 * @param bePos The position of the projector which draws the fake block
-	 * @param pose the MatrixStack of the current render context
 	 * @param direction The direction the projector is facing
-	 * @param x The offset from the projectors position on the x axis of the position at which to draw the fake block
-	 * @param y The offset from the projectors position on the y axis of the position at which to draw the fake block
-	 * @param distance The distance in blocks that the fake block is away from the projector (set by player)
-	 * @param offset The offset in blocks that the fake block is moved to the side from the projector (set by player)
-	 * @return The BlockPos of the fake block to be drawn, null if an invalid direction was given
+	 * @param x The offset from the projectors position on the x-axis of the position at which to draw the fake block
+	 * @param y The offset from the projectors position on the y-axis of the position at which to draw the fake block
+	 * @param originalDistance The distance in blocks that the fake block is away from the projector (set by player)
+	 * @param originalOffset The offset in blocks that the fake block is moved to the side from the projector (set by player)
+	 * @return The offset of the fake block to be drawn, null if a non-horizontal direction was given
 	 */
-	private BlockPos translateProjection(BlockPos bePos, PoseStack pose, Direction direction, int x, int y, double distance, double offset) {
-		BlockPos pos = null;
+	private Vec3i getPositionalOffset(Direction direction, int x, int y, double originalDistance, double originalOffset) {
+		int distance = Mth.floor(originalDistance);
+		int offset = Mth.floor(originalOffset);
 
-		switch (direction) {
-			case NORTH:
-				pos = BlockPos.containing(bePos.getX() + x + offset, bePos.getY() + y, bePos.getZ() + distance);
-				pose.translate(0.0D + x + offset, 0.0D + y, distance);
-				break;
-			case SOUTH:
-				pos = BlockPos.containing(bePos.getX() + x + offset, bePos.getY() + y, bePos.getZ() + -distance);
-				pose.translate(0.0D + x + offset, 0.0D + y, -distance);
-				break;
-			case WEST:
-				pos = BlockPos.containing(bePos.getX() + distance, bePos.getY() + y, bePos.getZ() + x + offset);
-				pose.translate(distance, 0.0D + y, 0.0D + x + offset);
-				break;
-			case EAST:
-				pos = BlockPos.containing(bePos.getX() + -distance, bePos.getY() + y, bePos.getZ() + x + offset);
-				pose.translate(-distance, 0.0D + y, 0.0D + x + offset);
-				break;
-			default:
-				break;
-		}
-
-		return pos;
+		return switch (direction) {
+			case NORTH -> new Vec3i(x + offset, y, distance);
+			case SOUTH -> new Vec3i(x + offset, y, -distance);
+			case WEST -> new Vec3i(distance, y, x + offset);
+			case EAST -> new Vec3i(-distance, y, x + offset);
+			default -> null;
+		};
 	}
 
 	@Override
@@ -136,4 +119,6 @@ public class ProjectorRenderer implements BlockEntityRenderer<ProjectorBlockEnti
 	public AABB getRenderBoundingBox(ProjectorBlockEntity be) {
 		return new AABB(be.getBlockPos()).inflate(RENDER_DISTANCE);
 	}
+
+	public record ProjectionInfo(Vec3i positionalOffset, int lightCoords) {}
 }
--- src/main/java/net/geforcemods/securitycraft/renderers/state/ProjectorRenderState.java
+++ src/main/java/net/geforcemods/securitycraft/renderers/state/ProjectorRenderState.java
@@ -1,20 +1,13 @@
 package net.geforcemods.securitycraft.renderers.state;
 
+import java.util.List;
+
+import net.geforcemods.securitycraft.renderers.ProjectorRenderer.ProjectionInfo;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
-import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class ProjectorRenderState extends DisguisableBlockEntityRenderState {
 	public BlockEntityRenderState projectedBlockEntityRenderState;
 	public BlockState projectedState;
-	public Direction facing;
-	public boolean isActive;
-	public boolean isHanging;
-	public boolean isHorizontal;
-	public boolean isOverridingBlocks;
-	public boolean projectsBlock;
-	public int projectionHeight;
-	public int projectionWidth;
-	public int projectionRange;
-	public int projectionOffset;
+	public List<ProjectionInfo> renderPositions;
 }
