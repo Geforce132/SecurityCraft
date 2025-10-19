8ff4d030307152b20b7deb5fd3ea27dda7a7e315
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-10-19 21:38:26 +0200

    Give reinforced observer its own be type to only add a renderer to it
    Fixes #590

--- src/main/java/net/geforcemods/securitycraft/blockentities/FrameBlockEntity.java
+++ src/main/java/net/geforcemods/securitycraft/blockentities/FrameBlockEntity.java
@@ -315,9 +315,4 @@ public class FrameBlockEntity extends CustomizableBlockEntity implements ITickin
 				ModuleType.ALLOWLIST, ModuleType.REDSTONE
 		};
 	}
-
-	@Override
-	public boolean shouldRender() {
-		return true;
-	}
 }
