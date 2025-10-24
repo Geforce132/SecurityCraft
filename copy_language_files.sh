f92d1b0cceed32e6343b029dc47e9cb844ad5e51
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-10-24 23:01:06 +0200

    Support more German dialects

--- copy_language_files.sh
+++ copy_language_files.sh
@@ -2,15 +2,15 @@
 cd src/main/resources/assets/securitycraft/lang
 
 declare -A languages=(
-	["de"]="at ch"
-	["es"]="ar cl ec mx uy ve"
-	["fr"]="ca"
+	["de"]="de_at de_ch bar ksh nds_de"
+	["es"]="es_ar es_cl es_ec es_mx es_uy es_ve"
+	["fr"]="fr_ca"
 )
 
 for standard in "${!languages[@]}"; do
 	declare -a dialects=(${languages[$standard]})
 
 	for dialect in "${dialects[@]}"; do
-		cp "${standard}_${standard}.json" "${standard}_${dialect}.json"
+		cp "${standard}_${standard}.json" "${dialect}.json"
 	done
 done
