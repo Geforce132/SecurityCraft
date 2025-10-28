a0b25774c4c6a428a50db8aa9893abff0c6d51f7
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-10-28 21:35:33 +0100

    Fixed some briefcase and keycard interactions not respecting mask
    Fixes #598

--- src/main/java/net/geforcemods/securitycraft/items/BriefcaseItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/BriefcaseItem.java
@@ -2,12 +2,14 @@ package net.geforcemods.securitycraft.items;
 
 import it.unimi.dsi.fastutil.objects.ReferenceLinkedOpenHashSet;
 import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.api.Owner;
 import net.geforcemods.securitycraft.components.OwnerData;
 import net.geforcemods.securitycraft.components.PasscodeData;
 import net.geforcemods.securitycraft.inventory.BriefcaseMenu;
 import net.geforcemods.securitycraft.inventory.ItemContainer;
 import net.geforcemods.securitycraft.network.client.OpenScreen;
 import net.geforcemods.securitycraft.util.PasscodeUtils;
+import net.geforcemods.securitycraft.util.PlayerUtils;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
@@ -71,13 +73,12 @@ public class BriefcaseItem extends Item {
 	}
 
 	public static boolean isOwnedBy(ItemStack briefcase, Player player) {
-		OwnerData owner = briefcase.get(SCContent.OWNER_DATA);
+		OwnerData ownerData = briefcase.get(SCContent.OWNER_DATA);
 
-		if (owner != null) {
-			String ownerName = owner.name();
-			String ownerUUID = owner.uuid();
+		if (ownerData != null) {
+			Owner briefcaseOwner = ownerData.toOwner();
 
-			return ownerName.isEmpty() || ownerUUID.equals(player.getUUID().toString()) || (ownerUUID.equals("ownerUUID") && ownerName.equals(player.getName().getString()));
+			return ownerData.name().isEmpty() || (briefcaseOwner.isTreatedTheSameAs(new Owner(player)) && briefcaseOwner.isTreatedTheSameAs(PlayerUtils.getOwnerFromPlayerOrMask(player)));
 		}
 
 		return false;
