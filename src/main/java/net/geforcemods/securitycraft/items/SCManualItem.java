557acf247aaaf09531ce5120148737e7a9a11d90
Geoffrey Casper <gcasper42@gmail.com>
2026-01-05 14:49:07 -0500

    Handle mods incorrectly returning null when assembling a recipe for the manual

--- src/main/java/net/geforcemods/securitycraft/items/SCManualItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/SCManualItem.java
@@ -104,7 +104,12 @@ public class SCManualItem extends Item {
 
 	private static ItemStack safeAssemble(CraftingRecipe recipe, CraftingInput dummyInput, HolderLookup.Provider registryAccess) {
 		try {
-			return recipe.assemble(dummyInput, registryAccess);
+			ItemStack res = recipe.assemble(dummyInput, registryAccess);
+
+			if (res == null)
+				new NullPointerException("Recipe#assemble unexpectedly returned null").printStackTrace();
+			else
+				return res;
 		}
 		catch (Exception e) {
 			//If an exception is thrown, it's safe to assume that recipe assembling failed for some reason that means the recipe is not relevant for one of SC's items
