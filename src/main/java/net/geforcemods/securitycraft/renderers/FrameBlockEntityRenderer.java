c4f610d33142a2308cd8c010446e6d39e6899c11
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-02-20 00:27:17 +0100

    Fix frame feed being rendered with fully transparent pixels

--- src/main/java/net/geforcemods/securitycraft/renderers/FrameBlockEntityRenderer.java
+++ src/main/java/net/geforcemods/securitycraft/renderers/FrameBlockEntityRenderer.java
@@ -1,18 +1,23 @@
 package net.geforcemods.securitycraft.renderers;
 
+import java.nio.ByteBuffer;
 import java.util.OptionalDouble;
 import java.util.OptionalInt;
 
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
+import org.lwjgl.system.MemoryUtil;
 
 import com.mojang.blaze3d.buffers.GpuBuffer;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
+import com.mojang.blaze3d.buffers.Std140Builder;
+import com.mojang.blaze3d.buffers.Std140SizeCalculator;
 import com.mojang.blaze3d.pipeline.BlendFunction;
 import com.mojang.blaze3d.pipeline.RenderPipeline;
 import com.mojang.blaze3d.pipeline.RenderTarget;
 import com.mojang.blaze3d.platform.DepthTestFunction;
+import com.mojang.blaze3d.shaders.UniformType;
 import com.mojang.blaze3d.systems.GpuDevice;
 import com.mojang.blaze3d.systems.RenderPass;
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -35,6 +40,7 @@ import net.geforcemods.securitycraft.entity.camera.CameraFeed;
 import net.geforcemods.securitycraft.entity.camera.FrameFeedHandler;
 import net.geforcemods.securitycraft.renderers.state.FrameRenderState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.DynamicUniforms;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.SubmitNodeCollector.CustomGeometryRenderer;
@@ -59,6 +65,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
 public class FrameBlockEntityRenderer implements BlockEntityRenderer<FrameBlockEntity, FrameRenderState> {
+	private static final int VEC_3_SIZE = new Std140SizeCalculator().putFloat().putFloat().putFloat().get();
 	private static final Identifier CAMERA_NOT_FOUND = SecurityCraft.resLoc("textures/entity/frame/camera_not_found.png");
 	private static final Identifier INACTIVE = SecurityCraft.resLoc("textures/entity/frame/inactive.png");
 	private static final Identifier NO_REDSTONE_SIGNAL = SecurityCraft.resLoc("textures/entity/frame/no_redstone_signal.png");
@@ -72,6 +79,7 @@ public class FrameBlockEntityRenderer implements BlockEntityRenderer<FrameBlockE
 			.withFragmentShader(SecurityCraft.resLoc("frame_draw_fb_in_area"))
 			.withVertexFormat(DefaultVertexFormat.POSITION_TEX, VertexFormat.Mode.QUADS)
 			.withSampler("InSampler")
+			.withUniform("BackgroundColor", UniformType.UNIFORM_BUFFER)
 			.withBlend(BlendFunction.TRANSLUCENT)
 			.withDepthTestFunction(DepthTestFunction.LEQUAL_DEPTH_TEST)
 			.build();
@@ -105,16 +113,10 @@ public class FrameBlockEntityRenderer implements BlockEntityRenderer<FrameBlockE
 				submitSolidTexture(pose, collector, CAMERA_NOT_FOUND, innerVertices, lightCoords, normal, margin);
 			else if (!FrameFeedHandler.isCapturingCamera()) { //Only rendering the frame when no camera is being captured prevents screen-in-screen rendering
 				Vector3f backgroundColor = state.backgroundColor;
-				float xStart = innerVertices.x;
-				float xEnd = innerVertices.y;
-				float zStart = innerVertices.z;
-				float zEnd = innerVertices.w;
 				float xStartO = outerVertices.x;
 				float xEndO = outerVertices.y;
 				float zStartO = outerVertices.z;
 				float zEndO = outerVertices.w;
-
-				submitOverlay(pose, collector, RenderTypes.entityShadow(WHITE), ARGB.colorFromFloat(1.0F, backgroundColor.x, backgroundColor.y, backgroundColor.z), xStart, xEnd, zStart, zEnd, margin, lightCoords, normal);
 
 				try (ByteBufferBuilder byteBufferBuilder = new ByteBufferBuilder(DefaultVertexFormat.POSITION_TEX.getVertexSize() * 4)) {
 					BufferBuilder bufferBuilder = new BufferBuilder(byteBufferBuilder, VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
@@ -130,8 +132,11 @@ public class FrameBlockEntityRenderer implements BlockEntityRenderer<FrameBlockE
 						GpuDevice device = RenderSystem.getDevice();
 						GpuBuffer vertexBuffer = device.createBuffer(() -> "Frame Vertex", GpuBuffer.USAGE_VERTEX, meshData.vertexBuffer());
 						GpuBuffer indexBuffer = device.createBuffer(() -> "Frame Index", GpuBuffer.USAGE_INDEX | GpuBuffer.USAGE_COPY_DST, meshData.indexBuffer());
+						ByteBuffer backgroundColorByteBuffer = Std140Builder.intoBuffer(MemoryUtil.memAlloc(VEC_3_SIZE)).putFloat(backgroundColor.x).putFloat(backgroundColor.y).putFloat(backgroundColor.z).get();
+						GpuBuffer backgroundColorBuffer = device.createBuffer(() -> "Background Color", GpuBuffer.USAGE_UNIFORM, backgroundColorByteBuffer);
 						RenderTarget mainRenderTarget = Minecraft.getInstance().getMainRenderTarget();
-						GpuBufferSlice dynamicTransforms = RenderSystem.getDynamicUniforms().writeTransform(RenderSystem.getModelViewMatrix(), new Vector4f(), new Vector3f(), new Matrix4f());
+						DynamicUniforms dynamicUniforms = RenderSystem.getDynamicUniforms();
+						GpuBufferSlice dynamicTransforms = dynamicUniforms.writeTransform(RenderSystem.getModelViewMatrix(), new Vector4f(), new Vector3f(), new Matrix4f());
 
 						try (RenderPass pass = RenderSystem.getDevice().createCommandEncoder().createRenderPass(() -> "SC camera frame at " + state.blockPos, mainRenderTarget.getColorTextureView(), OptionalInt.empty(), mainRenderTarget.getDepthTextureView(), OptionalDouble.empty())) {
 							RenderSystem.bindDefaultUniforms(pass);
@@ -139,6 +144,7 @@ public class FrameBlockEntityRenderer implements BlockEntityRenderer<FrameBlockE
 							pass.setVertexBuffer(0, vertexBuffer);
 							pass.setIndexBuffer(indexBuffer, meshData.drawState().indexType());
 							pass.setUniform("DynamicTransforms", dynamicTransforms);
+							pass.setUniform("BackgroundColor", backgroundColorBuffer);
 							pass.bindTexture("InSampler", state.renderTargetColorTexture, RenderSystem.getSamplerCache().getClampToEdge(FilterMode.NEAREST));
 							pass.drawIndexed(0, 0, 6, 1);
 						}
