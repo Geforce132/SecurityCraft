f36167272982fc573d857f92906ffe9a60b25db1
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2026-02-27 13:27:51 +0100

    Fixed reinforced lanterns and chains not being waterloggable with bucket

--- src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedChainBlock.java
+++ src/main/java/net/geforcemods/securitycraft/blocks/reinforced/ReinforcedChainBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -16,7 +17,7 @@ import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class ReinforcedChainBlock extends ReinforcedRotatedPillarBlock {
+public class ReinforcedChainBlock extends ReinforcedRotatedPillarBlock implements SimpleWaterloggedBlock {
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 	protected static final VoxelShape X_AXIS_SHAPE = Block.box(0.0D, 6.5D, 6.5D, 16.0D, 9.5D, 9.5D);
 	protected static final VoxelShape Y_AXIS_SHAPE = Block.box(6.5D, 0.0D, 6.5D, 9.5D, 16.0D, 9.5D);
@@ -24,7 +25,7 @@ public class ReinforcedChainBlock extends ReinforcedRotatedPillarBlock {
 
 	public ReinforcedChainBlock(BlockBehaviour.Properties properties, Block vB) {
 		super(properties, vB);
-		this.registerDefaultState(stateDefinition.any().setValue(WATERLOGGED, false).setValue(AXIS, Direction.Axis.Y));
+		registerDefaultState(stateDefinition.any().setValue(WATERLOGGED, false).setValue(AXIS, Direction.Axis.Y));
 	}
 
 	@Override
@@ -33,7 +34,6 @@ public class ReinforcedChainBlock extends ReinforcedRotatedPillarBlock {
 			case X -> X_AXIS_SHAPE;
 			case Y -> Y_AXIS_SHAPE;
 			case Z -> Z_AXIS_SHAPE;
-			default -> X_AXIS_SHAPE;
 		};
 	}
 
@@ -41,6 +41,7 @@ public class ReinforcedChainBlock extends ReinforcedRotatedPillarBlock {
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
 		FluidState fluidState = context.getLevel().getFluidState(context.getClickedPos());
 		boolean isWater = fluidState.getType() == Fluids.WATER;
+
 		return super.getStateForPlacement(context).setValue(WATERLOGGED, isWater);
 	}
 
