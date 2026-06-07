82e698883da0a87bf7c317834df4ba0126f10ed9
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2026-06-07 16:50:21 +0200

    Fixed cycle buttons in state selector not being clickable
    No changelog line, since the issue was introduced during the Reinforcer rework

--- src/main/java/net/geforcemods/securitycraft/screen/components/StateSelector.java
+++ src/main/java/net/geforcemods/securitycraft/screen/components/StateSelector.java
@@ -390,6 +390,8 @@ public class StateSelector extends Screen implements GuiEventListener, Narratabl
 
 		@Override
 		public void onPress() {
+			super.onPress();
+
 			if (property == STANDING_OR_WALL_TYPE_PROPERTY) {
 				state = standingOrWallType.getNewState((StandingAndWallBlockItem) blockItem);
 				standingOrWallType = standingOrWallType == StandingOrWallType.STANDING ? StandingOrWallType.WALL : StandingOrWallType.STANDING;
