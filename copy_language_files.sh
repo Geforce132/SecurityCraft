bd9d0a16862150bdb7e1aab72b4b077a0ad647e6
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-06-17 22:09:29 +0200

    Add two more German dialects

--- copy_language_files.sh
+++ copy_language_files.sh
@@ -2,7 +2,7 @@
 cd src/main/resources/assets/securitycraft/lang
 
 declare -A languages=(
-	["de"]="de_at de_ch bar ksh nds_de"
+	["de"]="de_at de_ch bar fra_de ksh nds_de sxu"
 	["es"]="es_ar es_cl es_ec es_mx es_uy es_ve"
 	["fr"]="fr_ca"
 )
