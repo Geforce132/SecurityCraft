2f6ce97a508ed2c7e82763854acf37ca731cd31c
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-06-19 11:14:01 +0200

    Fail more leniently when assembling recipes
    Fixes #615

--- src/main/java/net/geforcemods/securitycraft/items/SCManualItem.java
+++ src/main/java/net/geforcemods/securitycraft/items/SCManualItem.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.mojang.serialization.DataResult;
+
 import net.geforcemods.securitycraft.ClientHandler;
 import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.SecurityCraft;
 import net.geforcemods.securitycraft.misc.PageGroup;
 import net.geforcemods.securitycraft.misc.SCManualPage;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.Tag;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -102,17 +108,25 @@ public class SCManualItem extends Item {
 
 	private static ItemStack safeAssemble(CraftingRecipe recipe, CraftingInput dummyInput) {
 		try {
-			ItemStack res = recipe.assemble(dummyInput);
+			ItemStack result = recipe.assemble(dummyInput);
 
-			if (res == null)
-				new NullPointerException("Recipe#assemble unexpectedly returned null for type " + recipe.getType()).printStackTrace();
-			else
-				return res;
+			if (result != null)
+				return result;
+			else {
+				DataResult<Tag> encodedRecipe = recipe.getSerializer().codec().encoder().encodeStart(NbtOps.INSTANCE, cast(recipe));
+
+				SecurityCraft.LOGGER.warn("Recipe#assemble unexpectedly returned null for recipe {}", encodedRecipe.result().orElseGet(CompoundTag::new));
+			}
 		}
 		catch (Exception e) {
 			//If an exception is thrown, it's safe to assume that recipe assembling failed for some reason that means the recipe is not relevant for one of SC's items
 		}
 
 		return ItemStack.EMPTY;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T extends CraftingRecipe> T cast(CraftingRecipe recipe) {
+		return (T) recipe;
 	}
 }
