4766815d08d4e9c0a039b0754f9a674bfab728e4
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-07-18 11:30:28 +0200

    Update NeoForge and BetterF3, use expressions for BetterF3 mixin

--- src/main/java/net/geforcemods/securitycraft/mixin/f3/BetterF3TargetModuleMixin.java
+++ src/main/java/net/geforcemods/securitycraft/mixin/f3/BetterF3TargetModuleMixin.java
@@ -2,8 +2,11 @@ package net.geforcemods.securitycraft.mixin.f3;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.sugar.Local;
 
 import net.geforcemods.securitycraft.misc.F3Spoofer;
@@ -19,13 +22,17 @@ import net.minecraft.world.level.material.FluidState;
 		"me.cominixo.betterf3.modules.TargetModule"
 })
 public class BetterF3TargetModuleMixin {
-	@ModifyVariable(method = "update", name = "blockState", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"))
-	private BlockState securitycraft$spoofBlockState(BlockState state, @Local(ordinal = 0) BlockPos blockPos) {
-		return F3Spoofer.spoofBlockState(state, blockPos);
+	@Definition(id = "getBlockState", method = "Lnet/minecraft/client/multiplayer/ClientLevel;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;")
+	@Expression("? = ?.getBlockState(?)")
+	@ModifyVariable(method = "update", name = "blockState", at = @At(value = "MIXINEXTRAS:EXPRESSION", shift = Shift.AFTER))
+	private BlockState securitycraft$spoofBlockState(BlockState blockState, @Local(name = "blockPos") BlockPos blockPos) {
+		return F3Spoofer.spoofBlockState(blockState, blockPos);
 	}
 
-	@ModifyVariable(method = "update", name = "fluidState", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getFluidState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/material/FluidState;"))
-	private FluidState securitycraft$spoofFluidState(FluidState originalState) {
-		return F3Spoofer.spoofFluidState(originalState);
+	@Definition(id = "getFluidState", method = "Lnet/minecraft/client/multiplayer/ClientLevel;getFluidState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/material/FluidState;")
+	@Expression("? = ?.getFluidState(?)")
+	@ModifyVariable(method = "update", name = "fluidState", at = @At(value = "MIXINEXTRAS:EXPRESSION", shift = Shift.AFTER))
+	private FluidState securitycraft$spoofFluidState(FluidState fluidState) {
+		return F3Spoofer.spoofFluidState(fluidState);
 	}
 }
